4cdf3cde7d93f138e0f9b0e72a55e611f849819d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-01-09 12:37:40 +0000

    adding reduction process and the schedule for cmos in ngts

---
+++ marana_schedule.py
@@ -0,0 +1,139 @@
+"""
+Fetch the scheduled observing sequence from a
+given NGTS camera and produce a JSON file for
+the CMOS to observe with
+"""
+import argparse as ap
+import json
+from datetime import datetime
+import numpy as np
+import pymysql
+
+# pylint: disable=invalid-name
+# pylint: disable=redefined-outer-name
+
+# set some flags to avoid duplicates
+FOCUS_SWEEP_SCHEDULED = False
+# make blocks for flats and focusing
+evening_flats = {"type": "SkyFlats",
+                 "evening": True,
+                 "pipeline": {"prefix": "evening-flat"}}
+morning_flats = {"type": "SkyFlats",
+                 "evening": False,
+                 "pipeline": {"prefix": "morning-flat"}}
+
+
+def arg_parse():
+    p = ap.ArgumentParser()
+    p.add_argument('camera_id',
+                   help='camera_id to follow',
+                   type=int,
+                   choices=np.arange(801, 814))
+    p.add_argument('night',
+                   help="Night to query from (e.g. 20231008)",
+                   type=str)
+    return p.parse_args()
+
+
+def get_action_info(camera_id, night):
+    qry = """
+        SELECT action_id, action, schedule_start_utc, schedule_end_utc
+        FROM action_list
+        WHERE camera_id=%s
+        AND night=%s
+        ORDER BY schedule_start_utc ASC
+        """
+    qry_args = (camera_id, night)
+
+    with pymysql.connect(host='10.2.5.32', db='ngts_ops', user='ops') as conn:
+        with conn.cursor() as cur:
+            cur.execute(qry, qry_args)
+            action_info = cur.fetchall()
+    return action_info
+
+
+def get_action_args(action_id):
+    qry = """
+        SELECT arg_key, arg_value
+        FROM action_args
+        WHERE action_id=%s
+        AND arg_key IN ('campaign', 'field', 'exposureTime')
+        """
+    qry_args = (action_id,)
+    with pymysql.connect(host='10.2.5.32', db='ngts_ops', user='ops') as conn:
+        with conn.cursor() as cur:
+            cur.execute(qry, qry_args)
+            action_args = cur.fetchall()
+        act_args = {}
+        for aa in action_args:
+            act_args[aa[0]] = aa[1]
+    return act_args
+
+
+def get_field_coords(field):
+    qry = """
+        SELECT ra_centre_deg, dec_centre_deg
+        FROM master_field_list
+        WHERE field=%s
+        """
+    qry_args = (field,)
+    with pymysql.connect(host='10.2.5.32', db='ngts_ops', user='ops') as conn:
+        with conn.cursor() as cur:
+            cur.execute(qry, qry_args)
+            coords = cur.fetchone()
+    return coords
+
+
+if __name__ == "__main__":
+    args = arg_parse()
+
+    # fetch all actions for camera we are following
+    action_info = get_action_info(args.camera_id, args.night)
+
+    # create empty dict to populate
+    OB = {}
+    OB['night'] = datetime.strptime(args.night, "%Y%m%d").strftime("%Y-%m-%d")
+    OB['actions'] = []
+    OB['actions'].append(evening_flats)
+
+    # loop over actions and add to schedule
+    for action in action_info:
+        action_id, action_type, start, end = action
+
+        if action_type == "focusSweep" and not FOCUS_SWEEP_SCHEDULED:
+            evening_focus = {"type": "AutoFocus",
+                             "start": start.strftime("%Y-%m-%dT%H:%M:%S") + "Z",
+                             "camera": {"exposure": 10.0}}
+            OB['actions'].append(evening_focus)
+            FOCUS_SWEEP_SCHEDULED = True
+
+        elif action_type == "observeField":
+            act_args = get_action_args(action_id)
+            # build some inputs
+            field = act_args['field']
+            prefix = f"{act_args['field']}_{act_args['campaign']}"
+            exposure = round(float(act_args['exposureTime']), 2)
+            # get field coords
+            ra_centre_deg, dec_centre_deg = get_field_coords(field)
+            # make a science block
+            science = {"type": "ObserveTimeSeries",
+                       "start": start.strftime("%Y-%m-%dT%H:%M:%S") + "Z",
+                       "end": end.strftime("%Y-%m-%dT%H:%M:%S") + "Z",
+                       "ra": ra_centre_deg,
+                       "dec": dec_centre_deg,
+                       "pipeline": {"prefix": prefix,
+                                    "object": prefix},
+                       "camera": {"exposure": exposure}
+                       }
+
+            # finally plonk the action into the scedule
+            OB['actions'].append(science)
+
+    # schedule morning flats
+    OB['actions'].append(morning_flats)
+
+    # save the json file
+    with open(f'{args.night}.json', 'w') as fp:
+        json.dump(OB, fp, indent=4)
+
+    print(f"Saved observing plan to {args.night}.json")
--- reduction.py
+++ reduction.py
@@ -24,96 +24,115 @@ logger.handlers = []
 logger.propagate = False
 
 
+def plot_master_image(master_image):
+    if not os.path.exists(master_image):
+        pass
+    else:
+        plt.imshow(master_image, cmap='gray', origin='lower', vmin=np.min(master_image), vmax=np.max(master_image))
+        plt.colorbar(pad=0.01, fraction=0.047, shrink=1, aspect=20, extend='both', extendrect=True, extendfrac='auto')
+        plt.show()
+
+
 class BiasDarkFlatCorrection:
-    def __init__(self, path_1, path_2, path_3, path_4):
+    def __init__(self, path_1):
         self.path_1 = path_1
-        self.path_2 = path_2
-        self.path_3 = path_3
-        self.path_4 = path_4
         self.master_bias = None
         self.master_dark = None
         self.master_flat = None
 
     def create_master_bias(self):
-        # Create a list of the bias images
-        bias_list = [f for f in glob.glob(self.path_1 + '*.fits') if 'bias' in fits.getheader(f)['IMAGETYP']]
+        if os.path.exists(self.path_1 + 'master_bias.fits'):
+            print('Master bias already exists')
+            pass
+        else:
+            bias_list = [f for f in glob.glob(self.path_1 + '*.fits') if 'BIAS' in fits.getheader(f)['IMAGETYP']]
 
-        ccd_list = []
-        for f in bias_list:
-            ccd = CCDData.read(f, unit='adu')
-            ccd_list.append(ccd)
+            ccd_list = []
+            for f in bias_list:
+                ccd = CCDData.read(f, unit='adu')
+                ccd_list.append(ccd)
 
         # Combine the bias images
-        self.master_bias = combine(ccd_list, method='median')
-        self.master_bias.write(self.path_2 + 'master_bias.fits', overwrite=True)
+            self.master_bias = combine(ccd_list, method='median')
+            self.master_bias.write(self.path_1 + 'master_bias.fits', overwrite=True)
 
     logging.debug('Master bias created')
 
     def create_master_dark(self, dark_exposure):
-        # Create a list of the dark images
-        dark_list = [f for f in glob.glob(self.path_1 + '*.fits') if 'dark' in fits.getheader(f)['IMAGETYP']]
+        if os.path.exists(self.path_1 + 'master_dark.fits'):
+            print('Master dark already exists')
+            pass
+        else:
 
-        ccd_list = []
-        for f in dark_list:
-            ccd = CCDData.read(f, unit='adu')
-            ccd = subtract_bias(ccd, self.master_bias)
-            ccd_list.append(ccd)
+            dark_list = [f for f in glob.glob(self.path_1 + '*.fits') if 'DARK' in fits.getheader(f)['IMAGETYP']]
+
+            ccd_list = []
+            for f in dark_list:
+                ccd = CCDData.read(f, unit='adu')
+                ccd = subtract_bias(ccd, self.master_bias)
+                ccd_list.append(ccd)
 
         # Combine the dark images
-        self.master_dark = combine(ccd_list, method='median')
-        self.master_dark.write(self.path_2 + 'master_dark.fits', overwrite=True)
+            self.master_dark = combine(ccd_list, method='median')
+            self.master_dark.write(self.path_1 + 'master_dark.fits', overwrite=True)
 
     logging.debug('Master dark created')
 
     def create_master_flat(self):
-        # Create a list of the flat images
-        flat_list = [f for f in glob.glob(self.path_1 + '*.fits') if 'flat' in fits.getheader(f)['IMAGETYP']]
-        flat_list.sort()
+        if os.path.exists(self.path_1 + 'master_flat.fits'):
+            print('Master flat already exists')
+            pass
+        else:
+            flat_list = [f for f in glob.glob(self.path_1 + '*.fits') if 'FLAT' in fits.getheader(f)['IMAGETYP']]
+            flat_list.sort()
 
-        for f in flat_list:
-            with fits.open(f) as fitsfile:
-                data_exp = int(fitsfile[0].header[exptime_keyword])
-            ccd = CCDData.read(f, unit=u.adu)
-            ccd = subtract_bias(ccd, self.master_bias)
-            ccd = subtract_dark(ccd, self.master_dark,
-                                scale=True,
-                                dark_exposure=dark_exposure * u.second,
-                                data_exposure=data_exp * u.second)
-            ccd.data = ccd.data / np.ma.average(ccd.data)
-            flat_list.append(ccd)
+            for f in flat_list:
+                with fits.open(f) as fitsfile:
+                    data_exp = int(fitsfile[0].header[exptime_keyword])
+                ccd = CCDData.read(f, unit=u.adu)
+                ccd = subtract_bias(ccd, self.master_bias)
+                ccd = subtract_dark(ccd, self.master_dark,
+                                    scale=True,
+                                    dark_exposure=dark_exposure * u.second,
+                                    data_exposure=data_exp * u.second)
+                ccd.data = ccd.data / np.ma.average(ccd.data)
+                flat_list.append(ccd)
 
         # Combine the flat images
-        self.master_flat = combine(flat_list, method='median')
-        self.master_flat.write(self.path_2 + 'master_flat.fits', overwrite=True)
-        logging.debug('Master flat created')
+            self.master_flat = combine(flat_list, method='median')
+            self.master_flat.write(self.path_1 + 'master_flat.fits', overwrite=True)
+            logging.debug('Master flat created')
 
     def apply_bias_dark_flat_correction(self):
+        print('Applying bias, dark, and flat correction')
+
         # Create a list of the science images
-        science_list = glob.glob(self.path_4 + '*.fits')
+        science_list = [f for f in glob.glob(self.path_1 + '*.fits')]
         science_list.sort()
 
         for f in science_list:
-            ccd = CCDData.read(f, unit=u.adu)
-            ccd = subtract_bias(ccd, self.master_bias)
-            ccd = subtract_dark(ccd, self.master_dark, exposure_time='EXPOSURE', exposure_unit=u.second)
-            ccd = flat_correct(ccd, self.master_flat)
-            fits.writeto(self.path_4 + 'd_' + os.path.basename(f), ccd.data, ccd.header, overwrite=True)
+            header = fits.getheader(f)
+            if "SCIENCE" in header.get("IMAGETYP", ""):
+                ccd = CCDData.read(f, unit=u.adu)
+
+                # Make sure master_bias is not None before attempting to subtract
+                if self.master_bias is not None:
+                    ccd = subtract_bias(ccd, self.master_bias)
+
+                ccd = subtract_dark(ccd, self.master_dark, scale=True,
+                                    dark_exposure=dark_exposure * u.second,
+                                    data_exposure=dark_exposure * u.second)
+
+                # ccd = flat_correct(ccd, self.master_flat)
+                fits.writeto(self.path_1 + 'd_' + os.path.basename(f), ccd.data, ccd.header, overwrite=True)
+            else:
+                print(f"Skipping {f}: 'IMAGETYP' not found in header or not 'SCIENCE'.")
 
     logging.debug('Bias, dark, and flat correction applied')
 
-    def plot_master_image(self, master_image):
-        plt.imshow(master_image, cmap='gray', origin='lower', vmin=np.min(master_image), vmax=np.max(master_image))
-        plt.colorbar(pad=0.01, fraction=0.047, shrink=1, aspect=20, extend='both', extendrect=True, extendfrac='auto')
-        plt.show()
-
-    logging.debug('Master image plotted')
-
 
 # Paths
-path_1 = '/Users/apergis/Downloads/cmos_data//'  # Bias
-path_2 = '/Users/apergis/Downloads/cmos_data//'  # Dark
-path_3 = '/Users/apergis/Downloads/cmos_data//'  # Flat
-path_4 = '/Users/apergis/Downloads/cmos_data//'  # Science
+path_1 = '/Users/u5500483/Downloads/Datasets/20231211/20231211/'
 
 # Dark exposure time for creating master dark
 dark_exposure = 10
@@ -122,17 +141,18 @@ dark_exposure = 10
 exptime_keyword = 'EXPTIME'
 
 # Perform bias, dark, and flat correction
-correction = BiasDarkFlatCorrection(path_1, path_2, path_3, path_4)
+correction = BiasDarkFlatCorrection(path_1)
 correction.create_master_bias()
 correction.create_master_dark(dark_exposure)
-correction.create_master_flat()
+# correction.create_master_flat()
 correction.apply_bias_dark_flat_correction()
 
 # Plot the master bias image
-correction.plot_master_image(correction.master_bias)
+plot_master_image(correction.master_bias)
 
 # Plot the master dark image
-correction.plot_master_image(correction.master_dark)
+# plot_master_image(correction.master_dark)
 
 # Plot the master flat image
-correction.plot_master_image(correction.master_flat)
+# correction.plot_master_image(correction.master_flat)
+
