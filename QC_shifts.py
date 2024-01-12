fe447ed63dc6e512a88a067912e5c488af1a668c
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-01-12 10:34:34 +0000

    adding a plot for the pixels shifts

--- QC_shifts.py
+++ QC_shifts.py
@@ -1,6 +1,7 @@
 from donuts import Donuts
 import glob
 
+from matplotlib import pyplot as plt
 
 path = '/Users/u5500483/Downloads/DATA_MAC/CMOS/TOI-00451/'
 
@@ -19,6 +20,10 @@ d = Donuts(
     ntiles=32)
 # for each image, compute the x/y translation required
 # to align the images onto the reference image
+
+x_shifts = []
+y_shifts = []
+
 for image in science_image_names:
     shift_result = d.measure_shift(image)
     x = shift_result.x
@@ -31,3 +36,22 @@ for image in science_image_names:
     else:
         pass
 
+    # Append shift values to the lists
+    x_shifts.append(x.value)
+    y_shifts.append(y.value)
+
+plt.figure(figsize=(8, 8))
+plt.scatter(x_shifts, y_shifts, label='Shifts', marker='o')
+plt.xlabel('X Shift (pixels)')
+plt.ylabel('Y Shift (pixels)')
+plt.title('Shifts with respect to the first image')
+plt.axhline(0, color='black', linestyle='-', linewidth=1)  # Add horizontal line at y=0
+plt.axvline(0, color='black', linestyle='-', linewidth=1)  # Add vertical line at x=0
+plt.legend()
+
+# Set the axes limits to center (0, 0)
+plt.xlim(-1, 1)
+plt.ylim(-1, 1)
+
+
+plt.show()
