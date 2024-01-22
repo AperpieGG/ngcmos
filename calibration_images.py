b0f47abc6c52bc002f5ee34818b4356700e80222
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-01-22 11:44:30 +0000

    fixing flat function

--- calibration_images.py
+++ calibration_images.py
@@ -1,82 +1,114 @@
 import glob
 import os
+from datetime import datetime, timedelta
 from astropy.io import fits
 import numpy as np
 import re
 
 
 def bias(base_path, out_path):
-    if os.path.exists(out_path + 'master_bias.fits'):
+    master_bias_path = os.path.join(out_path, 'master_bias.fits')
+
+    if os.path.exists(master_bias_path):
         print('Master bias already exists')
-        pass
+    else:
+        print('Creating master bias')
 
-    # Find and read the bias for hdr mode
-    files = [f for f in glob.glob(base_path + 'bias*.fits') if 'HDR' in fits.getheader(f)['READMODE']]
+        # Find and read the bias for hdr mode
+        files = [f for f in glob.glob(os.path.join(base_path, 'bias*.fits')) if 'HDR' in fits.getheader(f)['READMODE']]
 
-    # Limit the number of files to the first 21
-    files = files[:21]
+        # Limit the number of files to the first 21
+        files = files[:21]
 
-    cube = np.zeros((2048, 2048, len(files)))
-    for i, f in enumerate(files):
-        cube[:, :, i] = fits.getdata(f)
-    master_bias = np.median(cube, axis=2)
-    fits.PrimaryHDU(master_bias).writeto('master-bias.fits', overwrite=True)
-    return master_bias
+        cube = np.zeros((2048, 2048, len(files)))
+        for i, f in enumerate(files):
+            cube[:, :, i] = fits.getdata(f)
+        master_bias = np.median(cube, axis=2)
+        fits.PrimaryHDU(master_bias).writeto(master_bias_path, overwrite=True)
+        return master_bias
 
 
 def dark(base_path, out_path, master_bias):
-    if os.path.exists(out_path + 'master_dark.fits'):
+    master_dark_path = os.path.join(out_path, 'master_dark.fits')
+
+    if os.path.exists(master_dark_path):
         print('Master dark already exists')
-        pass
+    else:
+        print('Creating master dark')
 
-    # Find and read the darks for hdr mode
-    files = [f for f in glob.glob(base_path + 'dark*.fits') if 'HDR' in fits.getheader(f)['READMODE']]
+        # Find and read the darks for hdr mode
+        files = [f for f in glob.glob(os.path.join(base_path, 'dark*.fits')) if 'HDR' in fits.getheader(f)['READMODE']]
 
-    # Limit the number of files to the first 21
+        # Limit the number of files to the first 21
+        files = files[:21]
+
+        cube = np.zeros((2048, 2048, len(files)))
+        for i, f in enumerate(files):
+            cube[:, :, i] = fits.getdata(f)
+        master_dark = np.median(cube, axis=2) - master_bias
+        fits.PrimaryHDU(master_dark).writeto(master_dark_path, overwrite=True)
+        return master_dark
+
+
+def find_current_night_directory(base_path):
+    # Get the previous date directory in the format YYYYMMDD
+    previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+
+    # Construct the path for the previous_date directory
+    current_date_directory = os.path.join(base_path, previous_date)
+
+    # Check if the directory exists
+    if os.path.isdir(current_date_directory):
+        return current_date_directory
+    else:
+        return None
+
+
+def flat(base_path, out_path, master_bias, master_dark, dark_exposure=10):
+    current_night_directory = find_current_night_directory(base_path)
+
+    if current_night_directory is None:
+        print('Current night directory not found')
+        return
+
+    if os.path.exists(os.path.join(out_path, f'master_flat_{os.path.basename(current_night_directory)}.fits')):
+        print('Master flat already exists')
+        return
+
+    files = [f for f in glob.glob(os.path.join(current_night_directory, 'evening*.fits')) if
+             'HDR' in fits.getheader(f)['READMODE']]
+    if not files:
+        print('No HDR flat files found.')
+        return
+
+    print('Creating master flat')
+    # take only the first 21
     files = files[:21]
 
-    cube = np.zeros((2048, 2048, len(files)))
+    cube = np.zeros((*master_bias.shape, len(files)))
     for i, f in enumerate(files):
-        cube[:, :, i] = fits.getdata(f)
-    master_dark = np.median(cube, axis=2) - master_bias
-    fits.PrimaryHDU(master_dark).writeto('master-dark.fits', overwrite=True)
-    return master_dark
+        data, header = fits.getdata(f, header=True)
+        cube[:, :, i] = data - master_bias - master_dark * header['EXPTIME'] / dark_exposure
+        cube[:, :, i] = cube[:, :, i] / np.average(cube[:, :, i])
+
+    master_flat = np.median(cube, axis=2)
+    fits.PrimaryHDU(master_flat).writeto(
+        os.path.join(out_path, f'master_flat_{os.path.basename(current_night_directory)}.fits'), overwrite=True)
+    del cube
+    del master_flat
 
 
-def flat(base_path, out_path, master_bias, master_dark, dark_exposure=10, prefix):
-    if os.path.exists(out_path + 'master_flat.fits'):
-        print('Master flat already exists')
-        pass
-    # Mask out the non-imaging areas of the sensor
-    h = fits.getheader(glob.glob(f'/data/20240109/evening-flat-*-*.fits')[0])
-    r = re.search(r'^\[(\d+):(\d+),(\d+):(\d+)\]$', h['IMAG-RGN']).groups()
-    x1 = int(r[0]) - 1
-    x2 = int(r[1])
-    y1 = int(r[2]) - 1
-    y2 = int(r[3])
+def flat_up_one_directory(calibration_path, out_path, master_bias, master_dark, dark_exposure=10):
+    # Get the parent directory of the calibration_path
+    flat_path = os.path.abspath(os.path.join(calibration_path, os.pardir))
 
-    mask = np.ones(master_bias.shape, dtype=bool)
-    mask[y1:y2, x1:x2] = 0
-
-    for filt in ['B', 'V', 'R', 'I', 'NONE']:
-        print(f'Creating {filt} master flat')
-        files = glob.glob(f'/data/20240109/evening-flat-{filt}-*.fits')
-        if files:
-            cube = np.zeros((*master_bias.shape, len(files)))
-            for i, f in enumerate(files):
-                data, header = fits.getdata(f, header=True)
-                cube[:, :, i] = data - master_bias - master_dark * header['EXPTIME'] / dark_exposure
-                cube[:, :, i] = cube[:, :, i] / np.average(cube[:, :, i])
-                cube[mask, i] = 1
-
-            master_flat = np.median(cube, axis=2)
-            fits.PrimaryHDU(master_flat).writeto(f'master-flat-{filt}.fits', overwrite=True)
-            del cube
-            del master_flat
+    # Call the flat function with the modified flat_path
+    flat(flat_path, out_path, master_bias, master_dark, dark_exposure)
 
 
 if __name__ == '__main__':
-    path = '/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/'
-    out_path = '/Users/u5500483/Documents/GitHub/ngcmos/'
-    master_bias = bias(path, out_path)
-    dark(path, out_path, master_bias)
+    calibration_path = '/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/'
+    out_path = '/Users/u5500483/Documents/GitHub/ngcmos/'  # to be changed for nuc (home/ops/calibration_images)
+    master_bias = bias(calibration_path, out_path)
+    master_dark = dark(calibration_path, out_path, master_bias)
+    flat_up_one_directory(calibration_path, out_path, master_bias, master_dark)
