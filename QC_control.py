a06ce8da8241874707caf0d9696c7e9a3a03b885
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-01-23 17:22:52 +0000

    remove all the limits

--- QC_control.py
+++ QC_control.py
@@ -104,21 +104,14 @@ def create_blink_animation(images, save_path):
 
     fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(16, 8))  # Two subplots side by side
 
-    zscale_interval = ZScaleInterval()
-
-    vmin = np.percentile(images[0][0], 5)
-    vmax = np.percentile(images[0][0], 95)
-
-    norm = ImageNormalize(vmin=vmin, vmax=vmax, stretch=SqrtStretch())
-
     # Plot for full frame data
-    im1 = ax1.imshow(images[0][0][450:550, 600:700], cmap='hot', origin='lower', norm=norm)
+    im1 = ax1.imshow(images[0][0][450:550, 600:700], cmap='hot', origin='lower')
     ax1.set_xlabel('X-axis [pix]')
     ax1.set_ylabel('Y-axis [pix]')
     ax1.set_title('Zoom in Image')
 
     # Plot for cropped data
-    im2 = ax2.imshow(images[0][0], cmap='hot', origin='lower', norm=norm)
+    im2 = ax2.imshow(images[0][0], cmap='hot', origin='lower')
     ax2.set_xlabel('X-axis [pix]')
     ax2.set_ylabel('Y-axis [pix]')
     ax2.set_title('Full frame Image')
