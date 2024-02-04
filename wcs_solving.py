16f0769e8b84d540ada11acad0bddee75a421590
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-04 15:50:53 +0000

    adding the new wcs_solving thst was called dev

--- wcs_solving.py
+++ wcs_solving.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python3
 """
 This script solves the WCS for all FITS images in the specified directory
 and removes unwanted files.
@@ -9,16 +9,29 @@ from astropy.io import fits
 import shutil
 
 
-def solve_reference_image(refimage, backup_folder):
+def find_current_night_directory(file_path):
+    # Get the current date in the format YYYYMMDD
+    current_date = datetime.now().strftime("%Y%m%d") + '/'
+    previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d") + '/'
+
+    # Construct the path for the previous_date directory
+    current_date_directory = os.path.join(file_path, previous_date)
+
+    # Check if the directory exists
+    if os.path.isdir(current_date_directory):
+        return current_date_directory
+    else:
+        return None
+
+
+def solve_reference_image(refimage):
     """
-    Solve the reference image to obtain better WCS
+    Solve the reference image to obtain better WCS if not already present
 
     Parameters
     ----------
     refimage : string
         path to the reference image for solving
-    backup_folder : string
-        path to the folder to store backup files
 
     Returns
     -------
@@ -29,46 +42,37 @@ def solve_reference_image(refimage, backup_folder):
     ------
     None
     """
-    solved_refimage = f"{os.path.splitext(refimage)[0]}.new"
+    with fits.open(refimage) as hdulist:
+        header = hdulist[0].header
 
-    if not os.path.exists(solved_refimage):
-        with fits.open(refimage) as hdulist:
-            header = hdulist[0].header
-            ra = float(header['TELRAD'])
-            dec = float(header['TELDECD'])
+        # Check if WCS information is already present
+        if 'CTYPE1' not in header or 'CTYPE2' not in header:
+            log_file = f"{refimage.split('.fits')[0]}.astrom_log"
+            solved_refimage = f"{os.path.splitext(refimage)[0]}.new"
 
-        # Build the astrometry.net command
-        command = (
-            f"solve-field {refimage} "
-            f"--ra {ra:.6f} --dec {dec:.6f} "
-            "--radius 5 "
-            "--overwrite "
-            "--scale-units arcsecperpix \
-             --scale-low 3.9 --scale-high 4.3"
-            "--skip-solved --no-plots --no-verify -z 2 --tweak-order 3 --cpulimit 600"
-        )
+            # Build the astrometry.net command
+            command = (
+                f"solve-field {refimage} "
+                f"--ra {header['TELRAD']:.6f} --dec {header['TELDECD']:.6f} "
+                "--radius 5 "
+                # "--overwrite "
+                "--scale-units arcsecperpix \
+                 --scale-low 3.9 --scale-high 4.3"
+                "--skip-solved --no-plots --no-verify -z 2 --tweak-order 3 --cpulimit 600 --continue"
+            )
 
-        try:
-            os.system(command)
-        except Exception as e:
-            print(f"Error solving image: {e}")
-            return False
-
-        # Backup the original .fits file
-        try:
-            shutil.move(refimage, os.path.join(backup_folder, os.path.basename(refimage)))
-            print(f"Moved original file to backup folder: {os.path.basename(refimage)}")
-        except Exception as e:
-            print(f"Error moving original file to backup folder: {e}")
-
-        # Rename the solved image to use .fits instead of .new
-        os.rename(solved_refimage, f"{os.path.splitext(refimage)[0]}.fits")
-        print(f"Renamed solved image to: {os.path.splitext(refimage)[0]}.fits")
-
-    return os.path.exists(f"{os.path.splitext(refimage)[0]}.fits")
+            try:
+                os.system(command)
+                return True  # Image solved successfully
+            except Exception as e:
+                print(f"Error solving image: {e}")
+                return False
+        else:
+            print(f"WCS already present in header for {refimage}")
+            return True
 
 
-def solve_all_images_in_directory(directory, backup_folder):
+def solve_all_images_in_directory(directory):
     """
     Solve WCS for all FITS images in the specified directory
 
@@ -89,13 +93,13 @@ def solve_all_images_in_directory(directory, backup_folder):
 
     for filename in fits_files:
         filepath = os.path.join(directory, filename)
-        if solve_reference_image(filepath, backup_folder):
-            print(f"WCS solved successfully for {filename}")
-        else:
-            print(f"Failed to solve WCS for {filename}")
+        solve_reference_image(filepath)
 
 
 def remove_unwanted_files(directory):
+    """
+    When finished with the wcs it removes all unwanted files and renames the .new files to .fits
+    """
     unwanted_extensions = ['.xyls', '.axy', '.corr', '.match', '.rdls', '.solved', '.wcs']
 
     for filename in os.listdir(directory):
@@ -103,31 +107,63 @@ def remove_unwanted_files(directory):
             file_path = os.path.join(directory, filename)
             try:
                 os.remove(file_path)
-                print(f"Removed unwanted file: {filename}")
             except Exception as e:
                 print(f"Error removing file {filename}: {e}")
 
-
-def find_current_night_directory(file_path):
-    # Get the current date in the format YYYYMMDD
-    current_date = datetime.now().strftime("%Y%m%d") + '/'
-    previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d") + '/'
-
-    # Construct the path for the previous_date directory
-    current_date_directory = os.path.join(file_path, previous_date)
-
-    # Check if the directory exists
-    if os.path.isdir(current_date_directory):
-        return current_date_directory
-    else:
-        return None
+    for filename in os.listdir(directory):
+        if filename.endswith('.new'):
+            file_path = os.path.join(directory, filename)
+            try:
+                os.rename(file_path, f"{os.path.splitext(file_path)[0]}.fits")
+            except Exception as e:
+                print(f"Error renaming file {filename}: {e}")
 
 
-def zip_backup_folder(backup_folder):
-    # Zip the contents of the backup folder directly
-    zip_filename = f"{os.path.splitext(backup_folder)[0]}"
-    shutil.make_archive(zip_filename, 'zip', os.path.dirname(backup_folder), os.path.basename(backup_folder))
-    print(f"Backup folder zipped successfully: {zip_filename}")
+def check_headers(directory):
+    """
+
+    When finished with the wcs it Checks the headers of all FITS files in the specified directory for CTYPE1 and CTYPE2
+    and moves the files that don't have them to a subdirectory called 'no_wcs'.
+
+    Parameters
+    ----------
+    directory : string
+        path to the directory containing FITS images
+
+    Returns
+    -------
+    None
+
+    """
+    no_wcs = os.path.join(directory, 'no_wcs')
+
+    # Create 'no_wcs' subdirectory if it doesn't exist
+    if not os.path.exists(no_wcs):
+        os.makedirs(no_wcs)
+
+    for filename in os.listdir(directory):
+        if filename.endswith('.fits'):
+            exclude_words = ["evening", "morning", "flat", "bias", "dark"]
+            if any(word in filename.lower() for word in exclude_words):
+                continue
+
+            file_path = os.path.join(directory, filename)
+
+            try:
+                with fits.open(file_path) as hdulist:
+                    header = hdulist[0].header
+                    ctype1 = header.get('CTYPE1')
+                    ctype2 = header.get('CTYPE2')
+
+                    if ctype1 is None or ctype2 is None:
+                        print(f"Warning: {filename} does not have CTYPE1 and/or CTYPE2 in the header. Moving to "
+                              f"'no_wcs' directory.")
+                        # Move the file to 'no_wcs' subdirectory
+                        new_path = os.path.join(no_wcs, filename)
+                        os.rename(file_path, new_path)
+
+            except Exception as e:
+                print(f"Error checking header for {filename}: {e}")
 
 
 def main():
@@ -138,23 +174,17 @@ def main():
     if current_night_directory:
         print(f"Current night directory found: {current_night_directory}")
 
-        # Directly create the backup zip file without creating a backup folder
-        previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
-        backup_folder = os.path.join(current_night_directory, f"backup_{previous_date}")
-        os.mkdir(backup_folder)
-        print(f"Backup folder created: {backup_folder}")
         # Proceed with solving and other operations
-        solve_all_images_in_directory(current_night_directory, backup_folder)
+        solve_all_images_in_directory(current_night_directory)
         remove_unwanted_files(current_night_directory)
 
-        # Zip the backup folder
-        zip_backup_folder(backup_folder)
-        shutil.rmtree(backup_folder)
-        print(f"Backup folder zipped successfully: {backup_folder}")
-
+        # Check CTYPE1 and CTYPE2 in the headers
+        check_headers(current_night_directory)
     else:
         print("No current night directory found.")
 
 
 if __name__ == "__main__":
     main()
+
+# TODO - pass argument to run the script for a single image and see if it works, then proceed with the total code
