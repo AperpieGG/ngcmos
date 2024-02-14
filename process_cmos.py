80cf29965eba126242855195e7bbb518ef367362
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-14 17:17:11 +0000

    fixing region files

--- process_cmos.py
+++ process_cmos.py
@@ -18,6 +18,9 @@ from calibration_images import reduce_images, bias, dark, flat
 from donuts import Donuts
 import warnings
 
+warnings.filterwarnings("ignore", category=UserWarning, module="numpy.core.fromnumeric")
+warnings.filterwarnings("ignore", category=UserWarning, module="donuts.image")
+
 # First directory
 calibration_path_1 = '/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/'
 base_path_1 = '/Users/u5500483/Downloads/DATA_MAC/CMOS/'
@@ -99,6 +102,23 @@ def find_current_night_directory(directory):
         return os.getcwd()
 
 
+def get_prefix(filename):
+    """
+    Extracts the prefix (first 11 letters) from the filename.
+
+    Parameters
+    ----------
+    filename : str
+        Name of the file.
+
+    Returns
+    -------
+    str
+        Prefix of the filename.
+    """
+    return filename[:11]
+
+
 def calibrate_images(directory):
     """
     Calibrate the images in the directory
@@ -131,58 +151,6 @@ def calibrate_images(directory):
     return fits_files
 
 
-def get_coords_from_header(fits_files):
-    """
-    Get the coordinates from the header of FITS files.
-
-    Parameters
-    ----------
-    fits_files : list
-        List of FITS files.
-
-    Returns
-    -------
-    None
-    """
-    for fits_file in fits_files:
-        # Open the FITS file
-        hdulist = fits.open(fits_file)
-
-        # Get the binary table data
-        data = hdulist[1].data
-
-        # Extract RA and DEC coordinates
-        RA = data['ra_deg']
-        DEC = data['dec_deg']
-
-        # Print the first 10 entries as an example
-        for i in range(10):
-            print(f'Entry {i + 1}: RA = {RA[i]}, DEC = {DEC[i]}')
-
-        # Close the FITS file
-        hdulist.close()
-
-
-def get_prefix(filename):
-    """
-    Extracts the prefix (first 11 letters) from the filename.
-
-    Parameters
-    ----------
-    filename : str
-        Name of the file.
-
-    Returns
-    -------
-    str
-        Prefix of the filename.
-    """
-    return filename[:11]
-
-
-# load aperture file
-
-# check donuts and measure shifts
 def check_donuts(filenames):
     """
     Check the donuts for each group of images and measure the shifts.
@@ -226,103 +194,68 @@ def check_donuts(filenames):
                 continue
 
 
-def get_region(filename):
-    pass
-
-
-# photometry
-def phot(data, shift, x, y, rsi, rso, aper_radii, filename, jd, bjd, hjd,
-         phot_filename_prefix="rtp", ds9_name=None, draw_regions=False,
-         gain=1.00, index_offset=1):
+def get_region(directory, prefix):
     """
-    Measure the photometry of the given image at the positions
-    requested. Output the results to a photometry file and
-    display the apertures in DS9 if required.
+    Find the region file corresponding to the given prefix in the current night directory.
 
     Parameters
     ----------
-    data : numpy array
-        Image array
-    shift : donuts.image.Image
-        The shift between the reference and this image is found
-        in the image object for the shift measurment
-    x : array
-        X postions to extract photometry
-    y : array
-        Y postions to extract photometry
-    rsi : array
-        Inner sky annulii radii
-    rso : array
-        Outer sky annulii radii
-    aper_radii : array
-        Sequence of aperture radii to extract
-    filename : str
-        The name of the file we are extracting photometry from
-    jd : astropy.time.Time
-        JD at mid exposure
-    bjd : astropy.time.Time
-        BJD_TDB at mid exposure
-    hjd : astropy.time.Time
-        HJD as mid exposure
-    phot_filename_prefix : str
-        Prefix for the photometry output filename
-        Default = 'rtp'
-    ds9_name : str, optional
-        Name of instance of the DS9 program
-        Default = None
-    draw_regions : bool, optional
-        Boolean to set whether we draw the regions in DS9 or not
-        Default = False
-    gain : float, optional
-        CCD gain setting
-        Default = 1.00
-    index_offset : int
-        Index offset between python and ds9
-        default = 1
+    directory : str
+        Path to the current night directory.
+    prefix : str
+        Prefix to search for.
 
     Returns
     -------
-    None
-
-    Raises
-    ------
-    None
+    pyregion.ShapeList or None
+        Region shapes parsed from the region file if found, otherwise None.
     """
-    x = np.array(x)-shift.x.value
-    y = np.array(y)-shift.y.value
-    # loop over each aperture size
-    for r, r_aper in enumerate(aper_radii):
-        # preamble
-        out_str = "{0:s}  {1:.8f}  {2:.8f}  {3:.8f}  ".format(filename, jd.value, bjd.value, hjd.value)
-        # do the phot
-        flux, fluxerr, _ = sep.sum_circle(data, x, y, r_aper,
-                                          subpix=0,
-                                          bkgann=(rsi, rso),
-                                          gain=gain)
-        flux_w_sky, fluxerr_w_sky, _ = sep.sum_circle(data, x, y, r_aper,
-                                                      subpix=0,
-                                                      gain=gain)
-        # loop over each object and compile the output
-        for i, j, inner, outer, f, fe, fs, fse in zip(x, y, rsi, rso, flux, fluxerr, flux_w_sky, fluxerr_w_sky):
-            if ds9_name and draw_regions and r == 0:
-                annulus = '{{annulus {0} {1} {2} {3} # color=green}}'.format(i+index_offset,
-                                                                             j+index_offset,
-                                                                             inner, outer)
-                circle = '{{circle {0} {1} {2} # colo=red}}'.format(i+index_offset,
-                                                                    j+index_offset,
-                                                                    r_aper)
-                dset(ds9_name, f"regions command '{annulus}'")
-                dset(ds9_name, f"regions command '{circle}'")
-            max_pixel_value = find_max_pixel_value(data, int(i), int(j), r_aper+1)
-            temp = "{:.2f}  {:.2f}  {:.2f}  {:.2f}  {:.2f}  {:.2f}  {:.2f}  ".format(
-                float(i), float(j), float(f), float(fe), float(fs), float(fse), float(max_pixel_value))
-            out_str = out_str + temp
-        out_str = out_str + "\n"
+    region_files = [f for f in os.listdir(directory) if f.startswith(prefix) and f.endswith('_master.reg')]
+    print("Found region file {} for prefix {}".format(region_files, prefix))
+    if region_files:
+        region_file_path = os.path.join(directory, region_files[0])
+        try:
+            x, y = pyregion.open(region_file_path)
+            return x, y
+        except Exception as e:
+            print(f"Error reading region file {region_file_path}: {e}")
+            return None
+    else:
+        return None
 
-        # output the photometry with .photAPERTURE_SIZE extension
-        with open(f"{phot_filename_prefix}.phot{r_aper}", 'a') as outfile:
-            outfile.write(out_str)
 
+def phot(data, shift, x, y, r_aperture, filename, jd, bjd, hjd,
+         phot_filename_prefix="rtp",
+         gain=1.12):
+    # Apply shifts
+    x = np.array(x) - shift.x.value
+    y = np.array(y) - shift.y.value
+
+    # Preamble for output
+    out_str = "{0:s}  {1:.8f}  {2:.8f}  {3:.8f}  ".format(filename, jd.value, bjd.value, hjd.value)
+
+    # Perform photometry for each star
+    for i, j in zip(x, y):
+        # Extract flux within the fixed aperture
+        flux, fluxerr, _ = sep.sum_circle(data, i, j, r_aperture, subpix=0, gain=gain)
+
+        # Estimate background using SEP
+        bkg = sep.Background(data)
+        bkg_value = bkg.globalback
+
+        # Adjusted flux to subtract background
+        flux_w_sky = flux - bkg_value * np.pi * r_aperture ** 2
+
+        # Compile output string
+        temp = "{:.2f}  {:.2f}  {:.2f}  {:.2f}  ".format(
+            float(i), float(j), float(flux_w_sky), float(fluxerr))
+        out_str += temp
+
+    out_str += "\n"
+
+    # Output the photometry
+    with open(f"{phot_filename_prefix}.phot{r_aperture}", 'a') as outfile:
+        outfile.write(out_str)
 
 
 def main():
@@ -337,11 +270,6 @@ def main():
     for prefix in set(get_prefix(filename) for filename in fits_files):
         catalog_files[prefix] = [f for f in fits_files if f.startswith(prefix) and 'catalog' in f]
 
-    # Process catalog files for each prefix
-    for prefix, files in catalog_files.items():
-        print(f'Processing catalog files for prefix: {prefix}')
-        get_coords_from_header(files)
-
     # Check donuts for each group
     grouped_filenames = defaultdict(list)
     for filename in fits_files:
@@ -351,6 +279,17 @@ def main():
     # Check donuts for each group
     for filenames in grouped_filenames.values():
         check_donuts(filenames)
+
+    # Find region files for each prefix
+    current_night_directory = find_current_night_directory(base_path)
+    for filenames in grouped_filenames.values():
+        for filename in filenames:
+            prefix = get_prefix(filename)
+            region_file = get_region(current_night_directory, prefix)
+            if region_file:
+                print(f"Found region file {region_file} for prefix {prefix}")
+            else:
+                print(f"No region file found for prefix {prefix}")
 
 
 if __name__ == "__main__":
