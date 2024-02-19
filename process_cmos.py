406d323524f02ae6d89b028f76bede05bd95700b
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-19 17:41:52 +0200

    reformat the file

--- process_cmos.py
+++ process_cmos.py
@@ -1,55 +1,40 @@
 #! /usr/bin/env python
 import os
-import sys
-from collections import defaultdict
 from datetime import datetime, timedelta
-import numpy as np
-from astropy.wcs import WCS
-from skyfield.api import Topos
-import re
+from collections import defaultdict
 from calibration_images import reduce_images
 from donuts import Donuts
-from utils import source_extract, catalogue_to_pixels
+import numpy as np
+from utils import catalogue_to_pixels, parse_region_content
+import json
 import warnings
-from astropy.io import fits
 
-# Suppress warnings
-warnings.filterwarnings("ignore", category=UserWarning, module="numpy.core.fromnumeric")
-warnings.filterwarnings("ignore", category=UserWarning, module="donuts.image")
-warnings.filterwarnings("ignore", category=UserWarning, module="FITsFixedWarning")
+# ignore some annoying warnings
+warnings.simplefilter('ignore', category=UserWarning)
 
-# pylint: disable = invalid-name
-# pylint: disable = redefined-outer-name
-# pylint: disable = no-member
-# pylint: disable = too-many-locals
-# pylint: disable = too-many-arguments
-# pylint: disable = unused-variable
 
-# Define directories
-calibration_path_1 = '/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/'
-base_path_1 = '/Users/u5500483/Downloads/DATA_MAC/CMOS/'
-out_path_1 = '/Users/u5500483/Downloads/DATA_MAC/CMOS/calibration_images/'
+def load_config(filename):
+    with open(filename, 'r') as file:
+        config = json.load(file)
+    return config
 
-calibration_path_2 = '/home/ops/data/20231212/'
-base_path_2 = '/home/ops/data/'
-out_path_2 = '/home/ops/data/calibration_images/'
+
+# Load paths from the configuration file
+config = load_config('directories.json')
+calibration_paths = config["calibration_paths"]
+base_paths = config["base_paths"]
+out_paths = config["out_paths"]
 
 # Select directory based on existence
-if os.path.exists(base_path_1):
-    calibration_path = calibration_path_1
-    base_path = base_path_1
-    out_path = out_path_1
-else:
-    base_path = base_path_2
-    calibration_path = calibration_path_2
-    out_path = out_path_2
+for calibration_path, base_path, out_path in zip(calibration_paths, base_paths, out_paths):
+    if os.path.exists(base_path):
+        break
 
 
-# Find current night directory
 def find_current_night_directory(directory):
     """
     Find the directory for the current night based on the current date.
-    if not then use the current working directory.
+    If not found, use the current working directory.
 
     Parameters
     ----------
@@ -58,36 +43,12 @@ def find_current_night_directory(directory):
 
     Returns
     -------
-    str or None
-        Path to the current night directory if found, otherwise None.
+    str
+        Path to the current night directory.
     """
     previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
     current_date_directory = os.path.join(directory, previous_date)
-    if os.path.isdir(current_date_directory):
-        return current_date_directory
-    else:
-        return os.getcwd()
-
-
-def find_first_image_of_each_prefix(filenames):
-    # Sort the filenames
-    sorted_filenames = sorted(filenames)
-
-    # Create a dictionary to store the first image of each prefix
-    first_images = {}
-
-    # Iterate over each filename
-    for filename in sorted_filenames:
-        # Extract the prefix from the filename
-        prefix = get_prefix(filename)
-
-        # Check if the prefix is not already in the dictionary
-        if prefix not in first_images:
-            # Add the filename to the dictionary
-            first_images[prefix] = filename
-
-    print(first_images)
-    return first_images
+    return current_date_directory if os.path.isdir(current_date_directory) else os.getcwd()
 
 
 def get_prefix(filename):
@@ -97,12 +58,16 @@ def get_prefix(filename):
     return filename[:11]
 
 
-# Check donuts for each group
-def check_donuts(filenames):
-    """
-    Check donuts for each group.
+def find_first_image_of_each_prefix(filenames):
+    first_images = {}
+    for filename in sorted(filenames):
+        prefix = get_prefix(filename)
+        if prefix not in first_images:
+            first_images[prefix] = filename
+    return first_images
 
-    """
+
+def check_donuts(filenames):
     excluded_keywords = ['catalog', 'morning', 'evening', 'bias', 'flat', 'dark']
     grouped_filenames = defaultdict(list)
     for filename in filenames:
@@ -111,15 +76,12 @@ def check_donuts(filenames):
 
     for prefix, filenames in grouped_filenames.items():
         filenames.sort()
-
         reference_image = filenames[0]
         d = Donuts(reference_image)
         for filename in filenames[1:]:
             if any(keyword in filename for keyword in excluded_keywords):
                 continue
-            with warnings.catch_warnings():
-                warnings.simplefilter("ignore")
-                shift = d.measure_shift(filename)
+            shift = d.measure_shift(filename)
             sx = round(shift.x.value, 2)
             sy = round(shift.y.value, 2)
             print(f'{filename} shift X: {sx} Y: {sy}')
@@ -131,25 +93,10 @@ def check_donuts(filenames):
                 comm = f'mv {filename} failed_donuts/'
                 print(comm)
                 os.system(comm)
-                continue
 
 
-# Find region files for each prefix
 def get_region_files(filenames):
-    """
-    Find region files for each prefix.
-
-    Parameters
-    ----------
-    filenames : list of filenames to search for region files.
-
-    Returns
-    -------
-    dict
-        Dictionary containing prefixes as keys and region files as values.
-    """
-    grouped_region_files = defaultdict(set)  # Use set to ensure uniqueness
-
+    grouped_region_files = defaultdict(set)
     for filename in filenames:
         prefix = get_prefix(filename)
         exclude_keywords = ['catalog', 'morning', 'evening', 'bias', 'flat', 'dark']
@@ -157,61 +104,16 @@ def get_region_files(filenames):
             continue
         region_files = [f for f in os.listdir() if f.startswith(prefix) and f.endswith('_input.reg')]
         grouped_region_files[prefix].update(region_files)
-
     return grouped_region_files
 
 
 def read_region_files(region_files):
-    """
-    Read the contents of region files.
-
-    Parameters
-    ----------
-    region_files : list
-        List of region files to read.
-
-    Returns
-    -------
-    dict
-        Dictionary containing region file names as keys and their contents as values.
-    """
     region_contents = {}
-
     for region_file in region_files:
         with open(region_file, 'r') as file:
             contents = file.read()
             region_contents[region_file] = contents
-
     return region_contents
-
-
-def parse_region_content(region_content):
-    """
-    Parse the content of a region file to extract RA and Dec coordinates.
-
-    Parameters
-    ----------
-    region_content : str
-        Content of the region file.
-
-    Returns
-    -------
-    list of tuples
-        List of (RA, Dec) coordinate tuples extracted from the region content.
-    """
-    ra_dec_coords = []
-    # Split the region content into lines
-    lines = region_content.split('\n')
-    for line in lines:
-        # Assuming each line represents a circular region with RA and Dec coordinates
-        # Extract the RA and Dec coordinates from the line
-        if line.startswith('circle'):
-            # Example line format: "circle(123.456, 78.901, 2.0)"
-            parts = line.split('(')[1].split(')')[0].split(',')
-            ra = float(parts[0])
-            dec = float(parts[1])
-            ra_dec_coords.append((ra, dec))
-    return ra_dec_coords
 
 
 def main():
@@ -224,10 +126,6 @@ def main():
     # Get region files for each prefix
     region_files = get_region_files(filenames)
 
-    # Print region files for each prefix
-    for prefix, files in region_files.items():
-        print(f"Prefix: {prefix}, Region Files: {files}")
-
     # Read the contents of region files
     region_contents = {}
     for prefix, files in region_files.items():
@@ -236,12 +134,10 @@ def main():
     for prefix, contents in region_contents.items():
         first_images = find_first_image_of_each_prefix(filenames)
         for region_file, region_content in contents.items():
-            # Extract RA and Dec from region content (assuming you have a function to parse the region file)
             ra_dec_coords = parse_region_content(region_content)
-            print(f"Prefix: {prefix}, Region File: {region_file}, cordinates: {ra_dec_coords}")
+            print(f"Prefix: {prefix}, Region File: {region_file}, coordinates: {ra_dec_coords}")
 
-            if prefix in first_images:  # Check if the prefix exists in the fits_files dictionary
-                # Convert RA and Dec to pixel coordinates
+            if prefix in first_images:
                 xy_coordinates = catalogue_to_pixels(first_images[prefix], ra_dec_coords)
                 print("X coordinates:", xy_coordinates[0])
                 print("Y coordinates:", xy_coordinates[1])
@@ -251,5 +147,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-# TODO: Add aditional annulus size for the region files (follow J MCMC code)
