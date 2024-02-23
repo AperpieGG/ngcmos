898aeaa68187479cd97591668667b20f2d0d10d1
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-23 14:07:11 +0000

    reformat script

--- photometry.py
+++ photometry.py
@@ -439,65 +439,62 @@ def wcs_phot(data, x, y, rsi, rso, aperture_radii, gain=1.12):
     return Tout
 
 
-def main():
-    # set directory for the current night or use the current working directory
-    directory = find_current_night_directory(base_path)
-    print(f"Directory: {directory}")
+# set directory for the current night or use the current working directory
+directory = find_current_night_directory(base_path)
+print(f"Directory: {directory}")
 
-    # filter filenames only for .fits data files
-    filenames = filter_filenames(directory)
-    print(f"Number of files: {len(filenames)}")
+# filter filenames only for .fits data files
+filenames = filter_filenames(directory)
+print(f"Number of files: {len(filenames)}")
 
-    # Iterate over each filename to get the prefix
-    prefixes = get_prefix(filenames)
-    print(f"The prefixes are: {prefixes}")
+# Iterate over each filename to get the prefix
+prefixes = get_prefix(filenames)
+print(f"The prefixes are: {prefixes}")
 
-    # Get filenames corresponding to each prefix
-    prefix_filenames = [[filename for filename in filenames if filename.startswith(prefix)] for prefix in prefixes]
+# Get filenames corresponding to each prefix
+prefix_filenames = [[filename for filename in filenames if filename.startswith(prefix)] for prefix in prefixes]
 
-    for prefix, filenames in zip(prefixes, prefix_filenames):
-        print(f"Processing prefix: {prefix}")
+for prefix, filenames in zip(prefixes, prefix_filenames):
+    print(f"Processing prefix: {prefix}")
 
-        # Check headers for CTYPE1 and CTYPE2
-        check_headers(directory, filenames)
+    # Check headers for CTYPE1 and CTYPE2
+    check_headers(directory, filenames)
 
-        # Calibrate images and get FITS files
-        reduced_data, jd_list, bjd_list, hjd_list, prefix_filenames = reduce_images(base_path, out_path)
+    # Calibrate images and get FITS files
+    reduced_data, jd_list, bjd_list, hjd_list, prefix_filenames = reduce_images(base_path, out_path)
 
-        # Load WCS information from the first processed image
-        first_processed_image = prefix_filenames[0]
-        ref_frame_data, ref_header = load_fits_image(first_processed_image)
+    # Load WCS information from the first processed image
+    first_processed_image = prefix_filenames[0]
+    ref_frame_data, ref_header = load_fits_image(first_processed_image)
 
-        wcs_ignore_cards = ['SIMPLE', 'BITPIX', 'NAXIS', 'EXTEND', 'DATE', 'IMAGEW', 'IMAGEH']
-        wcs_header = {}
-        for line in [ref_header[i:i + 80] for i in range(0, len(ref_header), 80)]:
-            key = line[0:8].strip()
-            if '=' in line and key not in wcs_ignore_cards:
-                card = fits.Card.fromstring(line)
-                wcs_header[card.keyword] = card.value
+    wcs_ignore_cards = ['SIMPLE', 'BITPIX', 'NAXIS', 'EXTEND', 'DATE', 'IMAGEW', 'IMAGEH']
+    wcs_header = {}
+    for line in [ref_header[i:i + 80] for i in range(0, len(ref_header), 80)]:
+        key = line[0:8].strip()
+        if '=' in line and key not in wcs_ignore_cards:
+            card = fits.Card.fromstring(line)
+            wcs_header[card.keyword] = card.value
 
-        # ref_frame_bg = sep.Background(ref_frame_data)
-        # ref_frame_data_corr_no_bg = ref_frame_data - ref_frame_bg
-        # estimate_coord = SkyCoord(ra=ref_header['TELRA'],
-        #                           dec=ref_header['TELDEC'],
-        #                           unit=(u.deg, u.deg))
-        # estimate_coord_radius = 3 * u.deg
-        #
-        # ref_objects = _detect_objects_sep(ref_frame_data_corr_no_bg, ref_frame_bg.globalrms,
-        #                                   AREA_MIN, AREA_MAX, DETECTION_SIGMA, DEFOCUS)
-        # if len(ref_objects) < N_OBJECTS_LIMIT:
-        #     print(f"Fewer than {N_OBJECTS_LIMIT} found in reference, quitting!")
-        #     sys.exit(TOO_FEW_OBJECTS)
+    # ref_frame_bg = sep.Background(ref_frame_data)
+    # ref_frame_data_corr_no_bg = ref_frame_data - ref_frame_bg
+    # estimate_coord = SkyCoord(ra=ref_header['TELRA'],
+    #                           dec=ref_header['TELDEC'],
+    #                           unit=(u.deg, u.deg))
+    # estimate_coord_radius = 3 * u.deg
+    #
+    # ref_objects = _detect_objects_sep(ref_frame_data_corr_no_bg, ref_frame_bg.globalrms,
+    #                                   AREA_MIN, AREA_MAX, DETECTION_SIGMA, DEFOCUS)
+    # if len(ref_objects) < N_OBJECTS_LIMIT:
+    #     print(f"Fewer than {N_OBJECTS_LIMIT} found in reference, quitting!")
+    #     sys.exit(TOO_FEW_OBJECTS)
 
-        # Load the photometry catalog
-        phot_cat, _ = get_catalog(f"{directory}/{prefix}_catalog_input.fits", ext=1)
-        print(f"Found catalog with name {prefix}_catalog_input.fits")
-        # Convert RA and DEC to pixel coordinates using the WCS information from the header
-        wcs = WCS(wcs_header)
-        phot_x, phot_y = wcs.all_world2pix(phot_cat['ra_deg_corr'], phot_cat['dec_deg_corr'], 1)
+    # Load the photometry catalog
+    phot_cat, _ = get_catalog(f"{directory}/{prefix}_catalog_input.fits", ext=1)
+    print(f"Found catalog with name {prefix}_catalog_input.fits")
+    # Convert RA and DEC to pixel coordinates using the WCS information from the header
+    wcs = WCS(wcs_header)
+    phot_x, phot_y = wcs.all_world2pix(phot_cat['ra_deg_corr'], phot_cat['dec_deg_corr'], 1)
 
-        print(f"X and Y coordinates: {phot_x}, {phot_y}")
+    print(f"X and Y coordinates: {phot_x}, {phot_y}")
 
 
-if __name__ == "__main__":
-    main()
