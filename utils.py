65904c6a526adc40647457133065147ecde525d5
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-26 18:25:43 +0000

    move functions from process here

--- utils.py
+++ utils.py
@@ -1,17 +1,13 @@
 """
 Functions for handling on-sky or on chip coordinates
 """
-import sys
-import math
-from datetime import datetime
 from astropy.io import fits
 import sep
 import numpy as np
 from astropy.coordinates import SkyCoord, EarthLocation
 from astropy.wcs import WCS
 import astropy.units as u
-import jastro.housekeeping as jhk
-
+from astropy.table import Table, hstack
 
 # pylint: disable=invalid-name
 # pylint: disable=no-member
@@ -78,200 +74,229 @@ def get_light_travel_times(ra, dec, time_to_correct):
     return ltt_bary, ltt_helio
 
 
-def catalogue_to_pixels(filenames, ra_dec_coords):
+def get_catalog(filename, ext=0):
     """
-    Convert a list of catalogue positions to X and Y image
-    coordinates
-
-    Parameters
-    ----------
-    filenames : list of filenames
-    ra_dec_coords : list of (RA, Dec) coordinate tuples
-
-    Returns
-    -------
-    numpy.ndarray, numpy.ndarray
-        Arrays of X and Y pixel coordinates
-    """
-    try:
-        _, hdr = fits.getdata(filenames, header=True)
-    except (OSError, IndexError, KeyError) as e:
-        print(f'CANNOT FIND HEADER INFORMATION IN {filenames}, EXITING...')
-        sys.exit(1)
-
-    # Load the WCS
-    w = WCS(hdr)
-
-    # Convert RA and Dec to pixel coordinates
-    pix = w.wcs_world2pix(ra_dec_coords, 0)
-    x, y = pix[:, 0], pix[:, 1]
-
-    return x, y
-
-
-def parse_region_content(region_content):
-    """
-    Parse the content of a region file to extract RA and Dec coordinates.
-
-    Parameters
-    ----------
-    region_content : str
-        Content of the region file.
-
-    Returns
-    -------
-    list of tuples
-        List of (RA, Dec) coordinate tuples extracted from the region content.
-    """
-    ra_dec_coords = []
-    # Split the region content into lines
-    lines = region_content.split('\n')
-    for line in lines:
-        # Assuming each line represents a circular region with RA and Dec coordinates
-        # Extract the RA and Dec coordinates from the line
-        if line.startswith('circle'):
-            # Example line format: "circle(123.456, 78.901, 2.0)"
-            parts = line.split('(')[1].split(')')[0].split(',')
-            ra = float(parts[0])
-            dec = float(parts[1])
-            ra_dec_coords.append((ra, dec))
-    return ra_dec_coords
-
-
-# TODO: Fix the following functions for the pipeline
-def extract_background(filename, sigma):
-    """
-    Extract background from the image using SEP
+    Read a fits image and header with fitsio
 
     Parameters
     ----------
     filename : str
-        The name of the file to run sep.extract on
-    sigma : int, optional
-        The number of sigma a detection must be above the background
-        to be flagged as a star
+        filename to load
+    ext : int
+        extension to load
 
     Returns
     -------
-    bkg : array-like
-        Background values extracted from the image
+    data : array
+        data from the corresponding extension
+    header : fitsio.header.FITSHDR
+        list from file header
 
     Raises
     ------
     None
     """
-    data, _ = jhk.load_fits_image(filename)
-    data = data.astype(np.float64)
-
-    bkg = sep.Background(data)
-    return bkg
+    data, header = fits.getdata(filename, header=True, ext=ext)
+    return data, header
 
 
-def source_extract(filename, sigma, ap_radius, catalogue_coords):
+def convert_coords_to_pixels(cat, filenames):
     """
-    Measure the sky background and perform photometry on sources
-    in the image supplied
+    Convert RA and DEC coordinates to pixel coordinates using the first image for each prefix.
 
     Parameters
     ----------
-    filename : str
-        The name of the file to run sep.extract on
-    sigma : int, optional
-        The number of sigma a detection must be above the background
-        to be flagged as a star
-    ap_radius : float
-        Radius of the circular aperture for photometry
-    catalogue_coords : array-like
-        RA and Dec in degrees of the targets positions
+    cat : astropy.table.table.Table
+        Catalog containing the RA and DEC coordinates
+    filenames : list of str
+        List of filenames corresponding to each prefix
 
     Returns
     -------
-    results : list
-        List containing dictionaries with photometry results
+    x, y : numpy arrays
+        Pixel coordinates
 
     Raises
     ------
     None
     """
-    data, hdr = jhk.load_fits_image(filename)
-    data = data.astype(np.float64)
+    # Initialize lists to store pixel coordinates
+    all_x = []
+    all_y = []
 
-    bkg = extract_background(filename, sigma)
-    data_sub_bkg = data - bkg
+    # Iterate over each set of filenames corresponding to each prefix
+    for prefix_filenames in filenames:
+        # Take the first filename for the current prefix
+        filename = prefix_filenames[0]
 
-    # perform photometry on sources
-    phot_results = []
-    for coord in catalogue_coords:
-        x, y = catalogue_to_pixels(filename, [coord])
-        result = sep.sum_circle(data_sub_bkg, x[0], y[0], ap_radius)
-        phot_results.append(result)
+        # Open the FITS file and extract the WCS information
+        with fits.open(filename) as hdul:
+            wcs_header = hdul[0].header
 
-    return phot_results
+            # Convert the RA and DEC to pixel coordinates
+            wcs = WCS(wcs_header)
+            x, y = wcs.all_world2pix(cat['ra_deg_corr'], cat['dec_deg_corr'], 1)
+
+            # Append the pixel coordinates to the lists
+            all_x.append(x)
+            all_y.append(y)
+
+    # Convert lists to numpy arrays
+    all_x = np.concatenate(all_x)
+    all_y = np.concatenate(all_y)
+
+    return all_x, all_y
 
 
-def compute_background_flux(filename, ap_radius, catalogue_coords, sigma):
+def _detect_objects_sep(data, background_rms, area_min, area_max,
+                        detection_sigma, defocus_mm, trim_border=10):
     """
-    Compute background flux using circular apertures
+    Find objects in an image array using SEP
 
     Parameters
     ----------
-    filename : str
-        The name of the FITS file
-    ap_radius : float
-        Radius of the circular aperture for photometry
-    catalogue_coords : array-like
-        RA and Dec in degrees of the background regions
+    data : array
+        Image array to source detect on
+    background_rms
+        Std of the sky background
+    area_min : int
+        Minimum number of pixels for an object to be valid
+    area_max : int
+        Maximum number of pixels for an object to be valid
+    detection_sigma : float
+        Number of sigma above the background for source detecting
+    defocus_mm : float
+        Level of defocus. Used to select kernel for source detect
+    trim_border : int
+        Number of pixels to exclude from the edge of the image array
 
     Returns
     -------
-    background_flux : float
-        Background flux value
+    objects : astropy Table
+        A list of detected objects in astropy Table format
 
     Raises
     ------
     None
     """
-    data, hdr = jhk.load_fits_image(filename)
-    data = data.astype(np.float64)
+    # set up some defocused kernels for sep
+    kernel1 = np.array([[1, 1, 1, 1, 1],
+                        [1, 2, 3, 2, 1],
+                        [1, 3, 1, 3, 1],
+                        [1, 2, 3, 2, 1],
+                        [1, 1, 1, 1, 1]])
+    kernel2 = np.array([[1, 1, 1, 1, 1, 1],
+                        [1, 2, 3, 3, 2, 1],
+                        [1, 3, 1, 1, 3, 1],
+                        [1, 3, 1, 1, 3, 1],
+                        [1, 2, 3, 3, 2, 1],
+                        [1, 1, 1, 1, 1, 1]])
+    kernel3 = np.array([[1, 1, 1, 1, 1, 1, 1, 1],
+                        [1, 3, 3, 3, 3, 3, 3, 1],
+                        [1, 3, 2, 2, 2, 2, 3, 1],
+                        [1, 3, 2, 1, 1, 2, 3, 1],
+                        [1, 3, 2, 1, 1, 2, 3, 1],
+                        [1, 3, 2, 2, 2, 2, 3, 1],
+                        [1, 3, 3, 3, 3, 3, 3, 1],
+                        [1, 1, 1, 1, 1, 1, 1, 1]])
 
-    bkg = extract_background(filename, sigma)
-    data_sub_bkg = data - bkg
+    # check for defocus
+    if 0.15 <= defocus_mm < 0.3:
+        print("Source detect using defocused kernel 1")
+        raw_objects = sep.extract(data, detection_sigma * background_rms,
+                                  minarea=area_min, filter_kernel=kernel1)
+    elif 0.3 <= defocus_mm < 0.5:
+        print("Source detect using defocused kernel 2")
+        raw_objects = sep.extract(data, detection_sigma * background_rms,
+                                  minarea=area_min, filter_kernel=kernel2)
+    elif defocus_mm >= 0.5:
+        print("Source detect using defocused kernel 3")
+        raw_objects = sep.extract(data, detection_sigma * background_rms,
+                                  minarea=area_min, filter_kernel=kernel3)
+    else:
+        print("Source detect using default kernel")
+        raw_objects = sep.extract(data, detection_sigma * background_rms, minarea=area_min)
 
-    background_flux = []
-    for coord in catalogue_coords:
-        x, y = catalogue_to_pixels(filename, [coord])
-        result = sep.sum_circle(data_sub_bkg, x[0], y[0], ap_radius)
-        background_flux.append(result)
+    initial_objects = len(raw_objects)
 
-    return background_flux
+    raw_objects = Table(raw_objects[np.logical_and.reduce([
+        raw_objects['npix'] < area_max,
+        # Filter targets near the edge of the frame
+        raw_objects['xmin'] > trim_border,
+        raw_objects['xmax'] < data.shape[1] - trim_border,
+        raw_objects['ymin'] > trim_border,
+        raw_objects['ymax'] < data.shape[0] - trim_border
+    ])])
+
+    print(detection_sigma * background_rms, initial_objects, len(raw_objects))
+
+    # Astrometry.net expects 1-index pixel positions
+    objects = Table()
+    objects['X'] = raw_objects['x'] + 1
+    objects['Y'] = raw_objects['y'] + 1
+    objects['FLUX'] = raw_objects['cflux']
+    objects.sort('FLUX')
+    objects.reverse()
+    return objects
 
 
-def output_results_to_file(photometry_results, background_flux, filename, ap_radius):
+def find_max_pixel_value(data, x, y, radius):
     """
-    Output photometry results and background flux to a text file
+    Find the maximum pixel value in the image
+    in a square around the aperture centre
 
     Parameters
     ----------
-    photometry_results : list
-        List containing dictionaries with photometry results
-    background_flux : float
-        Background flux value
-    filename : str
-        Name of the output text file
+    data : array-like
+        The image to search
+    x : int
+        X coordinate of the search box
+    y : int
+        Y coordinate of the search box
+    radius : int
+        The half width of the search box
 
     Returns
     -------
-    None
+    max_pixel_value : int
+        The maximum pixel value in the area provided
 
     Raises
     ------
     None
     """
-    with open(filename, 'w') as f:
-        f.write("Object_ID Flux Flux_Error Background Background_Signal\n")
-        for idx, result in enumerate(photometry_results):
-            flux = result['flux']
-            fluxerr = result['fluxerr']
-            bkg = background_flux[idx]
-            bkg_signal = bkg * ap_radius ** 2 * np.pi
-            f.write(f"{idx + 1} {flux} {fluxerr} {bkg} {bkg_signal}\n")
+    return round(data[int(y - radius):int(y + radius),
+                 int(x - radius):int(x + radius)].ravel().max(), 2)
+
+
+def wcs_phot(data, x, y, rsi, rso, aperture_radii, gain=1.12):
+    """
+    Take a corrected image array and extract photometry for a set of WCS driven
+    X and Y pixel positions. Do this for a series of aperture radii and apply
+    a gain correction to the photometry
+    """
+    col_labels = ["flux", "fluxerr", "flux_w_sky", "fluxerr_w_sky", "max_pixel_value"]
+    Tout = None
+    for r in aperture_radii:
+        flux, fluxerr, _ = sep.sum_circle(data, x, y, r,
+                                          subpix=0,
+                                          bkgann=(rsi, rso),
+                                          gain=gain)
+        flux_w_sky, fluxerr_w_sky, _ = sep.sum_circle(data, x, y, r,
+                                                      subpix=0,
+                                                      gain=gain)
+        # calculate the max pixel value in each aperture
+        max_pixel_value = np.array([find_max_pixel_value(data, int(i), int(j), int(r + 1)) for i, j in zip(x, y)])
+        # build this photometry into a table
+        if Tout is None:
+            Tout = Table([flux, fluxerr, flux_w_sky, fluxerr_w_sky, max_pixel_value],
+                         names=tuple([f"{c}_{r}" for c in col_labels]))
+        else:
+            T = Table([flux, fluxerr, flux_w_sky, fluxerr_w_sky, max_pixel_value],
+                      names=tuple([f"{c}_{r}" for c in col_labels]))
+            # stack the new columns onto the RHS of the table
+            Tout = hstack([Tout, T])
+    return Tout
+
+
+
