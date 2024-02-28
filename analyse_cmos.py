ced1dc1a3ca4b22069fdaba4d19e27d5091e8f3f
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-28 11:20:14 +0000

    set if args are none then plot all stars phot

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -216,7 +216,7 @@ def main():
     phot_table = read_phot_file(phot_files[0])
     plot_lc(phot_table, gaia_id_to_plot, bin_size)
 
-    if 'bin' not in vars(args):
+    if vars(args) is None:
         plot_noise_vs_sqrt_flux(phot_table, bin_size)
 
     plt.show()
