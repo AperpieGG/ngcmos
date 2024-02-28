4dd0ea64ad74afb999385d25312a8b596217db49
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-28 14:41:12 +0000

    testing wotan function

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -166,14 +166,12 @@ def plot_lc_with_detrend(table, gaia_id_to_plot):
     fluxerr_2 = gaia_id_data['fluxerr_2']
 
     # Detrend the light curve
-    detrended_flux, trend = flatten(jd_mid, flux_2, method='mean', window_length=0.75, return_trend=True)
-    detrended_flux_1, trend_1 = flatten(jd_mid, flux_2, method='biweight', window_length=0.75, return_trend=True)
+    detrended_flux, trend = flatten(jd_mid, flux_2, method='mean', window_length=1, return_trend=True)
 
     # Plot jd_mid vs detrended flux
     # plt.errorbar(jd_mid, detrended_flux, yerr=fluxerr_2, fmt='o', color='black', label='Detrended Flux')
     plt.plot(jd_mid, trend, color='red', label='Trend')
     plt.plot(jd_mid, flux_2, 'o', color='black', label='Flux 2')
-    plt.plot(jd_mid, detrended_flux_1, 'o', color='blue', label='Detrended Flux 1')
 
     # Add labels and title
     plt.xlabel('MJD [days]')
