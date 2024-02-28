82a5dfb3b4bb137adda38c40068248ce213adc50
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-28 14:03:51 +0000

    moving plot_images function to utils

--- utils.py
+++ utils.py
@@ -8,10 +8,47 @@ from astropy.coordinates import SkyCoord, EarthLocation
 from astropy.wcs import WCS
 import astropy.units as u
 from astropy.table import Table, hstack
+import matplotlib.pyplot as plt
 
 # pylint: disable=invalid-name
 # pylint: disable=no-member
 # pylint: disable=c-extension-no-member
+
+
+def plot_images():
+    # Set plot parameters
+    plt.rcParams['figure.dpi'] = 100
+    plt.rcParams['xtick.top'] = True
+    plt.rcParams['xtick.labeltop'] = False
+    plt.rcParams['xtick.labelbottom'] = True
+    plt.rcParams['xtick.bottom'] = True
+    plt.rcParams['xtick.direction'] = 'in'
+    plt.rcParams['ytick.direction'] = 'in'
+    plt.rcParams['xtick.minor.visible'] = True
+    plt.rcParams['xtick.major.top'] = True
+    plt.rcParams['xtick.minor.top'] = True
+    plt.rcParams['xtick.minor.bottom'] = True
+    plt.rcParams['xtick.alignment'] = 'center'
+
+    plt.rcParams['ytick.left'] = True
+    plt.rcParams['ytick.labelleft'] = True
+    plt.rcParams['ytick.right'] = True
+    plt.rcParams['ytick.minor.visible'] = True
+    plt.rcParams['ytick.major.right'] = True
+    plt.rcParams['ytick.major.left'] = True
+    plt.rcParams['ytick.minor.right'] = True
+    plt.rcParams['ytick.minor.left'] = True
+
+    # Font and fontsize
+    plt.rcParams['font.family'] = 'serif'
+    plt.rcParams['font.size'] = 14
+
+    # Legend
+    plt.rcParams['legend.frameon'] = True
+    plt.rcParams['legend.framealpha'] = 0.8
+    plt.rcParams['legend.loc'] = 'best'
+    plt.rcParams['legend.fancybox'] = True
+    plt.rcParams['legend.fontsize'] = 14
 
 
 def get_location():
