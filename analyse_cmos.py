f8aeb007f07d91794b15d4c9bf05e80367475bed
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-29 12:17:32 +0000

    reformat the file

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -136,7 +136,6 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10):
     plt.show()
 
 
-
 def plot_lc_for_all_stars(table, bin_size=1):
     # Get unique Gaia IDs from the table
     unique_gaia_ids = np.unique(table['gaia_id'])
