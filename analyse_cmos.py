1d13b2d19bc846f73582a69f6697cddd71cc7d8d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-02-29 11:19:25 +0000

    fixing the plot

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -119,7 +119,7 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10):
     # Determine the bin label for the y-axis
     bin_label = f'binned {bin_size * exposure_time / 60:.2f} min'
 
-    fig, axs = plt.subplots(3, 2, figsize=(12, 10))
+    fig, axs = plt.subplots(3, 2, figsize=(16, 10))
 
     # Plot jd_mid vs flux_2
     for i in range(5):
