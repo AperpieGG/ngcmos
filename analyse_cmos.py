32d59ffd3c175429c2e3843ad1699f424a33932a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-01 11:52:44 +0000

    fixing the binned data

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -113,11 +113,11 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10):
 
     # Bin the data
     jd_mid_binned = [np.mean(jd_mid[i:i + bin_size]) for i in range(0, len(jd_mid), bin_size)]
-    fluxes_binned = [[np.mean(fluxes[i][j:j + bin_size]) for j in range(2, len(fluxes[i]), bin_size)] for i in range(5)]
-    fluxerrs_binned = [[np.sqrt(np.sum(fluxerrs[i][j:j + bin_size] ** 2)) / bin_size for j in range(2, len(fluxerrs[i]), bin_size)] for i in range(5)]
-    sky_binned = [[np.mean(sky[i][j:j + bin_size]) for j in range(2, len(sky[i]), bin_size)] for i in range(5)]
-    skyerrs_binned = [[np.sqrt(np.sum(skyerrs[i][j:j + bin_size] ** 2)) / bin_size for j in range(2, len(skyerrs[i]), bin_size)] for i in range(5)]
-    print(len(jd_mid_binned), len(fluxes_binned[0]), len(sky_binned[0]), len(x), len(y))
+    fluxes_binned = [[np.mean(fluxes[i][j:j + bin_size]) for j in range(0, len(fluxes[i]), bin_size)] for i in range(5)]
+    fluxerrs_binned = [[np.sqrt(np.sum(fluxerrs[i][j:j + bin_size] ** 2)) / bin_size for j in range(0, len(fluxerrs[i]), bin_size)] for i in range(5)]
+    sky_binned = [[np.mean(sky[i][j:j + bin_size]) for j in range(0, len(sky[i]), bin_size)] for i in range(5)]
+    skyerrs_binned = [[np.sqrt(np.sum(skyerrs[i][j:j + bin_size] ** 2)) / bin_size for j in range(0, len(skyerrs[i]), bin_size)] for i in range(5)]
+
     # Determine the bin label for the y-axis
     bin_label = f'binned {bin_size * exposure_time / 60:.2f} min'
 
