25a1602edef5b596f1af48298211d487786103dd
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-01 11:48:37 +0000

    fixing the jd_binned

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -115,7 +115,6 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10):
     # Bin the data
     jd_mid_binned = [np.mean(jd_mid[i:i + bin_size]) for i in range(0, len(jd_mid), bin_size)]
     fluxes_binned = [[np.mean(fluxes[i][j:j + bin_size]) for j in range(2, len(fluxes[i]), bin_size)] for i in range(5)]
-    print(len(fluxes_binned, len(jd_mid_binned)))
     fluxerrs_binned = [[np.sqrt(np.sum(fluxerrs[i][j:j + bin_size] ** 2)) / bin_size for j in range(2, len(fluxerrs[i]), bin_size)] for i in range(5)]
     sky_binned = [[np.mean(sky[i][j:j + bin_size]) for j in range(2, len(sky[i]), bin_size)] for i in range(5)]
     skyerrs_binned = [[np.sqrt(np.sum(skyerrs[i][j:j + bin_size] ** 2)) / bin_size for j in range(2, len(skyerrs[i]), bin_size)] for i in range(5)]
@@ -136,6 +135,7 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10):
         axs[row, col].legend()
 
     # Plot x, y positions
+    print(len(jd_mid), len(x), len(y))
     axs[2, 1].plot(jd_mid, x, 'o', color='red', label='X Position')
     axs[2, 1].plot(jd_mid, y, 'o', color='green', label='Y Position')
     axs[2, 1].set_xlabel('MJD [days]')
