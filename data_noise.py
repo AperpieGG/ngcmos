df6acae3881645aa0a58000c2bcc7c9bb83687ca
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-04 11:29:45 +0000

    start over

--- data_noise.py
+++ data_noise.py
@@ -1,11 +1,14 @@
-#! /usr/bin/env python
-import numpy as np
-import os
-from matplotlib import pyplot as plt
+#!/usr/bin/env python
+import argparse
+import datetime
 import json
-from astropy.io import fits
-from datetime import datetime, timedelta
+import os
 import fnmatch
+from datetime import datetime, timedelta
+import numpy as np
+from astropy.io import fits
+from matplotlib import pyplot as plt
+from utils import plot_images
 
 
 def load_config(filename):
@@ -30,6 +33,16 @@ def find_current_night_directory(directory):
     """
     Find the directory for the current night based on the current date.
     If not found, use the current working directory.
+
+    Parameters
+    ----------
+    directory : str
+        Base path for the directory.
+
+    Returns
+    -------
+    str
+        Path to the current night directory.
     """
     previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
     current_date_directory = os.path.join(directory, previous_date)
@@ -39,6 +52,16 @@ def find_current_night_directory(directory):
 def get_phot_files(directory):
     """
     Get photometry files with the pattern 'phot_*.fits' from the directory.
+
+    Parameters
+    ----------
+    directory : str
+        Directory containing the files.
+
+    Returns
+    -------
+    list of str
+        List of photometry files matching the pattern.
     """
     phot_files = []
     for filename in os.listdir(directory):
@@ -50,9 +73,21 @@ def get_phot_files(directory):
 def read_phot_file(filename):
     """
     Read the photometry file.
+
+    Parameters
+    ----------
+    filename : str
+        Photometry file to read.
+
+    Returns
+    -------
+    astropy.table.table.Table
+        Table containing the photometry data.
     """
+    # Read the photometry file here using fits or any other appropriate method
     try:
         with fits.open(filename) as ff:
+            # Access the data in the photometry file as needed
             tab = ff[1].data
             return tab
     except Exception as e:
@@ -60,156 +95,168 @@ def read_phot_file(filename):
         return None
 
 
-def plot_images():
-    plt.rcParams['figure.dpi'] = 100
-    plt.rcParams['xtick.top'] = True
-    plt.rcParams['xtick.labeltop'] = False
-    plt.rcParams['xtick.labelbottom'] = True
-    plt.rcParams['xtick.bottom'] = True
-    plt.rcParams['xtick.direction'] = 'in'
-    plt.rcParams['ytick.direction'] = 'in'
-    plt.rcParams['xtick.minor.visible'] = True
-    plt.rcParams['xtick.major.top'] = True
-    plt.rcParams['xtick.minor.top'] = True
-    plt.rcParams['xtick.minor.bottom'] = True
-    plt.rcParams['xtick.alignment'] = 'center'
-    plt.rcParams['ytick.left'] = True
-    plt.rcParams['ytick.labelleft'] = True
-    plt.rcParams['ytick.right'] = True
-    plt.rcParams['ytick.minor.visible'] = True
-    plt.rcParams['ytick.major.right'] = True
-    plt.rcParams['ytick.major.left'] = True
-    plt.rcParams['ytick.minor.right'] = True
-    plt.rcParams['ytick.minor.left'] = True
-    plt.rcParams['font.size'] = 14
-    plt.rcParams['legend.frameon'] = True
-    plt.rcParams['legend.framealpha'] = 0.8
-    plt.rcParams['legend.loc'] = 'best'
-    plt.rcParams['legend.fancybox'] = True
-    plt.rcParams['legend.fontsize'] = 14
-
-
 def bin_time_flux_error(time, flux, error, bin_fact):
+    """
+    Use reshape to bin light curve data, clip under filled bins
+    Works with 2D arrays of flux and errors
+
+    Note: under filled bins are clipped off the end of the series
+
+    Parameters
+    ----------
+    time : array         of times to bin
+    flux : array         of flux values to bin
+    error : array         of error values to bin
+    bin_fact : int
+        Number of measurements to combine
+
+    Returns
+    -------
+    times_b : array
+        Binned times
+    flux_b : array
+        Binned fluxes
+    error_b : array
+        Binned errors
+
+    Raises
+    ------
+    None
+    """
     n_binned = int(len(time) / bin_fact)
     clip = n_binned * bin_fact
     time_b = np.average(time[:clip].reshape(n_binned, bin_fact), axis=1)
+    # determine if 1 or 2d flux/err inputs
     if len(flux.shape) == 1:
         flux_b = np.average(flux[:clip].reshape(n_binned, bin_fact), axis=1)
         error_b = np.sqrt(np.sum(error[:clip].reshape(n_binned, bin_fact) ** 2, axis=1)) / bin_fact
     else:
+        # assumed 2d with 1 row per star
         n_stars = len(flux)
         flux_b = np.average(flux[:clip].reshape((n_stars, n_binned, bin_fact)), axis=2)
         error_b = np.sqrt(np.sum(error[:clip].reshape((n_stars, n_binned, bin_fact)) ** 2, axis=2)) / bin_fact
     return time_b, flux_b, error_b
 
 
-def scintillation_noise():
-    t = 10  # exposure time
-    D = 20  # telescope diameter
-    secZ = 1.2  # airmass
-    h = 2433  # height of Paranal
-    ho = 8000  # height of atmospheric scale
-    W = 1.75  # wind speed
-    N = 0.09 * (D ** (-2 / 3) * secZ ** W * np.exp(-h / ho)) * (2 * t) ** (-1 / 2)
-    return N
+def calculate_mean_rms_binned(table, bin_size, num_stars):
+    mean_flux_list = []
+    RMS_list = []
+    RMS_unbinned_list = []
+
+    for gaia_id in table['gaia_id'][:num_stars]:  # Selecting the first num_stars stars
+        gaia_id_data = table[table['gaia_id'] == gaia_id]
+        jd_mid = gaia_id_data['jd_mid']
+        flux_2 = gaia_id_data['flux_2']
+        fluxerr_2 = gaia_id_data['fluxerr_2']
+
+        trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_2, 2), jd_mid - int(jd_mid[0]))
+        dt_flux = flux_2 / trend
+        dt_fluxerr = fluxerr_2 / trend
+
+        time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, bin_size)
+        print(len(time_binned), len(dt_flux_binned), len(dt_fluxerr_binned))
+        print(len(jd_mid), len(dt_flux), len(dt_fluxerr))
+
+        # Calculate mean flux and RMS
+        mean_flux = np.mean(flux_2)
+        RMS = np.std(dt_flux_binned)
+        rms_unbinned = np.std(dt_flux)
+
+        # Append to lists
+        mean_flux_list.append(mean_flux)
+        RMS_list.append(RMS)
+        RMS_unbinned_list.append(rms_unbinned)
+
+    print(f"The length of the RMS list is {len(RMS_list)}")
+    print(f"The length of the RMS unbinned list is {len(RMS_unbinned_list)}")
+    return mean_flux_list, RMS_list, RMS_unbinned_list
 
 
-def noise_model(flux, photon_shot_noise, sky_noise, read_noise, dc_noise, N):
-    N_sc = (N * flux) ** 2
-    total_noise = np.sqrt(photon_shot_noise + read_noise + dc_noise + N_sc + sky_noise ** 2)
-    photon_shot_noise /= flux
-    sky_noise /= flux
-    read_noise /= flux
-    dc_noise /= flux
-    N_sc /= flux
-    fig, ax = plt.subplots(figsize=(6, 8))
-    ax.plot(flux, photon_shot_noise, color='green', label='photon shot', linestyle='--')
-    ax.plot(flux, read_noise, color='red', label='read noise', linestyle='--')
-    ax.plot(flux, dc_noise, color='purple', label='dark noise', linestyle='--')
-    ax.plot(flux, sky_noise, color='blue', label='sky bkg', linestyle='--')
-    ax.plot(flux, N, color='orange', label='scintillation noise', linestyle='--')
-    ax.plot(flux, total_noise, color='black', label='total noise')
-    ax.set_xlabel('Flux')
-    ax.set_ylabel('RMS (mag)')
-    ax.set_xscale('log')
+def plot_noise_model(mean_flux_list, RMS_list, RMS_unbinned_list):
+    # Plot the noise model
+    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
+    ax.plot(mean_flux_list, RMS_list, 'o', color='black', label='Noise Model (Binned)')
+    ax.plot(mean_flux_list, RMS_unbinned_list, 'o', color='red', label='Unbinned')
+    ax.set_xlabel('Mean Flux [e-]')
+    ax.set_ylabel('RMS [e-]')
+    ax.set_title('Noise Model')
     ax.set_yscale('log')
+    ax.set_xscale('log')
+    ax.legend()
     plt.tight_layout()
-    plt.legend(loc='best')
+    plt.show()
+
+
+def plot_lc_with_detrend(table, gaia_id_to_plot):
+    # Select rows with the specified Gaia ID
+    gaia_id_data = table[table['gaia_id'] == gaia_id_to_plot]
+    # Get jd_mid, flux_2, and fluxerr_2 for the selected rows
+    jd_mid = gaia_id_data['jd_mid']
+    flux_2 = gaia_id_data['flux_2']
+    fluxerr_2 = gaia_id_data['fluxerr_2']
+    tmag = gaia_id_data['Tmag'][0]
+
+    # flatten_lc, trend = flatten(jd_mid, flux_2, window_length=0.01, return_trend=True, method='biweight')
+    # use polyfit to detrend the light curve
+    trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_2, 2), jd_mid - int(jd_mid[0]))
+
+    # Compute Detrended flux and errors
+    norm_flux = flux_2 / trend
+    relative_err = fluxerr_2 / trend
+    rms = np.std(norm_flux)
+    print(f"RMS for Gaia ID {gaia_id_to_plot} = {rms:.2f}")
+
+    # Create subplots
+    fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
+
+    # Plot raw flux with wotan model
+    ax1.plot(jd_mid, flux_2, 'o', color='black', label='Raw Flux 2')
+    ax1.plot(jd_mid, trend, color='red', label='Wotan Model')
+    ax1.set_title(f'Detrended LC for Gaia ID {gaia_id_to_plot} (Tmag = {tmag:.2f})')
+    ax1.set_xlabel('MJD [days]')
+    ax1.set_ylabel('Flux [e-]')
+    ax1.legend()
+
+    ax2.errorbar(jd_mid, norm_flux, yerr=relative_err, fmt='o', color='black', label='Detrended Flux')
+    ax2.set_ylabel('Detrended Flux [e-]')
+    ax2.set_xlabel('MJD [days]')
+    ax2.legend()
+
+    plt.tight_layout()
     plt.show()
 
 
 def main():
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Plot light curve for a specific Gaia ID')
+    parser.add_argument('--gaia_id', type=int, help='The Gaia ID of the star to plot')
+    parser.add_argument('--bin', type=int, default=1, help='Number of images to bin')
+    args = parser.parse_args()
+    gaia_id_to_plot = args.gaia_id
+    bin_size = args.bin
+
+    # Set plot parameters
     plot_images()
+
+    # Get the current night directory
     current_night_directory = find_current_night_directory(base_path)
+
+    # Get photometry files with the pattern 'phot_*.fits'
     phot_files = get_phot_files(current_night_directory)
-    num_stars = 50
-    bin_size = 60
+    print(f"Photometry files: {phot_files}")
 
-    all_flux = []
-    all_sky = []
-    all_photon_shot_noise = []
-    all_sky_noise = []
-    all_dc_noise = []
-    all_read_noise = []
-    all_N = []
+    # Plot the first photometry file
+    print(f"Plotting the first photometry file {phot_files[0]}...")
+    phot_table = read_phot_file(phot_files[0])
 
-    if phot_files:
-        for phot_file in phot_files:
-            phot_table = read_phot_file(phot_file)
-            if phot_table is not None:
-                for gaia_id in phot_table['gaia_id'][:num_stars]:
-                    gaia_id_data = phot_table[phot_table['gaia_id'] == gaia_id]
-                    jd_mid = gaia_id_data['jd_mid']
-                    flux_2 = gaia_id_data['flux_2']
-                    fluxerr_2 = gaia_id_data['fluxerr_2']
-                    flux_w_sky_2 = gaia_id_data['flux_w_sky_2']
-                    sky_2 = flux_w_sky_2 - flux_2
-
-                    trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_2, 2), jd_mid - int(jd_mid[0]))
-                    dt_flux = flux_2 / trend
-                    dt_fluxerr = fluxerr_2 / trend
-
-                    time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr,
-                                                                                         bin_size)
-
-                    mean_flux = np.mean(flux_2)
-                    mean_sky = np.mean(sky_2)
-
-                    flux = mean_flux
-                    sky = mean_sky
-                    photon_shot_noise = np.sqrt(flux) / flux
-                    sky_noise = np.sqrt(sky) / flux
-
-                    aperture_radius = 3
-                    npix = np.pi * aperture_radius ** 2
-
-                    exposure_time = 10
-                    dark_current_rate = 0.66
-                    dark_current = dark_current_rate * exposure_time * npix
-                    dc_noise = np.sqrt(dark_current) / flux
-
-                    read_noise_pix = 1.56
-                    read_noise = (read_noise_pix * npix) / flux
-
-                    N = scintillation_noise()
-
-                    all_flux.append(flux)
-                    all_sky.append(sky)
-                    all_photon_shot_noise.append(photon_shot_noise)
-                    all_sky_noise.append(sky_noise)
-                    all_dc_noise.append(dc_noise)
-                    all_read_noise.append(read_noise)
-                    all_N.append(N)
-
-    all_flux = np.array(all_flux)
-    all_sky = np.array(all_sky)
-    all_photon_shot_noise = np.array(all_photon_shot_noise)
-    all_sky_noise = np.array(all_sky_noise)
-    all_dc_noise = np.array(all_dc_noise)
-    all_read_noise = np.array(all_read_noise)
-    all_N = np.array(all_N)
-
-    noise_model(all_flux, all_photon_shot_noise, all_sky_noise, all_read_noise, all_dc_noise, all_N)
+    # Plot the light curve for the specified Gaia ID
+    if gaia_id_to_plot:
+        plot_lc_with_detrend(phot_table, gaia_id_to_plot)
+    else:
+        # Calculate mean and RMS for the noise model
+        mean_flux_list, RMS_list, RMS_unbinned_list = calculate_mean_rms_binned(phot_table, bin_size=60,
+                                                                                num_stars=100)
+        plot_noise_model(mean_flux_list, RMS_list, RMS_unbinned_list)
 
 
 if __name__ == "__main__":
