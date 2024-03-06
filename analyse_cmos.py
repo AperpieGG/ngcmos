c868428c671bf51ad1bb7d2ef16668d0da63d481
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-06 16:40:02 +0000

    adding airmass to the plot

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -214,7 +214,7 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10, image_director
 
     # Get image data based on frame_id
     image_data = get_image_data(gaia_id_data['frame_id'][0], image_directory)
-    image_header = fits.getheader(gaia_id_data['frame_id'][0], image_directory)
+    image_header = fits.getheader(os.path.join(image_directory, gaia_id_data['frame_id'][0]))
     airmass = image_header['AIRMASS']
     print(f"The star has GAIA id: {gaia_id_to_plot}")
 
