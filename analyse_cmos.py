a5847d4a5c7e89f5546648565830aaecd7350c98
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-07 13:03:06 +0000

    trying to plot airmass

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -268,10 +268,13 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10, image_director
         axs[0].set_ylabel('Flux [e-]')
         axs[0].legend()
 
-        # Create twin axis for airmass on top of the plot
         ax2 = axs[0].twiny()
-        ax2.set_xlim(min(airmass), max(airmass), 0.1)
+        ax2.set_xlim(min(airmass), max(airmass))  # Set the limits based on airmass values
         ax2.set_xlabel('Airmass')
+
+        # Set the ticks of the twin axis to be the same as the original x-axis
+        ax2.set_xticks(axs[0].get_xticks())
+        ax2.set_xticklabels(axs[0].get_xticklabels())
 
         # Plot jd_mid vs sky
         axs[1].errorbar(jd_mid_binned, sky_binned, yerr=skyerrs_binned, fmt='o', color='red', label='Sky')
