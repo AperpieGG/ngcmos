b992ecec55a7f52fe4327d3d11cd73263968efef
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-07 15:38:00 +0000

    change the figsize

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -268,7 +268,7 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10, image_director
         axs[0].set_ylabel('Flux [e-]')
         axs[0].legend()
 
-        ax2 = axs[0].twiny()
+        ax2 = axs[0].twinx()
         ax2.set_xlim(min(airmass), max(airmass))  # Set the limits based on airmass values
         # ax2.set_xscale('log')
         ax2.set_xlabel('Airmass')
