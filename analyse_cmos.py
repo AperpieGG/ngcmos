f9527154f844bbdaedf0e15943c611e5bf5e8582
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-07 15:42:14 +0000

    plot airmass

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -270,7 +270,7 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10, image_director
 
         ax2 = axs[0].twinx()
         ax2.set_ylim(min(airmass), max(airmass))  # Set the limits based on airmass values
-        ax2.invert_yaxis()
+        # ax2.invert_yaxis()
         # ax2.set_xscale('log')
         ax2.set_ylabel('Airmass')
 
