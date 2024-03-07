e6e00cec00ca4e270ce166ac354e7e70bae97cee
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-07 13:12:51 +0000

    fixing airmass plot

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -272,6 +272,9 @@ def plot_lc(table, gaia_id_to_plot, bin_size=1, exposure_time=10, image_director
         ax2.set_xlim(min(airmass), max(airmass))  # Set the limits based on airmass values
         ax2.set_xlabel('Airmass')
 
+        desired_ticks = 20
+        ax2.xaxis.set_major_locator(plt.MaxNLocator(desired_ticks))
+
         # Plot jd_mid vs sky
         axs[1].errorbar(jd_mid_binned, sky_binned, yerr=skyerrs_binned, fmt='o', color='red', label='Sky')
         axs[1].set_ylabel('Flux [e-]')
