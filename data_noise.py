50bf3cb7142da642ea095664f86c931773dba072
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-12 10:24:42 +0000

    setting max fluxes to plot

--- data_noise.py
+++ data_noise.py
@@ -154,7 +154,7 @@ def calculate_mean_rms_binned(table, bin_size, num_stars):
         sky_3 = gaia_id_data['flux_w_sky_3'] - gaia_id_data['flux_3']
 
         # exclude stars with flux > 200000
-        if np.max(flux_3) < 200000:
+        if np.max(flux_3) > 200000:
             continue
 
         trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_3, 2), jd_mid - int(jd_mid[0]))
