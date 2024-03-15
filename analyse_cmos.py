d5f808c1b15430c01ed4e5d09ad712b59f04d6f4
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-15 09:03:28 +0000

    change phot file to display

--- analyse_cmos.py
+++ analyse_cmos.py
@@ -351,8 +351,8 @@ def main():
     print(f"Photometry files: {phot_files}")
 
     # Plot the first photometry file
-    print(f"Plotting the first photometry file {phot_files[0]}...")
-    phot_table = read_phot_file(phot_files[0])
+    print(f"Plotting the first photometry file {phot_files[2]}...")
+    phot_table = read_phot_file(phot_files[2])
 
     if gaia_id_to_plot is None:
         plot_lc_for_all_stars(phot_table, bin_size)
