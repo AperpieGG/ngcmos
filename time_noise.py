bc497eba5c79fd92e20cf9c748b0de017164b98f
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-15 09:33:04 +0000

    fixing noise bin model

--- time_noise.py
+++ time_noise.py
@@ -184,10 +184,11 @@ def plot_rms_time(table, num_of_stars):
     average_rms_values = np.mean(average_rms_values, axis=0)
 
     # Generate binning times
-    binning_times = [i * 10 for i in range(1, max_binning)]
+    exposure_time = 10
+    binning_times = [i * exposure_time for i in range(1, max_binning)]
 
     # Calculate the expected decrease in RMS
-    RMS_model = average_rms_values[0] / np.sqrt(binning_times)
+    RMS_model = average_rms_values / np.sqrt(binning_times)
 
     # Plot RMS as a function of exposure time along with the expected decrease in RMS
     plt.figure(figsize=(10, 8))
