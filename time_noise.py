b517382b992fdd84d59835ac7aef1d93f3fd1de3
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-15 11:10:27 +0000

    fixing labels

--- time_noise.py
+++ time_noise.py
@@ -191,16 +191,15 @@ def plot_rms_time(table, num_stars):
 
     # Generate binning times
     # binning_times = [i * 10 for i in range(1, max_binning)]
-    exposure_time_per_image = 10  # seconds per image
-    binning_times = [i * binning_times * exposure_time_per_image for i in range(1, max_binning)]
+    binning_times = [i for i in range(1, max_binning)]
 
     # Calculate the expected decrease in RMS
     RMS_model = average_rms_values[0] / np.sqrt(binning_times)
 
     # Plot RMS as a function of exposure time along with the expected decrease in RMS
     plt.figure(figsize=(10, 8))
-    plt.plot(binning_times, average_rms_values, 'o', color='black', label='Actual RMS', alpha=0.5)
-    plt.plot(binning_times, RMS_model, '--', color='red', label='Model RMS')
+    plt.plot(time_binned, average_rms_values, 'o', color='black', label='Actual RMS', alpha=0.5)
+    plt.plot(time_binned, RMS_model, '--', color='red', label='Model RMS')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('Exposure time (s)')
