433fc96374ce4785768aeb011aca3e015b0deb2a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-15 14:03:48 +0000

    adding printing statements

--- time_noise.py
+++ time_noise.py
@@ -195,20 +195,17 @@ def plot_rms_time(table, num_stars):
     time_seconds = times_binned[0]
     print(time_seconds)
 
-    print(len(average_rms_values))
-    print(len(time_seconds))
-
     # Generate binning times
     binning_times = [i for i in range(1, max_binning)]
     # binning_times = [i for i in range(1, max_binning)]
 
     # Calculate the expected decrease in RMS
-    RMS_model = average_rms_values[0] / np.sqrt(binning_times)
+    RMS_model = average_rms_values[0] / np.sqrt(time_seconds)
 
     # Plot RMS as a function of exposure time along with the expected decrease in RMS
     plt.figure(figsize=(10, 8))
-    plt.plot(binning_times, average_rms_values, 'o', color='black', label='Actual RMS', alpha=0.5)
-    plt.plot(binning_times, RMS_model, '--', color='red', label='Model RMS')
+    plt.plot(time_seconds, average_rms_values, 'o', color='black', label='Actual RMS', alpha=0.5)
+    plt.plot(time_seconds, RMS_model, '--', color='red', label='Model RMS')
     plt.xscale('log')
     plt.yscale('log')
     plt.xlabel('Exposure time (s)')
