2c7124045444f3ce7e1c809ec5c55cade39c0a22
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-18 19:01:06 +0000

    remove the sorting function

--- single_rms_vs_time.py
+++ single_rms_vs_time.py
@@ -176,7 +176,7 @@ def plot_rms_time(table, num_stars, gaia_id=None):
             time_seconds.append(exposure_time_seconds)
 
         # Check if the first RMS value is greater than 0.0065
-        if RMS_values[0] > 0.01:
+        if RMS_values[0] > 0.007:
             print('Excluding star with gaia_id = {} and Tmag = {:.2f} due to RMS > 0.005'.format(current_gaia_id, Tmag))
             num_stars_excluded += 1
             continue
