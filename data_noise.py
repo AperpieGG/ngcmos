24c4d84e6b5cdc2915fb5c7ed7920cfcefca88fe
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-18 10:14:35 +0000

    add labels

--- data_noise.py
+++ data_noise.py
@@ -185,6 +185,8 @@ def calculate_mean_rms_flux(table, bin_size, num_stars):
     plt.xlabel('RMS')
     plt.ylabel('Frequency')
     plt.title('Histogram of RMS Distribution')
+    plt.legend()
+    plt.xlim(0, 2)
     plt.show()
 
     # plt.figure(figsize=(10, 6))
