0e8fdd4bff5aa11310cbe49067fc1927809a13e3
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-18 19:47:57 +0000

    first commit

---
+++ find_zeropoint.py
@@ -0,0 +1,245 @@
+#!/usr/bin/env python
+import argparse
+import datetime
+import json
+import os
+import fnmatch
+from datetime import datetime, timedelta
+import numpy as np
+from astropy.io import fits
+from matplotlib import pyplot as plt
+from utils import plot_images
+from scipy.stats import linregress
+
+
+def load_config(filename):
+    with open(filename, 'r') as file:
+        config = json.load(file)
+    return config
+
+
+# Load paths from the configuration file
+config = load_config('directories.json')
+calibration_paths = config["calibration_paths"]
+base_paths = config["base_paths"]
+out_paths = config["out_paths"]
+
+# Select directory based on existence
+for calibration_path, base_path, out_path in zip(calibration_paths, base_paths, out_paths):
+    if os.path.exists(base_path):
+        break
+
+
+def find_current_night_directory(directory):
+    """
+    Find the directory for the current night based on the current date.
+    If not found, use the current working directory.
+
+    Parameters
+    ----------
+    directory : str
+        Base path for the directory.
+
+    Returns
+    -------
+    str
+        Path to the current night directory.
+    """
+    previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
+    current_date_directory = os.path.join(directory, previous_date)
+    return current_date_directory if os.path.isdir(current_date_directory) else os.getcwd()
+
+
+def get_phot_files(directory):
+    """
+    Get photometry files with the pattern 'phot_*.fits' from the directory.
+
+    Parameters
+    ----------
+    directory : str
+        Directory containing the files.
+
+    Returns
+    -------
+    list of str
+        List of photometry files matching the pattern.
+    """
+    phot_files = []
+    for filename in os.listdir(directory):
+        if fnmatch.fnmatch(filename, 'phot_*.fits'):
+            phot_files.append(filename)
+    return phot_files
+
+
+def read_phot_file(filename):
+    """
+    Read the photometry file.
+
+    Parameters
+    ----------
+    filename : str
+        Photometry file to read.
+
+    Returns
+    -------
+    astropy.table.table.Table
+        Table containing the photometry data.
+    """
+    # Read the photometry file here using fits or any other appropriate method
+    try:
+        with fits.open(filename) as ff:
+            # Access the data in the photometry file as needed
+            tab = ff[1].data
+            return tab
+    except Exception as e:
+        print(f"Error reading photometry file {filename}: {e}")
+        return None
+
+
+def bin_time_flux_error(time, flux, error, bin_fact):
+    """
+    Use reshape to bin light curve data, clip under filled bins
+    Works with 2D arrays of flux and errors
+
+    Note: under filled bins are clipped off the end of the series
+
+    Parameters
+    ----------
+    time : array         of times to bin
+    flux : array         of flux values to bin
+    error : array         of error values to bin
+    bin_fact : int
+        Number of measurements to combine
+
+    Returns
+    -------
+    times_b : array
+        Binned times
+    flux_b : array
+        Binned fluxes
+    error_b : array
+        Binned errors
+
+    Raises
+    ------
+    None
+    """
+    n_binned = int(len(time) / bin_fact)
+    clip = n_binned * bin_fact
+    time_b = np.average(time[:clip].reshape(n_binned, bin_fact), axis=1)
+    # determine if 1 or 2d flux/err inputs
+    if len(flux.shape) == 1:
+        flux_b = np.average(flux[:clip].reshape(n_binned, bin_fact), axis=1)
+        error_b = np.sqrt(np.sum(error[:clip].reshape(n_binned, bin_fact) ** 2, axis=1)) / bin_fact
+    else:
+        # assumed 2d with 1 row per star
+        n_stars = len(flux)
+        flux_b = np.average(flux[:clip].reshape((n_stars, n_binned, bin_fact)), axis=2)
+        error_b = np.sqrt(np.sum(error[:clip].reshape((n_stars, n_binned, bin_fact)) ** 2, axis=2)) / bin_fact
+    return time_b, flux_b, error_b
+
+
+def calculate_mean_rms_flux(table, bin_size, num_stars):
+
+    mean_flux_list = []
+    RMS_list = []
+    sky_list = []
+    tmag_list = []
+    low_rms_gaia_ids = []  # Array to store gaia_id values for stars with RMS < 0.005
+
+    for gaia_id in table['gaia_id'][:num_stars]:  # Selecting the first num_stars stars
+        gaia_id_data = table[table['gaia_id'] == gaia_id]
+        Tmag = gaia_id_data['Tmag'][0]
+        jd_mid = gaia_id_data['jd_mid']
+        flux_4 = gaia_id_data['flux_6']
+        fluxerr_4 = gaia_id_data['fluxerr_6']
+        sky_4 = gaia_id_data['flux_w_sky_6'] - gaia_id_data['flux_6']
+
+        # if np.max(sky_4) > np.max(flux_4):
+        #     print('Sky flux is greater than flux for gaia_id = {} and Tmag = {:.2f}'.format(gaia_id, Tmag))
+        #     continue
+
+        # # exclude stars with flux > 200000
+        # if np.max(flux_4) > 200000:
+        #     print('Stars with gaia_id = {} and Tmag = {:.2f} have been excluded'.format(gaia_id, Tmag))
+        #     continue
+
+        trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_4, 2), jd_mid - int(jd_mid[0]))
+        dt_flux = flux_4 / trend
+        dt_fluxerr = fluxerr_4 / trend
+
+        time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, bin_size)
+
+        # Calculate mean flux and RMS
+        mean_flux = np.mean(flux_4)
+        RMS = np.std(dt_flux_binned)
+        mean_sky = np.median(sky_4)
+
+        # Append to lists
+        mean_flux_list.append(mean_flux)
+        RMS_list.append(RMS)
+        sky_list.append(mean_sky)
+        tmag_list.append(Tmag)
+
+        # Store gaia_id for stars with RMS lower than 0.005
+        if RMS < 0.005:
+            low_rms_gaia_ids.append(gaia_id)
+
+    print('The mean RMS is: ', np.mean(RMS_list))
+    print('Gaia IDs with RMS < 0.005:', low_rms_gaia_ids)  # Print the array of gaia_id values for low RMS stars
+
+    plt.figure(figsize=(10, 6))
+    plt.plot(tmag_list, np.log(mean_flux_list), 'o', color='black', alpha=0.5)
+    plt.gca().invert_xaxis()
+    plt.xlabel('Tmag')
+    plt.ylabel('Mean Flux (log scale)')
+    plt.title('Tmag vs Mean Flux')
+    plt.show()
+
+    # Fit a linear model
+    coefficients = np.polyfit(tmag_list, np.log(mean_flux_list), 1)
+    slope = coefficients[0]  # Slope (zeropoint)
+
+    # Plot the linear fit
+    plt.plot(tmag_list, coefficients[0] * tmag_list + coefficients[1], '--', color='red', label='Linear fit')
+    print(f"The slope of the linear fit is: {slope:.2f}")
+    plt.legend()
+    plt.show()
+
+    return mean_flux_list, RMS_list, sky_list
+
+
+def main(phot_file):
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Plot light curve for a specific Gaia ID')
+    parser.add_argument('--num_stars', type=int, default=100, help='Number of stars to plot')
+    args = parser.parse_args()
+
+    # Set plot parameters
+    plot_images()
+
+    # Get the current night directory
+    current_night_directory = find_current_night_directory(base_path)
+
+    # Plot the current photometry file
+    print(f"Plotting the photometry file {phot_file}...")
+    phot_table = read_phot_file(os.path.join(current_night_directory, phot_file))
+
+    calculate_mean_rms_flux(phot_table, bin_size=1, num_stars=args.num_stars)
+
+
+def main_loop(phot_files):
+    for phot_file in phot_files:
+        main(phot_file)
+
+
+if __name__ == "__main__":
+    # Get the current night directory
+    current_night_directory = find_current_night_directory(base_path)
+
+    # Get photometry files with the pattern 'phot_*.fits'
+    phot_files = get_phot_files(current_night_directory)
+    print(f"Photometry files: {phot_files}")
+
+    # Run the main function for each photometry file
+    main_loop(phot_files)
