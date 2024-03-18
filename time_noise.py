65d42c1cbe7c619818f507f0bb7e0a255def4bbf
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-18 16:04:47 +0000

    adding printing statements

--- time_noise.py
+++ time_noise.py
@@ -190,15 +190,15 @@ def plot_rms_time(table, num_stars):
             RMS_values.append(RMS)
             time_seconds.append(exposure_time_seconds)
 
+        print('The RMS values for the star with gaia_id = {} and Tmag = {:.2f} are: {:.4f}'.
+              format(gaia_id, Tmag, RMS_values[0]))
+
         average_rms_values.append(RMS_values)
 
         times_binned.append(time_seconds)
 
         if RMS_values[0] > 0.005:
             continue
-
-        print('The RMS values for the star with gaia_id = {} and Tmag = {:.2f} are: {:.4f}'.
-              format(gaia_id, Tmag, RMS_values[0]))
 
         # Stop if the number of stars used reaches the specified number
         if num_stars_used >= num_stars:
