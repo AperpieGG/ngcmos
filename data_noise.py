adefe6e86aac8502771e9f60c9b5a2a3e44347f2
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-19 15:23:20 +0000

    remove pringin statements

--- data_noise.py
+++ data_noise.py
@@ -184,9 +184,6 @@ def calculate_mean_rms_flux(table, bin_size, num_stars):
     print('The mean RMS is: ', np.mean(RMS_list))
     # print('Gaia IDs with RMS < 0.005:', low_rms_gaia_ids)  # Print the array of gaia_id values for low RMS stars
 
-    print('Average sky binned: ', len(sky_list))
-    print('Average sky: ', len(sky_4))
-
     return mean_flux_list, RMS_list, sky_list
 
 
