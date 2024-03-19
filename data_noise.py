98e72ca75d0602da1468b55201ee43a49a1f5cfc
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-19 10:05:11 +0000

    remove sky_4 bin function

--- data_noise.py
+++ data_noise.py
@@ -167,12 +167,12 @@ def calculate_mean_rms_flux(table, bin_size, num_stars):
 
         time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, bin_size)
 
-        _, sky_binned, skyerrs_binned = bin_time_flux_error(jd_mid, sky_4, skyerrs_4, bin_size)
+        # _, sky_binned, skyerrs_binned = bin_time_flux_error(jd_mid, sky_4, skyerrs_4, bin_size)
 
         # Calculate mean flux and RMS
         mean_flux = np.mean(dt_flux_binned)
         RMS = np.std(dt_flux_binned)
-        mean_sky = np.median(sky_binned)
+        mean_sky = np.median(sky_4)
 
         # Append to lists
         mean_flux_list.append(mean_flux)
