116e83ad79b92c6bfd4b1497b07bd17b43173f59
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-20 15:27:56 +0000

    ADD EXPOSURE

--- Noise_model.py
+++ Noise_model.py
@@ -73,7 +73,8 @@ def calculate_mean_rms_flux(table, bin_size, num_stars, directory):
                 continue
             else:
                 # Convert the non-rejected flux value to magnitude using the zero point
-                mag = -2.5 * np.log10(flux/t) + zp_value
+                zpoint = 20.95
+                mag = -2.5 * np.log10(flux/t) + np.ones(zpoint)
                 mag_error = 1.0857 * fluxerr_4 / flux_4
 
                 mags.append(mag)
