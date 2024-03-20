e0c9d0d6238096b27798b65bc53cb5d2103670a0
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-20 18:31:33 +0000

    change the fitting model

--- Noise_model.py
+++ Noise_model.py
@@ -96,7 +96,7 @@ def calculate_mean_rms_flux(table, bin_size, num_stars, directory):
         # dt_flux = fluxes_detrended / mean_flux  # Normalize the fluxes by dividing by the average flux
         # dt_fluxerr = fluxerr_4 / mean_flux  # Normalize the flux errors by dividing by the average flux
 
-        trend = np.polyval(np.polyfit(jd_mid_clipped - int(jd_mid_clipped[0]), fluxerr_4_clipped, 2), jd_mid_clipped - int(jd_mid_clipped[0]))
+        trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), fluxerr_4_clipped, 2), jd_mid - int(jd_mid[0]))
         dt_flux = flux_4_clipped / trend
         dt_fluxerr = fluxerr_4_clipped / trend
 
