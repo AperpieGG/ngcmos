217b3f84958ec5e49e805390d9fad1dc73d28728
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-21 12:04:22 +0000

    adding total path for scripts

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -6,7 +6,7 @@ echo "Starting processing..."
 python /home/ops/ngcmos/QC_Donuts.py
 python /home/ops/ngcmos/QC_control.py
 python /home/ops/ngcmos/calibration_images.py
-python /home/ops/ngcmos/Simple_wrapper.py
+python /home/ops/refcatpipe2/Simple_wrapper.py
 python /home/ops/ngcmos/check_cmos.py
 python /home/ops/ngcmos/adding_headers.py
 python /home/ops/ngcmos/process_cmos.py
