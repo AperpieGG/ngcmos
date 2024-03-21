a0920350ef185d3b66d144a4f8380b048a61be56
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-21 16:45:04 +0000

    add conditions when plotting

--- plot_detrending.py
+++ plot_detrending.py
@@ -85,7 +85,7 @@ def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
     ax1.set_xlabel('MJD [days]')
     ax1.set_ylabel('Flux [e-]')
     ax1.legend()
-    if dt_fluxerr <= 0:
+    if dt_fluxerr < 0:
         print(f"This star has negative flux error at some point. Gaia ID: {gaia_id_to_plot}")
         ax2.plot(jd_mid, dt_flux, '.', color='black', alpha=0.5)
     else:
