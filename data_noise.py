efa6aa9b222a20c647a8c63e1cd35a542661e8dc
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-21 10:20:46 +0000

    update noise_source function

--- data_noise.py
+++ data_noise.py
@@ -366,8 +366,8 @@ def noise_model(synthetic_flux, photon_shot_noise, sky_noise, read_noise, dc_noi
     ax.set_ylabel('RMS')
     ax.set_xscale('log')
     ax.set_yscale('log')
-    ax.set_ylim(0.0001, 0.1)
-    ax.set_xlim(1000, 1e6)
+    ax.set_ylim(100, 100000)
+    ax.set_xlim(100, 1e6)
     plt.tight_layout()
 
     plt.legend(loc='best')
