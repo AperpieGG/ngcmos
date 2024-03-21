cfcb4dc058ca5062884ca0683da1120d99508c50
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-21 12:38:19 +0000

    remove some scripts

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -3,7 +3,7 @@
 echo "Starting processing..."
 
 # Run the Python scripts
-python /home/ops/refcatpipe2/simple_wrapper.py
+python /home/ops/refcatpipe2/cmos/simple_wrapper.py
 python /home/ops/ngcmos/check_cmos.py
 python /home/ops/ngcmos/adding_headers.py
 python /home/ops/ngcmos/process_cmos.py
