ea8930211b39d01a5aac90d87d8cc4451a6bf036
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-21 14:56:18 +0000

    change the script entirely to plot only the detrended

--- analyse_noise.py
+++ analyse_noise.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 import argparse
-import datetime
 import json
 import os
-import fnmatch
-from datetime import datetime, timedelta
 import numpy as np
-from astropy.io import fits
 from matplotlib import pyplot as plt
-from utils import plot_images
+from utils import plot_images, find_current_night_directory, get_phot_files, read_phot_file, bin_time_flux_error
 
 
 def load_config(filename):
@@ -29,199 +25,65 @@ for calibration_path, base_path, out_path in zip(calibration_paths, base_paths, 
         break
 
 
-def find_current_night_directory(directory):
+def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
     """
-    Find the directory for the current night based on the current date.
-    If not found, use the current working directory.
+    Plot the light curve for a specific Gaia ID with detrending
 
-    Parameters
-    ----------
-    directory : str
-        Base path for the directory.
+    Parameters:
+    table : astropy.table.Table
+        Table containing the photometry data
+    gaia_id_to_plot : int
+        Gaia ID of the star to plot
+    bin_size : int
+        Number of images to bin
 
-    Returns
-    -------
-    str
-        Path to the current night directory.
+    Returns:
+        None
+
     """
-    previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
-    current_date_directory = os.path.join(directory, previous_date)
-    return current_date_directory if os.path.isdir(current_date_directory) else os.getcwd()
-
-
-def get_phot_files(directory):
-    """
-    Get photometry files with the pattern 'phot_*.fits' from the directory.
-
-    Parameters
-    ----------
-    directory : str
-        Directory containing the files.
-
-    Returns
-    -------
-    list of str
-        List of photometry files matching the pattern.
-    """
-    phot_files = []
-    for filename in os.listdir(directory):
-        if fnmatch.fnmatch(filename, 'phot_*.fits'):
-            phot_files.append(filename)
-    return phot_files
-
-
-def read_phot_file(filename):
-    """
-    Read the photometry file.
-
-    Parameters
-    ----------
-    filename : str
-        Photometry file to read.
-
-    Returns
-    -------
-    astropy.table.table.Table
-        Table containing the photometry data.
-    """
-    # Read the photometry file here using fits or any other appropriate method
-    try:
-        with fits.open(filename) as ff:
-            # Access the data in the photometry file as needed
-            tab = ff[1].data
-            return tab
-    except Exception as e:
-        print(f"Error reading photometry file {filename}: {e}")
-        return None
-
-
-def bin_time_flux_error(time, flux, error, bin_fact):
-    """
-    Use reshape to bin light curve data, clip under filled bins
-    Works with 2D arrays of flux and errors
-
-    Note: under filled bins are clipped off the end of the series
-
-    Parameters
-    ----------
-    time : array         of times to bin
-    flux : array         of flux values to bin
-    error : array         of error values to bin
-    bin_fact : int
-        Number of measurements to combine
-
-    Returns
-    -------
-    times_b : array
-        Binned times
-    flux_b : array
-        Binned fluxes
-    error_b : array
-        Binned errors
-
-    Raises
-    ------
-    None
-    """
-    n_binned = int(len(time) / bin_fact)
-    clip = n_binned * bin_fact
-    time_b = np.average(time[:clip].reshape(n_binned, bin_fact), axis=1)
-    # determine if 1 or 2d flux/err inputs
-    if len(flux.shape) == 1:
-        flux_b = np.average(flux[:clip].reshape(n_binned, bin_fact), axis=1)
-        error_b = np.sqrt(np.sum(error[:clip].reshape(n_binned, bin_fact) ** 2, axis=1)) / bin_fact
-    else:
-        # assumed 2d with 1 row per star
-        n_stars = len(flux)
-        flux_b = np.average(flux[:clip].reshape((n_stars, n_binned, bin_fact)), axis=2)
-        error_b = np.sqrt(np.sum(error[:clip].reshape((n_stars, n_binned, bin_fact)) ** 2, axis=2)) / bin_fact
-    return time_b, flux_b, error_b
-
-
-def calculate_mean_rms_binned(table, bin_size, num_stars):
-    mean_flux_list = []
-    RMS_list = []
-    RMS_unbinned_list = []
-
-    for gaia_id in table['gaia_id'][:num_stars]:  # Selecting the first num_stars stars
-        gaia_id_data = table[table['gaia_id'] == gaia_id]
-        jd_mid = gaia_id_data['jd_mid']
-        flux_2 = gaia_id_data['flux_2']
-        fluxerr_2 = gaia_id_data['fluxerr_2']
-
-        trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_2, 2), jd_mid - int(jd_mid[0]))
-        dt_flux = flux_2 / trend
-        dt_fluxerr = fluxerr_2 / trend
-
-        time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, bin_size)
-        print(len(time_binned), len(dt_flux_binned), len(dt_fluxerr_binned))
-        print(len(jd_mid), len(dt_flux), len(dt_fluxerr))
-
-        # Calculate mean flux and RMS
-        mean_flux = np.mean(flux_2)
-        RMS = np.std(dt_flux_binned)
-        rms_unbinned = np.std(dt_flux)
-
-        # Append to lists
-        mean_flux_list.append(mean_flux)
-        RMS_list.append(RMS)
-        RMS_unbinned_list.append(rms_unbinned)
-
-    print(f"The length of the RMS list is {len(RMS_list)}")
-    print(f"The length of the RMS unbinned list is {len(RMS_unbinned_list)}")
-    return mean_flux_list, RMS_list, RMS_unbinned_list
-
-
-def plot_noise_model(mean_flux_list, RMS_list, RMS_unbinned_list):
-    # Plot the noise model
-    fig, ax = plt.subplots(1, 1, figsize=(10, 8))
-    ax.plot(mean_flux_list, RMS_list, 'o', color='black', label='Noise Model (Binned)')
-    ax.plot(mean_flux_list, RMS_unbinned_list, 'o', color='red', label='Unbinned')
-    ax.set_xlabel('Mean Flux [e-]')
-    ax.set_ylabel('RMS [e-]')
-    ax.set_title('Noise Model')
-    ax.set_yscale('log')
-    ax.set_xscale('log')
-    ax.legend()
-    plt.tight_layout()
-    plt.show()
-
-
-def plot_lc_with_detrend(table, gaia_id_to_plot):
     # Select rows with the specified Gaia ID
     gaia_id_data = table[table['gaia_id'] == gaia_id_to_plot]
     # Get jd_mid, flux_2, and fluxerr_2 for the selected rows
     jd_mid = gaia_id_data['jd_mid']
-    flux_2 = gaia_id_data['flux_2']
-    fluxerr_2 = gaia_id_data['fluxerr_2']
     tmag = gaia_id_data['Tmag'][0]
 
-    # flatten_lc, trend = flatten(jd_mid, flux_2, window_length=0.01, return_trend=True, method='biweight')
-    # use polyfit to detrend the light curve
-    trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_2, 2), jd_mid - int(jd_mid[0]))
+    # Extract fluxes and errors based on Tmag
+    if tmag < 11:
+        fluxes = gaia_id_data['flux_5']
+        fluxerrs = gaia_id_data['fluxerr_5']
+    elif 12 > tmag >= 11:
+        fluxes = gaia_id_data['flux_4']
+        fluxerrs = gaia_id_data['fluxerr_4']
+    else:
+        fluxes = gaia_id_data['flux_3']
+        fluxerrs = gaia_id_data['fluxerr_3']
 
-    # Compute Detrended flux and errors
-    norm_flux = flux_2 / trend
-    relative_err = fluxerr_2 / trend
-    rms = np.std(norm_flux)
-    print(f"RMS for Gaia ID {gaia_id_to_plot} = {rms:.2f}")
+    # use polyfit to detrend the light curve
+    trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), fluxes, 2),
+                       jd_mid - int(jd_mid[0]))
+    dt_flux = fluxes / trend
+    dt_fluxerr = fluxerrs / trend
+
+    # Bin the time, flux, and error
+    time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, bin_size)
+
+    RMS = np.std(dt_flux_binned)
+    print(f"RMS for Gaia ID {gaia_id_to_plot} = {RMS:.2f}")
 
     # Create subplots
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
 
     # Plot raw flux with wotan model
-    ax1.plot(jd_mid, flux_2, 'o', color='black', label='Raw Flux')
-    ax1.plot(jd_mid, trend, color='red', label='Model fit')
+    ax1.plot(time_binned, fluxes, 'o', color='black', label='Raw Flux')
+    ax1.plot(time_binned, trend, color='red', label='Model fit')
     ax1.set_title(f'Detrended LC for Gaia ID {gaia_id_to_plot} (Tmag = {tmag:.2f})')
     ax1.set_xlabel('MJD [days]')
     ax1.set_ylabel('Flux [e-]')
     ax1.legend()
-
-    ax2.errorbar(jd_mid, norm_flux, yerr=relative_err, fmt='o', color='black', label='Detrended Flux')
+    ax2.errorbar(jd_mid, dt_flux_binned, yerr=dt_fluxerr, fmt='o', color='black', label='Detrended Flux')
     ax2.set_ylabel('Detrended Flux [e-]')
     ax2.set_xlabel('MJD [days]')
     ax2.legend()
-
     plt.tight_layout()
     plt.show()
 
@@ -231,11 +93,7 @@ def main():
     parser = argparse.ArgumentParser(description='Plot light curve for a specific Gaia ID')
     parser.add_argument('--gaia_id', type=int, help='The Gaia ID of the star to plot')
     parser.add_argument('--bin', type=int, default=1, help='Number of images to bin')
-    parser.add_argument('--num_stars', type=int, default=100, help='Number of stars to use for the noise model')
     args = parser.parse_args()
-    gaia_id_to_plot = args.gaia_id
-    bin_size = args.bin
-    num_stars = args.num_stars
 
     # Set plot parameters
     plot_images()
@@ -247,18 +105,20 @@ def main():
     phot_files = get_phot_files(current_night_directory)
     print(f"Photometry files: {phot_files}")
 
-    # Plot the first photometry file
-    print(f"Plotting the first photometry file {phot_files[0]}...")
-    phot_table = read_phot_file(phot_files[0])
+    # Loop through photometry files
+    for phot_file in phot_files:
+        phot_table = read_phot_file(os.path.join(current_night_directory, phot_file))
 
-    # Plot the light curve for the specified Gaia ID
-    if gaia_id_to_plot:
-        plot_lc_with_detrend(phot_table, gaia_id_to_plot)
+        # Check if gaia_id exists in the current photometry file
+        if args.gaia_id in phot_table['gaia_id']:
+            print('Found star in photometry file:', phot_file)
+            plot_lc_with_detrend(phot_table, args.gaia_id)
+            break  # Stop looping if gaia_id is found
+        else:
+            print(f"Gaia ID {args.gaia_id} not found in {phot_file}")
+
     else:
-        # Calculate mean and RMS for the noise model
-        mean_flux_list, RMS_list, RMS_unbinned_list = calculate_mean_rms_binned(phot_table, bin_size=bin_size,
-                                                                                num_stars=num_stars)
-        plot_noise_model(mean_flux_list, RMS_list, RMS_unbinned_list)
+        print(f"Gaia ID {args.gaia_id} not found in any photometry file.")
 
 
 if __name__ == "__main__":
