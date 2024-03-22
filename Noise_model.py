90f37fdd1b5dcb83bfb944afcc7d5c33e3ebcbae
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-22 16:13:22 +0000

    make the step of the synthetic lc to be 1000 instead of 5k

--- Noise_model.py
+++ Noise_model.py
@@ -228,7 +228,7 @@ def noise_sources(sky_list, bin_size, airmass_list, zp):
     # set exposure time and and random flux
     exposure_time = 10
 
-    synthetic_flux = np.arange(100, 1e7, 5000)
+    synthetic_flux = np.arange(100, 1e7, 1000)
     synthetic_mag = np.mean(zp) - 2.5 * np.log10(synthetic_flux / exposure_time)
 
     # set dark current rate from cmos characterisation
