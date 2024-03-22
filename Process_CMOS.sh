b578c3c56d865a7dee6592b9baf05d468ca11b69
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-22 12:33:07 +0000

    adding create_flats.py script

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -6,6 +6,7 @@ echo "Starting processing..."
 python /home/ops/refcatpipe2/cmos/simple_wrapper.py
 python /home/ops/ngcmos/check_cmos.py
 python /home/ops/ngcmos/adding_headers.py
+python /home/ops/ngcmos/create_flats.py
 python /home/ops/ngcmos/process_cmos.py
 
 echo "Finishing processing!"
