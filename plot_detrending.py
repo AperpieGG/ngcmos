4a08dd428e44f3f0b090bcfd5e6929a19b49345d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-22 14:55:33 +0000

    convert everything to tic_id

--- plot_detrending.py
+++ plot_detrending.py
@@ -25,15 +25,15 @@ for calibration_path, base_path, out_path in zip(calibration_paths, base_paths, 
         break
 
 
-def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
+def plot_lc_with_detrend(table, tic_id_to_plot, bin_size):
     """
-    Plot the light curve for a specific Gaia ID with detrending
+    Plot the light curve for a specific TIC ID with detrending
 
     Parameters:
     table : astropy.table.Table
         Table containing the photometry data
-    gaia_id_to_plot : int
-        Gaia ID of the star to plot
+    tic_id_to_plot : int
+        TIC ID of the star to plot
     bin_size : int
         Number of images to bin
 
@@ -41,27 +41,27 @@ def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
         None
 
     """
-    # Select rows with the specified Gaia ID
-    gaia_id_data = table[table['gaia_id'] == gaia_id_to_plot]
+    # Select rows with the specified TIC ID
+    tic_id_data = table[table['tic_id'] == tic_id_to_plot]
     # Get jd_mid, flux_2, and fluxerr_2 for the selected rows
-    jd_mid = gaia_id_data['jd_mid']
-    tmag = gaia_id_data['Tmag'][0]
-    fluxes = gaia_id_data['flux_6']
-    fluxerrs = gaia_id_data['fluxerr_6']
+    jd_mid = tic_id_data['jd_mid']
+    tmag = tic_id_data['Tmag'][0]
+    fluxes = tic_id_data['flux_6']
+    fluxerrs = tic_id_data['fluxerr_6']
 
     # Extract fluxes and errors based on Tmag
     # if tmag < 10.5:
-    #     fluxes = gaia_id_data['flux_6']
-    #     fluxerrs = gaia_id_data['fluxerr_6']
+    #     fluxes = tic_id_data['flux_6']
+    #     fluxerrs = tic_id_data['fluxerr_6']
     # elif 10.5 <= tmag < 11:
-    #     fluxes = gaia_id_data['flux_5']
-    #     fluxerrs = gaia_id_data['fluxerr_5']
+    #     fluxes = tic_id_data['flux_5']
+    #     fluxerrs = tic_id_data['fluxerr_5']
     # elif 12 > tmag >= 11:
-    #     fluxes = gaia_id_data['flux_4']
-    #     fluxerrs = gaia_id_data['fluxerr_4']
+    #     fluxes = tic_id_data['flux_4']
+    #     fluxerrs = tic_id_data['fluxerr_4']
     # else:
-    #     fluxes = gaia_id_data['flux_3']
-    #     fluxerrs = gaia_id_data['fluxerr_3']
+    #     fluxes = tic_id_data['flux_3']
+    #     fluxerrs = tic_id_data['fluxerr_3']
 
     # use polyfit to detrend the light curve
     trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), fluxes, 2),
@@ -73,7 +73,7 @@ def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
     time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, bin_size)
 
     RMS = np.std(dt_flux_binned)
-    print(f"RMS for Gaia ID {gaia_id_to_plot} = {RMS:.4f}")
+    print(f"RMS for TIC ID {tic_id_to_plot} = {RMS:.4f}")
 
     # Create subplots
     fig, (ax1, ax2) = plt.subplots(2, 1, figsize=(10, 8), sharex=True)
@@ -81,7 +81,7 @@ def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
     # Plot raw flux with wotan model
     ax1.plot(jd_mid, fluxes, '.', color='black', label='Raw Flux')
     ax1.plot(jd_mid, trend, color='red', label='Model fit')
-    ax1.set_title(f'Detrended LC for Gaia ID {gaia_id_to_plot} (Tmag = {tmag:.2f})')
+    ax1.set_title(f'Detrended LC for TIC ID {tic_id_to_plot} (Tmag = {tmag:.2f})')
     ax1.set_xlabel('MJD [days]')
     ax1.set_ylabel('Flux [e-]')
     ax1.legend()
@@ -96,8 +96,8 @@ def plot_lc_with_detrend(table, gaia_id_to_plot, bin_size):
 
 def main():
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Plot light curve for a specific Gaia ID')
-    parser.add_argument('--gaia_id', type=int, help='The Gaia ID of the star to plot')
+    parser = argparse.ArgumentParser(description='Plot light curve for a specific TIC ID')
+    parser.add_argument('--tic_id', type=int, help='The TIC ID of the star to plot')
     parser.add_argument('--bin', type=int, default=1, help='Number of images to bin')
     args = parser.parse_args()
 
@@ -115,16 +115,16 @@ def main():
     for phot_file in phot_files:
         phot_table = read_phot_file(os.path.join(current_night_directory, phot_file))
 
-        # Check if gaia_id exists in the current photometry file
-        if args.gaia_id in phot_table['gaia_id']:
+        # Check if tic_id exists in the current photometry file
+        if args.tic_id in phot_table['tic_id']:
             print('Found star in photometry file:', phot_file)
-            plot_lc_with_detrend(phot_table, args.gaia_id, args.bin)
-            break  # Stop looping if gaia_id is found
+            plot_lc_with_detrend(phot_table, args.tic_id, args.bin)
+            break  # Stop looping if tic_id is found
         else:
-            print(f"Gaia ID {args.gaia_id} not found in {phot_file}")
+            print(f"TIC ID {args.tic_id} not found in {phot_file}")
 
     else:
-        print(f"Gaia ID {args.gaia_id} not found in any photometry file.")
+        print(f"TIC ID {args.tic_id} not found in any photometry file.")
 
 
 if __name__ == "__main__":
