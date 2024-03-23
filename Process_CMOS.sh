91984f3071102683d08f488d14a3940a958fd92a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-23 18:23:33 +0000

    fix the path for Noise model

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -10,5 +10,5 @@ python /home/ops/ngcmos/create_flats.py
 python /home/ops/ngcmos/process_cmos.py
 
 # Run the Noise_model.py script with arguments
-python /path/to/Noise_model.py --num_stars 5000
+python /home/ops/ngcmos/Noise_model.py --num_stars 6000
 echo "Finishing processing!"
