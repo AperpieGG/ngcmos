9f52a24092078efbc596c73a6bedcd9a55260a0f
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-23 23:09:18 +0000

    change to zp correction

--- Noise_model.py
+++ Noise_model.py
@@ -120,14 +120,15 @@ def calculate_mean_rms_flux(table, bin_size, num_stars, directory):
 
         # # Detrend the flux by converting back to fluxes and normalize by the mean lc
         fluxes_detrended = 10 ** (-0.4 * np.array(mags))  # Convert magnitudes back to fluxes
-        # mean_flux = np.mean(fluxes_detrended)  # Calculate the average flux
-        # dt_flux = fluxes_detrended / mean_flux  # Normalize the fluxes by dividing by the average flux
-        # dt_fluxerr = fluxerr_4 / mean_flux  # Normalize the flux errors by dividing by the average flux
+        mean_flux = np.mean(fluxes_detrended)  # Calculate the average flux
+        dt_flux = fluxes_detrended / mean_flux  # Normalize the fluxes by dividing by the average flux
+        dt_fluxerr = fluxerr_4_clipped / mean_flux  # Normalize the flux errors by dividing by the average flux
 
-        trend = np.polyval(np.polyfit(time_clipped - int(time_clipped[0]), flux_4_clipped, 2),
-                           time_clipped - int(time_clipped[0]))
-        dt_flux = flux_4_clipped / trend
-        dt_fluxerr = fluxerr_4_clipped / trend
+        # # Fit a second order polynomial to the detrended flux for airmass
+        # trend = np.polyval(np.polyfit(time_clipped - int(time_clipped[0]), flux_4_clipped, 2),
+        #                    time_clipped - int(time_clipped[0]))
+        # dt_flux = flux_4_clipped / trend
+        # dt_fluxerr = fluxerr_4_clipped / trend
 
         time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(time_clipped, dt_flux, dt_fluxerr,
                                                                              bin_size)
