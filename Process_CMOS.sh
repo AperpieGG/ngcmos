5e6a8f116f21faf895cba16b88ee15879f7dd590
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-23 18:23:02 +0000

    adding noise model command

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -9,4 +9,6 @@ python /home/ops/ngcmos/adding_headers.py
 python /home/ops/ngcmos/create_flats.py
 python /home/ops/ngcmos/process_cmos.py
 
+# Run the Noise_model.py script with arguments
+python /path/to/Noise_model.py --num_stars 5000
 echo "Finishing processing!"
