32841d82b7828950f48c0d19f3d15516e7630216
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-23 23:05:49 +0000

    comment adding_headers, needs fixing first

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -5,7 +5,7 @@ echo "Starting processing..."
 # Run the Python scripts
 python /home/ops/refcatpipe2/cmos/simple_wrapper.py
 python /home/ops/ngcmos/check_cmos.py
-python /home/ops/ngcmos/adding_headers.py
+#python /home/ops/ngcmos/adding_headers.py
 python /home/ops/ngcmos/create_flats.py
 python /home/ops/ngcmos/process_cmos.py
 
