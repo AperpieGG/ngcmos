5257b6c900a55c6bafb197e6f421063a511462dd
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-25 22:23:19 +0000

    add name blended for the table

--- process_cmos.py
+++ process_cmos.py
@@ -200,7 +200,7 @@ def main():
 
             frame_preamble = Table([frame_ids, phot_cat['gaia_id'], phot_cat['Tmag'], phot_cat['tic_id'],
                                     phot_cat['blended'], time_jd.value, phot_x, phot_y],
-                                   names=("frame_id", "gaia_id", "Tmag", "tic_id", "jd_mid", "x", "y"))
+                                   names=("frame_id", "gaia_id", "Tmag", "tic_id", "blended", "jd_mid", "x", "y"))
 
             # Extract photometry at locations
             frame_phot = wcs_phot(frame_data, phot_x, phot_y, RSI, RSO, APERTURE_RADII, gain=GAIN)
