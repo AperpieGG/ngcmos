333a58fb9759441651a2a4c4f83f6865d3774eb8
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-25 10:22:58 +0000

    filter for average-faint stars

--- single_rms_vs_time.py
+++ single_rms_vs_time.py
@@ -1,14 +1,11 @@
 #! /usr/bin/env python
-import datetime
+import argparse
 import json
 import os
-import fnmatch
-from datetime import datetime, timedelta
 import numpy as np
-from astropy.io import fits
 from matplotlib import pyplot as plt, ticker
-from utils import plot_images
-import argparse
+from utils import (plot_images, find_current_night_directory, get_phot_files,
+                   read_phot_file, bin_time_flux_error, remove_outliers)
 
 
 def load_config(filename):
@@ -29,118 +26,9 @@ for calibration_path, base_path, out_path in zip(calibration_paths, base_paths, 
         break
 
 
-def find_current_night_directory(directory):
-    """
-    Find the directory for the current night based on the current date.
-    If not found, use the current working directory.
-
-    Parameters
-    ----------
-    directory : str
-        Base path for the directory.
-
-    Returns
-    -------
-    str
-        Path to the current night directory.
-    """
-    previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
-    current_date_directory = os.path.join(directory, previous_date)
-    return current_date_directory if os.path.isdir(current_date_directory) else os.getcwd()
-
-
-def get_phot_files(directory):
-    """
-    Get photometry files with the pattern 'phot_*.fits' from the directory.
-
-    Parameters
-    ----------
-    directory : str
-        Directory containing the files.
-
-    Returns
-    -------
-    list of str
-        List of photometry files matching the pattern.
-    """
-    phot_files = []
-    for filename in os.listdir(directory):
-        if fnmatch.fnmatch(filename, 'phot_*.fits'):
-            phot_files.append(filename)
-    return phot_files
-
-
-def read_phot_file(filename):
-    """
-    Read the photometry file.
-
-    Parameters
-    ----------
-    filename : str
-        Photometry file to read.
-
-    Returns
-    -------
-    astropy.table.table.Table
-        Table containing the photometry data.
-    """
-    # Read the photometry file here using fits or any other appropriate method
-    try:
-        with fits.open(filename) as ff:
-            # Access the data in the photometry file as needed
-            tab = ff[1].data
-            return tab
-    except Exception as e:
-        print(f"Error reading photometry file {filename}: {e}")
-        return None
-
-
-def bin_time_flux_error(time, flux, error, bin_fact):
-    """
-    Use reshape to bin light curve data, clip under filled bins
-    Works with 2D arrays of flux and errors
-
-    Note: under filled bins are clipped off the end of the series
-
-    Parameters
-    ----------
-    time : array         of times to bin
-    flux : array         of flux values to bin
-    error : array         of error values to bin
-    bin_fact : int
-        Number of measurements to combine
-
-    Returns
-    -------
-    times_b : array
-        Binned times
-    flux_b : array
-        Binned fluxes
-    error_b : array
-        Binned errors
-
-    Raises
-    ------
-    None
-    """
-    n_binned = int(len(time) / bin_fact)
-    clip = n_binned * bin_fact
-    time_b = np.average(time[:clip].reshape(n_binned, bin_fact), axis=1)
-    # determine if 1 or 2d flux/err inputs
-    if len(flux.shape) == 1:
-        flux_b = np.average(flux[:clip].reshape(n_binned, bin_fact), axis=1)
-        error_b = np.sqrt(np.sum(error[:clip].reshape(n_binned, bin_fact) ** 2, axis=1)) / bin_fact
-    else:
-        # assumed 2d with 1 row per star
-        n_stars = len(flux)
-        flux_b = np.average(flux[:clip].reshape((n_stars, n_binned, bin_fact)), axis=2)
-        error_b = np.sqrt(np.sum(error[:clip].reshape((n_stars, n_binned, bin_fact)) ** 2, axis=2)) / bin_fact
-    return time_b, flux_b, error_b
-
-
 def plot_rms_time(table, num_stars, tic_id=None):
     # filtered_table = table[(table['Tmag'] >= 9) & (table['Tmag'] <= 10.5)]
-    filtered_table = table[(table['Tmag'] >= 14) & (table['Tmag'] <= 15)]
+    filtered_table = table[(table['Tmag'] >= 9.5) & (table['Tmag'] <= 10.5)]
     unique_tmags = np.unique(filtered_table['Tmag'])
     print('The bright stars are: ', len(unique_tmags))
 
@@ -164,13 +52,16 @@ def plot_rms_time(table, num_stars, tic_id=None):
         if tic_id is not None and current_tic_id != tic_id:
             continue
 
-        trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), flux_5, 2), jd_mid - int(jd_mid[0]))
-        dt_flux = flux_5 / trend
-        dt_fluxerr = fluxerr_5 / trend
+        time_clipped, flux_5_clipped, fluxerr_5_clipped = remove_outliers(jd_mid, flux_5, fluxerr_5)
+
+        trend = np.polyval(np.polyfit(time_clipped - int(time_clipped[0]), flux_5_clipped, 2),
+                           time_clipped - int(time_clipped[0]))
+        dt_flux = flux_5_clipped / trend
+        dt_fluxerr = fluxerr_5_clipped / trend
         RMS_values = []
         time_seconds = []
         for i in range(1, max_binning):
-            time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(jd_mid, dt_flux, dt_fluxerr, i)
+            time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(time_clipped, dt_flux, dt_fluxerr, i)
             exposure_time_seconds = i * 10  # 10 seconds per binning
             RMS = np.std(dt_flux_binned)
             RMS_values.append(RMS)
