c76c71f95739410ff46c184fe6b71079109c412d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-27 16:28:11 +0000

    removing binning parameters for plotting

--- plot_results.py
+++ plot_results.py
@@ -54,15 +54,15 @@ def plot_noise_model(data):
     ax.plot(synthetic_mag, np.ones(len(synthetic_mag)) * N, color='orange', label='scintillation noise',
             linestyle='--')
     ax.set_xlabel('TESS Magnitude')
-    # ax.set_ylabel('RMS (ppm)')
+    ax.set_ylabel('RMS (ppm)')
     ax.set_yscale('log')
     ax.set_xlim(7.5, 14)
-    # ax.set_ylim(1000, 100000)
+    ax.set_ylim(1000, 100000)
     ax.invert_xaxis()
     plt.legend(loc='best')
 
     # if binning 30min
-    ax.set_ylim(100, 4000)
+    # ax.set_ylim(100, 4000)
     plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=False))
     plt.gca().yaxis.set_minor_formatter(ticker.ScalarFormatter(useMathText=False))
     plt.gca().tick_params(axis='y', which='minor', length=4)
