16911a8446cb1d89519ed52139f33759b00fc513
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-28 16:34:53 +0000

    testing

--- relative_phot.py
+++ relative_phot.py
@@ -78,6 +78,8 @@ def plot_lc_with_detrend(table, tic_id_to_plot, bin_size):
     # Convert master reference fluxes to a numpy array
     master_reference_flux = np.array(master_reference_fluxes)
 
+    fluxes_clipped = fluxes_clipped / master_reference_flux
+
     # use polyfit to detrend the light curve
     trend = np.polyval(np.polyfit(time_clipped - int(time_clipped[0]), fluxes_clipped, 2),
                        time_clipped - int(time_clipped[0]))
