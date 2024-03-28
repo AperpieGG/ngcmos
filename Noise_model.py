22ea381c7212dd4ca21819ff80b5232acf59f56b
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-28 08:49:32 +0000

    model bin to 180

--- Noise_model.py
+++ Noise_model.py
@@ -121,12 +121,12 @@ def calculate_mean_rms_flux(table, bin_size, num_stars, directory):
         dt_flux = flux_4_clipped / trend
         dt_fluxerr = fluxerr_4_clipped / trend
 
-        time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(time_clipped, dt_flux, dt_fluxerr,
-                                                                             bin_size)
+        # time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(time_clipped, dt_flux, dt_fluxerr,
+        #                                                                      bin_size)
 
-        # time_binned = time_clipped / np.sqrt(bin_size)
-        # dt_flux_binned = dt_flux / np.sqrt(bin_size)
-        # dt_fluxerr_binned = dt_fluxerr / np.sqrt(bin_size)
+        time_binned = time_clipped / np.sqrt(bin_size)
+        dt_flux_binned = dt_flux / np.sqrt(bin_size)
+        dt_fluxerr_binned = dt_fluxerr / np.sqrt(bin_size)
 
         # Calculate mean flux and RMS
         mean_flux = np.mean(flux_4_clipped)
