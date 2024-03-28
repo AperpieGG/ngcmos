c313038005940027016c2f287001246a1b726818
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-28 09:44:05 +0000

    exclude outliers

--- plot_results.py
+++ plot_results.py
@@ -64,6 +64,8 @@ def plot_noise_model(data):
     for tic_id, _, _ in outliers:
         indices_to_exclude.add(np.where(np.array(tic_ids) == tic_id)[0][0])
 
+    print("Indices to exclude:", indices_to_exclude)  # Print to debug
+
     # Plot total data excluding filtered points and outliers
     total_indices = [i for i in range(len(mags_list)) if i not in indices_to_exclude]
     total_mags = [mags_list[i] for i in total_indices]
