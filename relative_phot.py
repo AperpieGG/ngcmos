b6423b54843a239d72e54f1784e244f3519b3133
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-28 16:53:51 +0000

    testing

--- relative_phot.py
+++ relative_phot.py
@@ -102,7 +102,7 @@ def plot_lc_with_detrend(table, tic_id_to_plot, bin_size):
     ax1.set_xlabel('MJD [days]')
     ax1.set_ylabel('Relative Flux [e-]')
     ax1.legend()
-    ax2.plot(time_clipped, dt_flux, '.', color='black', alpha=0.5)
+    ax2.plot(time_binned, dt_flux_binned, '.', color='black', alpha=0.5)
     ax2.set_ylabel('Detrended Flux [e-], binned {}'.format(bin_size))
     ax2.set_xlabel('MJD [days]')
     plt.tight_layout()
