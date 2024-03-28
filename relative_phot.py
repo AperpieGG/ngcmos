9abcca8e9511edfc1bc0286320bb5386e97c2465
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-28 16:21:29 +0000

    testing

--- relative_phot.py
+++ relative_phot.py
@@ -53,10 +53,10 @@ def plot_lc_with_detrend(table, tic_id_to_plot, bin_size):
     time_clipped, fluxes_clipped, fluxerrs_clipped = remove_outliers(jd_mid, fluxes, fluxerrs)
 
     # Select stars for master reference star
-    master_star_data = table[(table['Tmag'] < 10) & (table['Tmag'] > 9.5)]
+    master_star_data = table[(table['Tmag'] >= 9.2) & (table['Tmag'] <= 9.6)]
     master_fluxes = []
     for master_tic_id in master_star_data['tic_id']:
-        print(f"Master reference star: {master_tic_id}")
+        print(f"stars to use for master reference star: {master_tic_id}")
         master_fluxes.extend(table[table['tic_id'] == master_tic_id]['flux_6'])
     # Calculate the median flux for the master reference star
 
