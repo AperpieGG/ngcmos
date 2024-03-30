3d36e61c2015ec271a773d1e2c071fc6327065bd
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-03-30 21:59:58 +0000

    remove plot_images function

--- QC_control.py
+++ QC_control.py
@@ -8,43 +8,12 @@ from collections import defaultdict
 from datetime import datetime, timedelta
 import glob
 import os
-
 import numpy as np
 from astropy.io import fits
-from astropy.visualization import ImageNormalize, SqrtStretch, ZScaleInterval, LogStretch, MinMaxInterval
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
-from matplotlib.colors import Normalize, LogNorm
-from mpl_toolkits.axes_grid1 import make_axes_locatable
-
-
-def plot_images():
-    plt.rcParams['figure.dpi'] = 100
-    plt.rcParams['xtick.top'] = True
-    plt.rcParams['xtick.labeltop'] = False
-    plt.rcParams['xtick.labelbottom'] = True
-    plt.rcParams['xtick.bottom'] = True
-    plt.rcParams['xtick.direction'] = 'in'
-    plt.rcParams['xtick.minor.visible'] = True
-    plt.rcParams['xtick.major.top'] = True
-    plt.rcParams['xtick.minor.top'] = True
-    plt.rcParams['xtick.minor.bottom'] = True
-    plt.rcParams['xtick.alignment'] = 'center'
-
-    plt.rcParams['ytick.left'] = True
-    plt.rcParams['ytick.labelleft'] = True
-    plt.rcParams['ytick.right'] = True
-    plt.rcParams['ytick.minor.visible'] = True
-    plt.rcParams['ytick.major.right'] = True
-    plt.rcParams['ytick.major.left'] = True
-    plt.rcParams['ytick.minor.right'] = True
-    plt.rcParams['ytick.minor.left'] = True
-    plt.rcParams['font.size'] = 12
-    plt.rcParams['legend.frameon'] = True
-    plt.rcParams['legend.framealpha'] = 0.8
-    plt.rcParams['legend.loc'] = 'best'
-    plt.rcParams['legend.fancybox'] = True
-    plt.rcParams['legend.fontsize'] = 12
+from matplotlib.colors import Normalize
+from utils import plot_images
 
 
 def find_fits(file_path):
