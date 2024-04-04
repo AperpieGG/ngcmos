20b791325e57a6b5dd726941db9a9f676f1e6d47
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-04 16:26:42 +0100

    fix the logic

--- simple_wrapper_ccd.py
+++ simple_wrapper_ccd.py
@@ -6,9 +6,30 @@ and try solving them one by one
 import os
 import glob as g
 from astropy.io import fits
+import argparse as ap
 
 # pylint: disable=invalid-name
 # pylint: disable=no-member
+
+
+def arg_parse():
+    """
+    Parse the command line arguments
+    """
+    p = ap.ArgumentParser("Solve AG references images for CASUTools")
+    p.add_argument('--defocus',
+                   help='manual override for defocus (mm)',
+                   type=float,
+                   default=0.0)
+    p.add_argument('--force3rd',
+                   help='force a 3rd order distortion polyfit',
+                   action='store_true',
+                   default=False)
+    p.add_argument('--save_matched_cat',
+                   help='output the matched catalog with basic photometry',
+                   action='store_true',
+                   default=False)
+    return p.parse_args()
 
 
 def get_subdirectories(parent_directory):
@@ -30,91 +51,92 @@ def get_subdirectories(parent_directory):
 
 
 if __name__ == "__main__":
-    # get the current working directory
+    # Parse command-line arguments
+    args = arg_parse()
+
+    # Get the current working directory
     parent_directory = os.getcwd()
 
-    # get a list of subdirectories inside the parent directory
-    subdirectories = get_subdirectories(parent_directory)
+    # Get a list of subdirectories inside the parent directory
+    subdirectories = [name for name in os.listdir(parent_directory) if
+                      os.path.isdir(os.path.join(parent_directory, name))]
 
     print('The subdirectories are:', subdirectories)
 
-    # iterate over each subdirectory
+    # Iterate over each subdirectory
     for subdirectory in subdirectories:
         if subdirectory.startswith("action") and subdirectory.endswith("_observeField"):
-            # form the full path to the subdirectory
+            # Form the full path to the subdirectory
             subdirectory_path = os.path.join(parent_directory, subdirectory)
 
-            # change the working directory to the subdirectory
+            # Change the working directory to the subdirectory
             os.chdir(subdirectory_path)
-
-            # Maintain a dictionary to keep track of which catalogs have been created
-            catalog_dict = {}
 
             print(f"Processing subdirectory: {subdirectory_path}")
 
-            # get a list of all fits images
+            # Get a list of all FITS images
             all_fits = sorted([f for f in g.glob("*.fits.bz2") if fits.getheader(f)['IMGCLASS'] == 'SCIENCE'])
             print("The number of science FITS files found:", len(all_fits))
-            print("The prefix is:", fits.getheader(all_fits[0])['OBJECT'])
+            prefix = fits.getheader(all_fits[0])['OBJECT']
+            print("The prefix is:", prefix)
 
-            # filter out the catalogs
-            ref_images = [f for f in all_fits if "_cat" not in f]
+            # Check if there are any FITS files in the subdirectory
+            if not all_fits:
+                print("No science FITS files found in this subdirectory.")
+                continue
 
-            # Iterate over reference images
-            for ref_image in ref_images:
-                print("Processing reference image:", ref_image)
-                base_name = ref_image.split('.fits.bz2')[0]
-                cat_file = f"{base_name}_catalog.fits"
+            # Use the first FITS file to extract prefix and create catalog filename
+            cat_file = f"{prefix}_catalog.fits"
 
-                # get the coords from the header, use this to make a catalog
-                with fits.open(ref_image) as ff:
-                    ra = str(ff[0].header['RA'])
-                    dec = str(ff[0].header['DEC'])
+            # Check if catalog for this subdirectory has already been created
+            if not os.path.exists(cat_file):
+                # Get the coords from the header of the first FITS file
+                with fits.open(all_fits[0]) as ff:
+                    ra = str(ff[0].header['CMD_RA'])
+                    dec = str(ff[0].header['CMD_DEC'])
                     epoch = str(ff[0].header['DATE-OBS'])
                     box_size = "2.8"  # You can adjust this as needed
 
-                # Check if catalog for this prefix has already been created
-                prefix = ref_image.split('_')[0]  # Extract prefix from filename
-                if prefix not in catalog_dict:
-                    catalog_dict[prefix] = cat_file  # Add catalog filename to dictionary
-                    # Call the catalog maker only if it hasn't been created already
-                    cmd_args = ["/home/ops/refcatpipe2/cmos/make_ref_catalog.py",
-                                ra, dec, box_size, box_size, epoch, cat_file]
-                    cmd = " ".join(cmd_args)
-                    os.system(cmd)
-                    print("Catalog created for reference image:", ref_image)
-                else:
-                    # Catalog for this prefix has already been created, use the existing one
-                    cat_file = catalog_dict[prefix]
+                # Call the catalog maker
+                cmd_args = ["/home/ops/refcatpipe2/cmos/make_ref_catalog.py",
+                            ra, dec, box_size, box_size, epoch, cat_file]
 
-                if os.path.exists(cat_file):
-                    print("Found catalog file:", cat_file)
-                    print("Solving reference image:", ref_image)
-                    # solve the reference image with this catalog file
-                    cmd2_args = ["/home/ops/refcatpipe2/cmos/solve_ref_images.py",
-                                 cat_file, ref_image]
+                # Add optional arguments based on command-line arguments
+                if args.save_matched_cat:
+                    cmd_args.append("--save_matched_cat")
+                if args.defocus is not None:
+                    cmd_args.append("--defocus {:.2f}".format(args.defocus))
+                if args.force3rd:
+                    cmd_args.append("--force3rd")
 
-                    cmd2 = " ".join(cmd2_args)
-                    os.system(cmd2)
-                    print("Solved reference image:", ref_image)
+                cmd = " ".join(cmd_args)
+                os.system(cmd)
+                print("Catalog created for subdirectory:", subdirectory)
 
-                # Now, iterate over all FITS files again and solve for each image
-                for fits_file in all_fits:
-                    if fits_file != ref_image and "_cat" not in fits_file:  # Exclude the reference image and catalogs
-                        print("Processing FITS file:", fits_file)
-                        with fits.open(fits_file) as hdulist:
-                            # Check if keywords exist
-                            if 'CTYPE1' not in hdulist[0].header or 'CTYPE2' not in hdulist[0].header:
-                                print("Solving FITS file:", fits_file)
-                                # solve the image with the same catalog file
-                                cmd2_args = ["/home/ops/refcatpipe2/cmos/solve_ref_images.py",
-                                             cat_file, fits_file]
+            # Now, iterate over all FITS files and process each image
+            for fits_file in all_fits:
+                print("Processing FITS file:", fits_file)
+                with fits.open(fits_file) as hdulist:
+                    # Check if keywords exist
+                    if 'CTYPE1' not in hdulist[0].header or 'CTYPE2' not in hdulist[0].header:
+                        print("Solving FITS file:", fits_file)
+                        # Solve the image with the catalog file
+                        cmd2_args = ["/home/ops/refcatpipe2/cmos/solve_ref_images.py",
+                                     cat_file, fits_file]
 
-                                cmd2 = " ".join(cmd2_args)
-                                os.system(cmd2)
-                                print("Solved FITS file:", fits_file)
-                            else:
-                                print("FITS file is already solved. Skipping:", fits_file)
+                        # Add optional arguments based on command-line arguments
+                        if args.save_matched_cat:
+                            cmd2_args.append("--save_matched_cat")
+                        if args.defocus is not None:
+                            cmd2_args.append("--defocus {:.2f}".format(args.defocus))
+                        if args.force3rd:
+                            cmd2_args.append("--force3rd")
 
-            # move back to the parent directory for the next subdirectory iteration
+                        cmd2 = " ".join(cmd2_args)
+                        os.system(cmd2)
+                        print("Solved FITS file:", fits_file)
+                    else:
+                        print("FITS file is already solved. Skipping:", fits_file)
+
+            # Move back to the parent directory for the next subdirectory iteration
             os.chdir(parent_directory)
