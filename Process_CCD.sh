6ed80fb9eed8e7f876b18ab16e186e07b983b449
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-05 13:48:17 +0100

    add function to unzip the images

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -3,6 +3,7 @@
 echo "Starting processing..."
 
 # Run the Python scripts
+python /home/ops/ngcmos/unzpip_fits.py
 python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/process_ccd.py
