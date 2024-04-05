89150027129706be3614fb6f602c1885d48c6173
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-05 11:07:44 +0100

    fixing subtracting overscan

--- process_ccd.py
+++ process_ccd.py
@@ -186,85 +186,87 @@ def main():
                     ref_oscan = np.median(ref_frame_data[:, 2075:], axis=1)
                     ref_frame_data = ref_frame_data[:, 20:2068]
                     ref_frame_data_corr = ref_frame_data - ref_oscan
-
-                    print(f"The average pixel value for {filename} is {np.mean(ref_frame_data_corr)}")
-                    # Convert reduced_data to a dictionary with filenames as keys
-                    reduced_data_dict = {filename: (ref_frame_data_corr, ref_header)}
-
-                    frame_data, frame_hdr = reduced_data_dict[filename]
-                    print(f"Extracting photometry for {filename}\n")
-
-                    wcs_ignore_cards = ['SIMPLE', 'BITPIX', 'NAXIS', 'EXTEND', 'DATE', 'IMAGEW', 'IMAGEH']
-                    wcs_header = {}
-                    for line in [frame_hdr[i:i + 80] for i in range(0, len(frame_hdr), 80)]:
-                        key = line[0:8].strip()
-                        if '=' in line and key not in wcs_ignore_cards:
-                            card = fits.Card.fromstring(line)
-                            wcs_header[card.keyword] = card.value
-
-                    frame_bg = sep.Background(frame_data)
-                    frame_data_corr_no_bg = frame_data - frame_bg
-                    estimate_coord = SkyCoord(ra=frame_hdr['CMD_RA'],
-                                              dec=frame_hdr['CMD_DEC'],
-                                              unit=(u.deg, u.deg))
-                    estimate_coord_radius = 3 * u.deg
-
-                    frame_objects = _detect_objects_sep(frame_data_corr_no_bg, frame_bg.globalrms,
-                                                        AREA_MIN, AREA_MAX, DETECTION_SIGMA, DEFOCUS)
-                    if len(frame_objects) < N_OBJECTS_LIMIT:
-                        print(f"Fewer than {N_OBJECTS_LIMIT} objects found in {filename}, skipping photometry!\n")
-                        continue
-
-                    # Load the photometry catalog
-                    phot_cat, _ = get_catalog(f"{directory}/{prefix}_catalog_input.fits", ext=1)
-                    print(f"Found catalog with name {prefix}_catalog_input.fits\n")
-                    # Convert RA and DEC to pixel coordinates using the WCS information from the header
-                    phot_x, phot_y = WCS(frame_hdr).all_world2pix(phot_cat['ra_deg_corr'], phot_cat['dec_deg_corr'], 1)
-
-                    # Do time conversions - one time value per format per target
-                    half_exptime = frame_hdr['EXPTIME'] / 2.
-                    time_isot = Time([frame_hdr['DATE-OBS'] for i in range(len(phot_x))],
-                                     format='isot', scale='utc', location=get_location())
-                    time_jd = Time(time_isot.jd, format='jd', scale='utc', location=get_location())
-                    # Correct to mid-exposure time
-                    time_jd = time_jd + half_exptime * u.second
-                    ra = phot_cat['ra_deg_corr']
-                    dec = phot_cat['dec_deg_corr']
-
-                    frame_ids = [filename for i in range(len(phot_x))]
-                    print(f"Found {len(frame_ids)} sources")
-
-                    frame_preamble = Table([frame_ids, phot_cat['gaia_id'], phot_cat['Tmag'], phot_cat['tic_id'],
-                                            phot_cat['gaiabp'], phot_cat['gaiarp'], time_jd.value, phot_x, phot_y],
-                                           names=(
-                                           "frame_id", "gaia_id", "Tmag", "tic_id", "gaiabp", "gaiarp", "jd_mid", "x", "y"))
-
-                    # Extract photometry at locations
-                    frame_phot = wcs_phot(frame_data, phot_x, phot_y, RSI, RSO, APERTURE_RADII, gain=GAIN)
-
-                    # Stack the photometry and preamble
-                    frame_output = hstack([frame_preamble, frame_phot])
-
-                    # Convert frame_output to a Table if it's not already
-                    if not isinstance(frame_output, Table):
-                        frame_output = Table(frame_output)
-
-                    # Append the current frame's photometry to the accumulated photometry
-                    if phot_table is None:
-                        phot_table = frame_output
-                    else:
-                        phot_table = vstack([phot_table, frame_output])
-
-                    print(f"Finished photometry for {filename}\n")
-
-                    # Save the photometry for the current prefix
-                if phot_table is not None:
-                    phot_table.write(phot_output_filename, overwrite=True)
-                    print(f"Saved photometry for prefix {prefix} to {phot_output_filename}\n")
                 else:
-                    print(f"No photometry data for prefix {prefix}.\n")
+                    ref_frame_data_corr = ref_frame_data
 
-                print("Done!\n")
+                print(f"The average pixel value for {filename} is {np.mean(ref_frame_data_corr)}")
+                # Convert reduced_data to a dictionary with filenames as keys
+                reduced_data_dict = {filename: (ref_frame_data_corr, ref_header)}
+
+                frame_data, frame_hdr = reduced_data_dict[filename]
+                print(f"Extracting photometry for {filename}\n")
+
+                wcs_ignore_cards = ['SIMPLE', 'BITPIX', 'NAXIS', 'EXTEND', 'DATE', 'IMAGEW', 'IMAGEH']
+                wcs_header = {}
+                for line in [frame_hdr[i:i + 80] for i in range(0, len(frame_hdr), 80)]:
+                    key = line[0:8].strip()
+                    if '=' in line and key not in wcs_ignore_cards:
+                        card = fits.Card.fromstring(line)
+                        wcs_header[card.keyword] = card.value
+
+                frame_bg = sep.Background(frame_data)
+                frame_data_corr_no_bg = frame_data - frame_bg
+                estimate_coord = SkyCoord(ra=frame_hdr['CMD_RA'],
+                                          dec=frame_hdr['CMD_DEC'],
+                                          unit=(u.deg, u.deg))
+                estimate_coord_radius = 3 * u.deg
+
+                frame_objects = _detect_objects_sep(frame_data_corr_no_bg, frame_bg.globalrms,
+                                                    AREA_MIN, AREA_MAX, DETECTION_SIGMA, DEFOCUS)
+                if len(frame_objects) < N_OBJECTS_LIMIT:
+                    print(f"Fewer than {N_OBJECTS_LIMIT} objects found in {filename}, skipping photometry!\n")
+                    continue
+
+                # Load the photometry catalog
+                phot_cat, _ = get_catalog(f"{directory}/{prefix}_catalog_input.fits", ext=1)
+                print(f"Found catalog with name {prefix}_catalog_input.fits\n")
+                # Convert RA and DEC to pixel coordinates using the WCS information from the header
+                phot_x, phot_y = WCS(frame_hdr).all_world2pix(phot_cat['ra_deg_corr'], phot_cat['dec_deg_corr'], 1)
+
+                # Do time conversions - one time value per format per target
+                half_exptime = frame_hdr['EXPTIME'] / 2.
+                time_isot = Time([frame_hdr['DATE-OBS'] for i in range(len(phot_x))],
+                                 format='isot', scale='utc', location=get_location())
+                time_jd = Time(time_isot.jd, format='jd', scale='utc', location=get_location())
+                # Correct to mid-exposure time
+                time_jd = time_jd + half_exptime * u.second
+                ra = phot_cat['ra_deg_corr']
+                dec = phot_cat['dec_deg_corr']
+
+                frame_ids = [filename for i in range(len(phot_x))]
+                print(f"Found {len(frame_ids)} sources")
+
+                frame_preamble = Table([frame_ids, phot_cat['gaia_id'], phot_cat['Tmag'], phot_cat['tic_id'],
+                                        phot_cat['gaiabp'], phot_cat['gaiarp'], time_jd.value, phot_x, phot_y],
+                                       names=(
+                                       "frame_id", "gaia_id", "Tmag", "tic_id", "gaiabp", "gaiarp", "jd_mid", "x", "y"))
+
+                # Extract photometry at locations
+                frame_phot = wcs_phot(frame_data, phot_x, phot_y, RSI, RSO, APERTURE_RADII, gain=GAIN)
+
+                # Stack the photometry and preamble
+                frame_output = hstack([frame_preamble, frame_phot])
+
+                # Convert frame_output to a Table if it's not already
+                if not isinstance(frame_output, Table):
+                    frame_output = Table(frame_output)
+
+                # Append the current frame's photometry to the accumulated photometry
+                if phot_table is None:
+                    phot_table = frame_output
+                else:
+                    phot_table = vstack([phot_table, frame_output])
+
+                print(f"Finished photometry for {filename}\n")
+
+                # Save the photometry for the current prefix
+            if phot_table is not None:
+                phot_table.write(phot_output_filename, overwrite=True)
+                print(f"Saved photometry for prefix {prefix} to {phot_output_filename}\n")
+            else:
+                print(f"No photometry data for prefix {prefix}.\n")
+
+            print("Done!\n")
 
 
 if __name__ == "__main__":
