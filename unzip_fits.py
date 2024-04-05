df587b83e6ddca5e5f7eadb4a39d417cbbaa2fb8
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-05 14:04:07 +0100

    add a statement to check if images are already unzipped

--- unzip_fits.py
+++ unzip_fits.py
@@ -3,6 +3,14 @@ import os
 import bz2
 
 
+def count_unzipped_fits_files(directory):
+    # Get a list of all files in the directory
+    files = os.listdir(directory)
+    # Count the number of .fits files
+    unzipped_fits_files = [f for f in files if f.endswith('.fits')]
+    return len(unzipped_fits_files)
+
+
 def unzip_fits_bz2_files():
     # Get the current working directory
     current_directory = os.getcwd()
@@ -12,6 +20,11 @@ def unzip_fits_bz2_files():
 
     # Filter out only the .fits.bz2 files
     fits_bz2_files = [f for f in files if f.endswith('.fits.bz2')]
+
+    # Check if there are already at least 5 unzipped .fits files
+    if count_unzipped_fits_files(current_directory) >= 5:
+        print("Images have already been unzipped. Skipping unzipping...")
+        return
 
     # Iterate over each .fits.bz2 file
     for fits_bz2_file in fits_bz2_files:
