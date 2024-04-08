8f4dbb11aab22334f3d942393d9012da556fbe12
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-08 10:09:00 +0100

    adding deleting unzip images script

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -7,5 +7,6 @@ python /home/ops/ngcmos/unzip_fits.py
 python /home/ops/ngcmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/process_ccd.py
+python /home/ops/ngcmos/delete_unzip_fits.py
 
 echo "Finishing processing!"
