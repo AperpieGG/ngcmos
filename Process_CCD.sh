a15ccfd15a0a5a289710b73e54d899aac35f5a70
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-12 15:32:38 +0100

    add trim_ccd

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -4,6 +4,7 @@ echo "Starting processing..."
 
 # Run the Python scripts
 python /home/ops/ngcmos/unzip_fits.py
+python /home/ops/ngcmos/trim_ccd.py
 python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/calibration_images_ccd.py
