f31e0826f00edd11bf008a38a636059c55c0ef3c
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-12 20:31:38 +0100

    comment deleting ccd images .fits

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -9,6 +9,6 @@ python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/calibration_images_ccd.py
 python /home/ops/ngcmos/process_ccd.py
-python /home/ops/ngcmos/delete_unzip_fits.py
+#python /home/ops/ngcmos/delete_unzip_fits.py
 
 echo "Finishing processing!"
