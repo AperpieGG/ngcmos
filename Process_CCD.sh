9fd54b0e0b303921d78be9f46a7f7f0967a625c4
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-12 15:30:06 +0100

    change directory for simple wrapper

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -4,7 +4,7 @@ echo "Starting processing..."
 
 # Run the Python scripts
 python /home/ops/ngcmos/unzip_fits.py
-python /home/ops/ngcmos/simple_wrapper_ccd.py
+python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/calibration_images_ccd.py
 python /home/ops/ngcmos/process_ccd.py
