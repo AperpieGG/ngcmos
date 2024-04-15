6b50388bd115889951277ad5b99ad2af4ef63fc6
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-15 10:22:35 +0100

    change the zip and unzip files

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -3,12 +3,12 @@
 echo "Starting processing..."
 
 # Run the Python scripts
-python /home/ops/ngcmos/unzip_fits.py
+python /home/ops/ngcmos/unzip_fits.py # unzipped the fits files and delete the bz2 extension
 python /home/ops/ngcmos/trim_ccd.py
 python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/calibration_images_ccd.py
 python /home/ops/ngcmos/process_ccd.py
-#python /home/ops/ngcmos/delete_unzip_fits.py
+python /home/ops/ngcmos/zip_fits.py # zipped the fits to bz2 and deleted .fits files
 
 echo "Finishing processing!"
