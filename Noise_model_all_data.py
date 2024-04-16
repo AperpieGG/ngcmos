6f7c9c38c70883463691674f4eaecf5113d736ea
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-16 13:44:34 +0100

    instead of tic_ids use tmags

--- Noise_model_all_data.py
+++ Noise_model_all_data.py
@@ -37,8 +37,17 @@ def process_json_files(directory):
 
     # Find the common TIC_IDs between the two JSON files
     common_tmag = set(all_data[0]['Tmag_list']).intersection(all_data[1]['Tmag_list'])
+    common_tic_ids = set(all_data[0]['TIC_IDs']).intersection(all_data[1]['TIC_IDs'])
     print(f"Found {len(common_tmag)} common Tmag values between the two JSON files")
 
+    # match common tic_ids with common_tmag
+    common_tmag = []
+    for tic_id in common_tic_ids:
+        for data in all_data:
+            if tic_id in data['TIC_IDs']:
+                common_tmag.append(data['Tmag_list'][data['TIC_IDs'].index(tic_id)])
+                break
+    print(f"Found {len(common_tmag)} common Tmag values between the two JSON files")
     # Extract RMS and magnitude values for the common TIC_IDs
     common_rms = [[] for _ in range(len(all_data))]
     common_mags = [[] for _ in range(len(all_data))]
