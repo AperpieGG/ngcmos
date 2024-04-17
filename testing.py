aa27e9921bdf0cf3245a067d2a765da00b1eb6dd
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-17 13:56:30 +0100

    plotting time vs flux

--- testing.py
+++ testing.py
@@ -14,9 +14,8 @@ first_star_flux = data_table['Relative_Flux'][2]  # Flux for the first star
 
 # Plot flux versus time for the first star
 plt.figure(figsize=(8, 6))
-plt.plot(first_star_time, first_star_flux, marker='o')
+plt.plot(first_star_time, first_star_flux, 'o')
 plt.xlabel('Time (JD)')
 plt.ylabel('Relative Flux')
 plt.title('Flux vs Time for the First Star')
-plt.grid(True)
 plt.show()
