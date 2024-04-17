bfba8a4229c54b4ddda5df1a65975d9b183263a7
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-17 13:54:03 +0100

    plotting time vs flux

--- testing.py
+++ testing.py
@@ -2,19 +2,20 @@
 from astropy.io import fits
 import matplotlib.pyplot as plt
 
+
 # Load the FITS file containing the relative photometry data
 fits_filename = 'rel_phot_NG0625+0058.fits'  # Replace 'XXXX-XXXX' with the appropriate filename
 data_table = fits.getdata(fits_filename)
 
-# Extract RMS and Tmag values from the data table
-rms_values = data_table['RMS']
-tmag_values = data_table['Tmag']
+# Extract time and flux for the first star
+first_star_time = data_table['Time_JD'][0]  # Time for the first star
+first_star_flux = data_table['Relative_Flux'][0]  # Flux for the first star
 
-# Plot RMS versus Tmag
+# Plot flux versus time for the first star
 plt.figure(figsize=(8, 6))
-plt.scatter(tmag_values, rms_values, color='blue', alpha=0.5)
-plt.xlabel('Tmag')
-plt.ylabel('RMS')
-plt.yscale('log')
-plt.title('RMS vs Tmag')
+plt.plot(first_star_time, first_star_flux, marker='o', linestyle='-', color='blue')
+plt.xlabel('Time (JD)')
+plt.ylabel('Relative Flux')
+plt.title('Flux vs Time for the First Star')
+plt.grid(True)
 plt.show()
