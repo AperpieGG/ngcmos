d1fbe38487998688ded5e281b05d5a89c8b03f5d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-18 10:11:54 +0100

    modify the name

--- name_ch.py
+++ name_ch.py
@@ -10,9 +10,9 @@ files = os.listdir(directory)
 # Iterate over each file
 for filename in files:
     # Check if the file starts with 'stacked_' and ends with '.fits'
-    if filename.startswith('reduced_') and filename.endswith('.fits'):
+    if filename.startswith('stacked_') and filename.endswith('.fits'):
         # Extract the part of the filename after 'stacked_'
-        new_filename = filename[len('reduced_'):]
+        new_filename = filename[len('stacked_'):]
         # Rename the file
         os.rename(os.path.join(directory, filename), os.path.join(directory, new_filename))
         print(f"Renamed {filename} to {new_filename}")
