b60523fd0a8b370b13ae425482b9c27618fbfb84
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-18 10:22:25 +0100

    move one commit back

--- stacking_images.py
+++ stacking_images.py
@@ -1,21 +1,49 @@
 #!/usr/bin/env python
 import os
+import glob
+import numpy as np
 from astropy.io import fits
+from utils import utc_to_jd
 
-# Get the current working directory
-current_directory = os.getcwd()
 
-# Get a list of all FITS files in the current directory
-fits_files = [f for f in os.listdir(current_directory) if f.endswith('.fits')]
+TODO = """
+1. Grab images in a list
+2. sort the list (in terms of name/time
+3. create an array to split depending on the number of images 422 images to be stacked (1 min binned)
+4. stack the images
+5. take the jd_mid from the first image and the last image and average them to get the mid point of the observation
+6. save the stacked image in the out directory with the name of the first image stacked_{first_image}.fits
+7. save the jd_mid on the header of the stacked image.
+"""
 
-# Iterate over each FITS file
-for fits_file in fits_files:
-    # Open the FITS file and check if it has the 'JD-MID' keyword in its header
-    with fits.open(os.path.join(current_directory, fits_file)) as hdul:
-        header = hdul[0].header
-        if 'JD-MID' not in header:
-            # Delete the file if 'JD-MID' keyword is not found
-            os.remove(os.path.join(current_directory, fits_file))
-            print(f"Deleted {fits_file} because it does not have 'JD-MID' keyword.")
+# Grab images in a list
+images = glob.glob("TIC-270577175-2024*.fits")
+images.sort()
+
+# create an array to split depending on the number of images 422 images to be stacked (1 min binned)
+n_images = len(images)
+n_images_to_stack = 100
+n_splits = n_images // n_images_to_stack
+image_splits = np.array_split(images, n_splits)
+
+# stack the images
+for i, image_split in enumerate(image_splits):
+    # convert DATE-OBS to JD
+    jd_start = utc_to_jd(fits.getheader(image_split[0])['DATE-OBS'])
+    jd_end = utc_to_jd(fits.getheader(image_split[-1])['DATE-OBS'])
+    # take the jd_mid from the first image and the last image and average them to get the mid point of the observation
+    jd_mid = (float(jd_start) + float(jd_end)) / 2
+    # save the stacked image in the out directory with the name of the first image stacked_{first_image}.fits
+    stacked_image = fits.HDUList()
+    for j, image in enumerate(image_split):
+        if j == 0:
+            stacked_image = fits.open(image)
+            stacked_image[0].data = stacked_image[0].data.astype(np.float32)  # Convert to float32
         else:
-            print(f"Kept {fits_file} because it has 'JD-MID' keyword.")
+            # convert the data to float32 to avoid overflow
+            stacked_image[0].data += fits.open(image)[0].data.astype(np.float32)
+    stacked_image_filename = f"stacked_{image_split[0]}"
+    stacked_image[0].header['JD-MID'] = jd_mid
+    stacked_image.writeto(stacked_image_filename)
+    stacked_image.close()
+    print(f"Stacked image saved as {stacked_image_filename} with JD-MID = {jd_mid}")
