3cc756e3430574445054af00e165fa1bd881ac1d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-18 13:38:52 +0100

    pass number of image to stack ias argument

--- stacking_images.py
+++ stacking_images.py
@@ -1,28 +1,33 @@
 #!/usr/bin/env python
 import os
 import glob
+import argparse
 import numpy as np
 from astropy.io import fits
 from utils import utc_to_jd
 
-
 TODO = """
 1. Grab images in a list
 2. sort the list (in terms of name/time
-3. create an array to split depending on the number of images 422 images to be stacked (1 min binned)
+3. create an array to split depending on the number of images to be stacked (1 min binned)
 4. stack the images
 5. take the jd_mid from the first image and the last image and average them to get the mid point of the observation
 6. save the stacked image in the out directory with the name of the first image stacked_{first_image}.fits
 7. save the jd_mid on the header of the stacked image.
 """
 
+parser = argparse.ArgumentParser(description="Stack FITS images.")
+parser.add_argument("n_images_to_stack", type=int, help="Number of images to stack together.")
+args = parser.parse_args()
+
 # Grab images in a list
-images = glob.glob("TIC-270577175-2024*.fits")
+images = glob.glob("*.fits")
+exclude = ['stacked', 'master_flat', 'master_dark', 'master_bias', 'catalog']
 images.sort()
 
 # create an array to split depending on the number of images 422 images to be stacked (1 min binned)
 n_images = len(images)
-n_images_to_stack = 100
+n_images_to_stack = args.n_images_to_stack
 n_splits = n_images // n_images_to_stack
 image_splits = np.array_split(images, n_splits)
 
