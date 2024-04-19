8cea0d4df971bd557e3ae4ba1ed3329f37b8ece5
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-04-19 14:19:20 +0100

    move noise sources function and scintilation to utils

--- Noise_model.py
+++ Noise_model.py
@@ -14,31 +14,10 @@ from astropy.io import fits
 from matplotlib import pyplot as plt, ticker
 import json
 from utils import (find_current_night_directory, read_phot_file, get_phot_files, bin_time_flux_error, plot_images,
-                   remove_outliers, calculate_trend_and_flux)
+                   remove_outliers, calculate_trend_and_flux, noise_sources)
 
 
-def load_config(filename):
-    """
-    Load configuration file
-    """
-    with open(filename, 'r') as file:
-        config = json.load(file)
-    return config
-
-
-# Load paths from the configuration file
-config = load_config('directories.json')
-calibration_paths = config["calibration_paths"]
-base_paths = config["base_paths"]
-out_paths = config["out_paths"]
-
-# Select directory based on existence
-for calibration_path, base_path, out_path in zip(calibration_paths, base_paths, out_paths):
-    if os.path.exists(base_path):
-        break
-
-
-def calculate_mean_rms_flux(table, bin_size, num_stars, directory, average_zp):
+def calculate_mean_rms_flux(table, bin_size, num_stars, average_zp):
     """
     Calculate the mean flux and RMS for a given number of stars
 
@@ -50,8 +29,6 @@ def calculate_mean_rms_flux(table, bin_size, num_stars, directory, average_zp):
         Number of images to bin
     num_stars : int
         Number of stars to plot
-    directory : str
-        Directory containing the images
     average_zp : float
         Average zero point value
 
@@ -137,99 +114,12 @@ def extract_header(table, image_directory):
     return airmass_list, zp_list
 
 
-def scintilation_noise(airmass_list):
-    t = 10  # exposure time
-    D = 0.2  # telescope diameter
-    h = 2433  # height of Paranal
-    H = 8000  # height of atmospheric scale
-    airmass = np.mean(airmass_list)  # airmass
-    C_y = 1.54  # constant
-    N = np.sqrt(10e-6 * (C_y ** 2) * (D ** (-4 / 3)) * (1 / t) * (airmass ** 3) * np.exp((-2. * h) / H))
-    print('Scintilation noise: ', N)
-    return N
-
-
-def noise_sources(sky_list, bin_size, airmass_list, zp):
-    """
-    Returns the noise sources for a given flux
-
-    returns arrays of noise and signal for a given flux
-
-    Parameters
-    ----------
-    sky_list : list
-        values of sky fluxes
-    bin_size : int
-        number of images to bin
-    airmass_list : list
-        values of airmass
-    zp : list
-        values of zero points
-
-    Returns
-    -------
-    synthetic_mag : array
-        values of synthetic magnitudes
-    photon_shot_noise : array
-        values of photon shot noise
-    sky_noise : array
-        values of sky noise
-    read_noise : array
-        values of read noise
-    dc_noise : array
-        values of dark current noise
-    N : array
-        values of scintilation noise
-    RNS : array
-        values of read noise squared
-    """
-
-    # set aperture radius
-    aperture_radius = 6
-    npix = np.pi * aperture_radius ** 2
-
-    # set exposure time and and random flux
-    exposure_time = 10
-
-    synthetic_flux = np.arange(100, 1e7, 1000)
-    synthetic_mag = np.mean(zp) - 2.5 * np.log10(synthetic_flux / exposure_time)
-
-    # set dark current rate from cmos characterisation
-    dark_current_rate = 1.6
-    dark_current = dark_current_rate * exposure_time * npix
-    dc_noise = np.sqrt(dark_current) / synthetic_flux / np.sqrt(bin_size) * 1000000  # Convert to ppm
-
-    # set read noise from cmos characterisation
-    read_noise_pix = 1.56
-    read_noise = (read_noise_pix * np.sqrt(npix)) / synthetic_flux / np.sqrt(bin_size) * 1000000  # Convert to ppm
-    read_signal = npix * (read_noise_pix ** 2)
-
-    # set random sky background
-    sky_flux = np.mean(sky_list)
-    sky_noise = np.sqrt(sky_flux) / synthetic_flux / np.sqrt(bin_size) * 1000000  # Convert to ppm
-    print('Average sky flux: ', sky_flux)
-
-    # set random photon shot noise from the flux
-    photon_shot_noise = np.sqrt(synthetic_flux) / synthetic_flux / np.sqrt(bin_size) * 1000000  # Convert to ppm
-
-    N = scintilation_noise(airmass_list)
-
-    N_sc = (N * synthetic_flux) ** 2
-    N = N / np.sqrt(bin_size) * 1000000  # Convert to ppm
-
-    total_noise = np.sqrt(synthetic_flux + sky_flux + dark_current + read_signal + N_sc)
-    RNS = total_noise / synthetic_flux / np.sqrt(bin_size)
-    RNS = RNS * 1000000  # Convert to ppm
-
-    return synthetic_mag, photon_shot_noise, sky_noise, read_noise, dc_noise, N, RNS
-
-
 def main(phot_file, bin_size):
     # Set plot parameters
     plot_images()
 
     # Get the current night directory
-    current_night_directory = find_current_night_directory(base_path)
+    current_night_directory = os.getcwd()
 
     # Plot the current photometry file
     print(f"Plotting the photometry file {phot_file}...")
@@ -240,12 +130,13 @@ def main(phot_file, bin_size):
     max_num_stars = len(np.unique(phot_table['tic_id']))  # Maximum number of stars based on unique TIC IDs
 
     # Calculate mean and RMS for the noise model
-    mean_flux_list, RMS_list, sky_list, mags_list, Tmags_list = calculate_mean_rms_flux(
-        phot_table, bin_size=bin_size, num_stars=max_num_stars, directory=current_night_directory, average_zp=np.mean(zp))
+    RMS_list, sky_list, mags_list, Tmags_list = calculate_mean_rms_flux(
+        phot_table, bin_size=bin_size, num_stars=max_num_stars, directory=current_night_directory,
+        average_zp=np.mean(zp))
 
     # Get noise sources
     synthetic_mag, photon_shot_noise, sky_noise, read_noise, dc_noise, N, RNS = (
-        noise_sources(sky_list, bin_size, airmass_list, zp))
+        noise_sources(sky_list, bin_size, airmass_list, zp, aper=6, read_noise=1.56, dark_curent=1.6))
 
     # Plot the noise model
 
@@ -294,7 +185,7 @@ def main_loop(phot_files, bin_size):
 
 if __name__ == "__main__":
     # Get the current night directory
-    current_night_directory = find_current_night_directory(base_path)
+    current_night_directory = os.getcwd()
 
     # Get photometry files with the pattern 'phot_*.fits'
     phot_files = get_phot_files(current_night_directory)
