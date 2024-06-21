5cab18a881492e53706299a0eb62cdeb26ba684a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-21 22:43:03 +0100

    print files with ansolute path

--- send_diagnostics.py
+++ send_diagnostics.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
+
+"""
+This scripts grabs the diagnostics plots, shifts plots and the mp4 movies
+from the previous night
+"""
 import os
 from datetime import datetime, timedelta
+import download
 
 path = '/home/ops/data/shifts_plots'
 
@@ -24,3 +30,8 @@ if __name__ == "__main__":
     files = get_files(path, last_night)
     print(f"Current night directory: {last_night}")
     print(f"Files: {files}")
+    files = [os.path.join(path, f) for f in files]
+    print(files)
+
+
+# take the absolute path of the printed files and pass them as cron-jobs with download function
