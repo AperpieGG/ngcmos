7105d9d12b726f15619ce9300e69aafa911b7206
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-21 23:04:45 +0100

    change the local des dir

--- Send_shift_plots.sh
+++ Send_shift_plots.sh
@@ -6,7 +6,7 @@ REMOTE_SCRIPT="/home/ops/ngcmos/send_diagnostics.py"
 REMOTE_FILE_LIST="/home/ops/ngcmos/files_to_download.txt"
 LOCAL_FILE_LIST="/home/u5500483/ngcmos/files_to_download.txt"
 LOCAL_DOWNLOAD_SCRIPT="/home/u5500483/ngcmos/download.py"
-LOCAL_DEST_DIR="/path/to/local/destination"
+LOCAL_DEST_DIR="/home/u5500483/shifts_plots_cmos"
 
 # Step 1: Run the file identification script on the remote machine
 ssh $REMOTE_USER@$REMOTE_HOST "python3 $REMOTE_SCRIPT"
