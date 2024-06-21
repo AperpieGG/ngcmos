e595e6346d75d140e421b6aec43a4eb6156f3e2a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-21 23:14:41 +0100

    fix the paths

--- git_pull_ngtshead.sh
+++ git_pull_ngtshead.sh
@@ -3,8 +3,8 @@
 # Variables
 REMOTE_USER="ops"
 REMOTE_HOST="10.2.5.115"
-REMOTE_DIR="/home/u5500483/ngcmos/"
-LOCAL_DIR="/home/ops/ngcmos"
+REMOTE_DIR="/home/ops/ngcmos/"
+LOCAL_DIR="/home/u5500483/ngcmos/"
 
 # Rsync command to synchronize files
 rsync -avz --delete $LOCAL_DIR $REMOTE_USER@$REMOTE_HOST:$REMOTE_DIR
