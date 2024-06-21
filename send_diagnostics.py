01a041a1c65ff2d1b49b113598bb5aece6016fd2
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-21 23:02:47 +0100

    update to save a txt file with the files to be downloaed for each night

--- send_diagnostics.py
+++ send_diagnostics.py
@@ -1,20 +1,20 @@
 #!/usr/bin/env python
 
 """
-This scripts grabs the diagnostics plots, shifts plots and the mp4 movies
-from the previous night
+This script grabs the diagnostics plots, shifts plots, and the mp4 movies
+from the previous night.
 """
+
 import os
 from datetime import datetime, timedelta
-import download
 
 path = '/home/ops/data/shifts_plots'
+output_file_path = '/home/ops/ngcmos/files_to_download.txt'
 
 
 def find_last_night():
     # Get the previous date in the format YYYYMMDD
     previous_date = (datetime.now() - timedelta(days=1)).strftime("%Y%m%d")
-
     return previous_date
 
 
@@ -25,13 +25,18 @@ def get_files(path, night):
     return files
 
 
+def save_file_list(file_list, output_path):
+    with open(output_path, "w") as f:
+        for file in file_list:
+            f.write(f"{file}\n")
+
+
 if __name__ == "__main__":
     last_night = find_last_night()
     files = get_files(path, last_night)
     print(f"Current night directory: {last_night}")
     print(f"Files: {files}")
     files = [os.path.join(path, f) for f in files]
-    print(files)
 
-
-# take the absolute path of the printed files and pass them as cron-jobs with download function
+    # Save the file list to a text file
+    save_file_list(files, output_file_path)
