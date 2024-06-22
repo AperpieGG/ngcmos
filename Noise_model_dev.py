4aafd9b694d290d9843c87d574155ff94e321bb2
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-23 00:28:24 +0100

    changing the loop, using dictionaries

--- Noise_model_dev.py
+++ Noise_model_dev.py
@@ -2,71 +2,88 @@
 
 import argparse
 import os
-import numpy as np
+from utils import noise_sources
 import json
-from utils import read_phot_file, noise_sources
+from astropy.io import fits
+from collections import defaultdict
 
-
+# Constants for noise calculations
 APERTURE = 6
 READ_NOISE = 1.56
 DARK_CURRENT = 1.6
 
 
-def extract_phot_data(table):
-    RMS_list = []
-    sky_list = []
-    Tmags_list = []
-    mags_list = []
+def read_data(filename):
+    data_dict = defaultdict(list)
 
-    unique_tic_ids = np.unique(table['TIC_ID'])
-    print(f'The unique_tic_ids are: {unique_tic_ids}')
-    for tic_id in unique_tic_ids:  # Selecting the first num_stars unique TIC IDs
-        tic_id_data = table[table['TIC_ID'] == tic_id]
-        print(f'The tic_id_data is: {tic_id_data}')
-        Tmag = tic_id_data['Tmag'][0]
-        print(f'the length of Tmag is: {len(Tmag)}')
-        sky = tic_id_data['Sky'][0]
-        rms = tic_id_data['RMS'][0]
-        zero_point = 1
-        airmass = 1
-        mags = 1
+    try:
+        with fits.open(filename) as hdul:
+            data = hdul[1].data  # Assuming data is in the first extension
+            for row in data:
+                tic_id = row['TIC_ID']
+                rms = row['RMS'] * 1000000  # Multiply RMS by 1000000 to convert to ppm
+                sky = row['Sky']
+                Tmag = row['Tmag']
+                airmass = row['Airmass']
+                zp = row['ZP']
+                mag = row['Magnitude']
 
-        print(f"Tmag: {Tmag}, Sky: {sky}, RMS: {rms}")
-        print(f"Average zero point: {zero_point}, Airmass: {airmass}")
+                data_dict[tic_id].append({
+                    'RMS': rms,
+                    'Sky': sky,
+                    'Tmag': Tmag,
+                    'Airmass': airmass,
+                    'ZP': zp,
+                    'Magnitude': mag
+                })
+    except Exception as e:
+        print(f"Error reading FITS file: {e}")
 
-        # Calculate mean flux and RMS
-        RMS_list.append(rms * 1000000)  # Convert to ppm
-        sky_list.append(np.median(sky))
-        Tmags_list.append(Tmag)
-        mags_list.append(mags)
-
-    return RMS_list, sky_list, Tmags_list, zero_point, airmass, mags_list
+    return data_dict
 
 
 def main():
     # Parse command-line arguments
-    parser = argparse.ArgumentParser(description='Plot noise models with rel photometry files')
-    parser.add_argument('filename', type=str, help='Name of the FITS table containing photometry data')
-    parser.add_argument('--bin', type=int, default=1, help='Number of images to bin')
+    parser = argparse.ArgumentParser(
+        description='Read and organize TIC IDs with associated RMS, Sky, Airmass, ZP, and Magnitude from FITS table')
+    parser.add_argument('filename', type=str, help='Name of the FITS file containing photometry data')
     args = parser.parse_args()
-    bin_size = args.bin
     filename = args.filename
 
-    # Get the current night directory
-    current_night_directory = os.getcwd()
+    # Get the current working directory
+    current_dir = os.getcwd()
 
-    # Plot the current photometry file
-    print(f"Plotting the photometry file {filename}...")
-    phot_table = read_phot_file(os.path.join(current_night_directory, filename))
+    # Construct full path to the FITS file
+    file_path = os.path.join(current_dir, filename)
 
-    # Calculate mean and RMS for the noise model
-    RMS_list, sky_list, Tmags_list, zero_point, airmass, mags_list = extract_phot_data(phot_table)
+    # Read and organize data by TIC ID
+    data_dict = read_data(file_path)
+
+    # Prepare lists for noise_sources function
+    sky_list = []
+    airmass_list = []
+    zp = []
+
+    # Calculate RMS_list, sky_list, Tmags_list, and other lists
+    RMS_list = []
+    mags_list = []
+    Tmags_list = []
+    for tic_id, data_list in data_dict.items():
+        for entry in data_list:
+            RMS_list.append(entry['RMS'])
+            sky_list.append(entry['Sky'])
+            Tmags_list.append(entry['Tmag'])
+            airmass_list.append(entry['Airmass'])
+            zp.append(entry['ZP'])
+            mags_list.append(entry['Magnitude'])
 
     # Get noise sources
+    bin_size = 1  # Example bin size, adjust as needed
     synthetic_mag, photon_shot_noise, sky_noise, read_noise, dc_noise, N, RNS = (
-        noise_sources(sky_list, bin_size, airmass, zero_point, aper=APERTURE, read_noise=READ_NOISE,
-                      dark_current=DARK_CURRENT))
-    print(f"The rms values are: {RMS_list}")
+        noise_sources(sky_list, bin_size, airmass_list, zp, APERTURE, READ_NOISE, DARK_CURRENT))
+
+    # Convert lists to JSON serializable lists
+
     synthetic_mag_list = synthetic_mag.tolist()
     photon_shot_noise_list = photon_shot_noise.tolist()
     sky_noise_list = sky_noise.tolist()
@@ -74,14 +91,15 @@ def main():
     dc_noise_list = dc_noise.tolist()
     N_list = N.tolist()
     RNS_list = RNS.tolist()
-    RMS_list = [float(x) for x in RMS_list]
     Tmags_list = [float(x) for x in Tmags_list]
+    mags_list = [float(x) for x in mags_list]
 
-    # Save RMS_list, and other lists to a JSON file
+    # Save results to JSON file
     output_data = {
-        "TIC_IDs": np.unique(phot_table['TIC_ID']).tolist(),  # [int(x) for x in TIC_IDs
+        "TIC_IDs": list(data_dict.keys()),  # TIC IDs
         "RMS_list": RMS_list,
         "Tmag_list": Tmags_list,
+        "mags_list": mags_list,
         "synthetic_mag": synthetic_mag_list,
         "photon_shot_noise": photon_shot_noise_list,
         "sky_noise": sky_noise_list,
@@ -90,11 +108,16 @@ def main():
         "N": N_list,
         "RNS": RNS_list
     }
+
+    # Construct output file name
     cwd_last_four = os.getcwd()[-4:]
     file_name = f"rms_mags_{filename.replace('.fits', '')}_{bin_size}_{cwd_last_four}.json"
-    output_path = os.path.join(os.getcwd(), file_name)
+    output_path = os.path.join(current_dir, file_name)
+
+    # Save JSON file
     with open(output_path, 'w') as json_file:
         json.dump(output_data, json_file, indent=4)
+    print(f"Results saved to {output_path}")
 
 
 if __name__ == "__main__":
