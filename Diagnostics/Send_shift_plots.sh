3f855e1fce4458d3c0756c16afdfd7b15876a324
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-22 11:05:20 +0100

    remove the files to download when this is run, and exit if the files are empty

--- Diagnostics/Send_shift_plots.sh
+++ Diagnostics/Send_shift_plots.sh
@@ -3,8 +3,8 @@
 REMOTE_USER="ops"
 REMOTE_HOST="10.2.5.115"
 REMOTE_SCRIPT="/home/ops/ngcmos/Diagnostics/send_diagnostics.py"
-REMOTE_FILE_LIST="/home/ops/ngcmos/files_to_download.txt"
-LOCAL_FILE_LIST="/home/u5500483/ngcmos/files_to_download.txt"
+REMOTE_FILE_LIST="/home/ops/ngcmos/Diagnostics/files_to_download.txt"
+LOCAL_FILE_LIST="/home/u5500483/ngcmos/Diagnostics/files_to_download.txt"
 LOCAL_DOWNLOAD_SCRIPT="/home/u5500483/ngcmos/Diagnostics/download_files.py"
 LOCAL_DEST_DIR="/home/u5500483/shifts_plots_cmos/"
 
@@ -14,5 +14,14 @@ ssh $REMOTE_USER@$REMOTE_HOST "python3 $REMOTE_SCRIPT"
 # Step 2: Copy the file list from the remote machine to the local machine
 scp $REMOTE_USER@$REMOTE_HOST:$REMOTE_FILE_LIST $LOCAL_FILE_LIST
 
-# Step 3: Run the download script locally to transfer the files
+# Step 3: Check if the file list is empty
+if [ ! -s $LOCAL_FILE_LIST ]; then
+  echo "No files to download. Exiting."
+  exit 0
+fi
+
+# Step 4: Run the download script locally to transfer the files
 python3 $LOCAL_DOWNLOAD_SCRIPT $LOCAL_FILE_LIST $LOCAL_DEST_DIR
+
+# Step 5: Delete the file list after transfer
+rm $LOCAL_FILE_LIST
