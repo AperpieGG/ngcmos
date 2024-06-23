4e3def7ae1148418b85b0d0a17786eb69d9e6691
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-23 13:43:13 +0100

    print elapsed time

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Record the start time
+start_time=$(date +%s)
+
 echo "Starting processing..."
 
 # Run the Python scripts
@@ -12,3 +15,12 @@ python /home/ops/ngcmos/relative_phot.py
 python /home/ops/ngcmos/remove_fits_files.py
 
 echo "Finishing processing!"
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate the total time taken
+elapsed_time=$((end_time - start_time))
+
+# Print the total time taken
+echo "Total time taken: $elapsed_time seconds"
