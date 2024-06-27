704bec39d499567bbf7b9e819f1e332878111ada
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-27 01:29:11 +0100

    testing for beta pic noise model

--- plot_results.py
+++ plot_results.py
@@ -74,9 +74,13 @@ def plot_noise_model(data):
     # append the indices of the outliers
     filtered_indices = np.append(filtered_indices_bright, filtered_indices_dim)
     
+    # # # Exclude outliers from the total data
+    # total_RMS = [RMS_list[i] for i in range(len(RMS_list)) if i not in filtered_indices]
+    # total_mags = [mags_list[i] for i in range(len(mags_list)) if i not in filtered_indices]
+
     # # Exclude outliers from the total data
-    total_RMS = [RMS_list[i] for i in range(len(RMS_list)) if i not in filtered_indices]
-    total_mags = [mags_list[i] for i in range(len(mags_list)) if i not in filtered_indices]
+    total_RMS = [RMS_list[i] for i in range(len(RMS_list))]
+    total_mags = [mags_list[i] for i in range(len(mags_list))]
 
     ax.plot(total_mags, total_RMS, 'o', color='c', label='total data', alpha=0.5)
 
