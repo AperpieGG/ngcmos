9733aab7a78880c29085d2c33d3248353180b5c7
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-06-27 23:00:30 +0100

    testing

--- utils.py
+++ utils.py
@@ -674,7 +674,7 @@ def noise_sources(sky_list, bin_size, airmass_list, zp, aper, read_noise, dark_c
     npix = np.pi * aperture_radius ** 2
 
     # set exposure time and and random flux
-    exposure_time = 0.1
+    exposure_time = 10
 
     synthetic_flux = np.arange(100, 1e8, 1000)
     synthetic_mag = np.mean(zp) - 2.5 * np.log10(synthetic_flux / exposure_time)
