7d24918ba2533aed9cc46d7b1be47b8d87e866c0
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-07-02 22:40:32 +0100

    starting the rms_vs_time with rel phot file script

--- rms_vs_time_dev.py
+++ rms_vs_time_dev.py
@@ -1,171 +1,135 @@
-#!/usr/bin/env python
-
+#! /usr/bin/env python
 import argparse
 import os
 import numpy as np
-from astropy.table import Table, hstack, vstack
-from utils import (plot_images, get_phot_files, read_phot_file, bin_time_flux_error,
-                   remove_outliers, extract_phot_file, calculate_trend_and_flux, extract_airmass_zp)
-
-SIGMA = 2
-APERTURE = 6
-EXPOSURE = 10
+from matplotlib import pyplot as plt, ticker
+from utils import (plot_images,  get_phot_files, read_phot_file, bin_time_flux_error, calculate_trend_and_flux)
 
 
-def expand_and_rename_table(phot_table):
-    expanded_rows = []
+def plot_rms_time(table, num_stars, tic_id=None):
+    filtered_table = table[(table['Tmag'] >= 9.2) & (table['Tmag'] <= 9.6)]
+    unique_tmags = np.unique(filtered_table['Tmag'])
+    print('The bright stars are: ', len(unique_tmags))
 
-    for row in phot_table:
-        jd_mid_values = row['Time_JD']
-        relative_flux_values = row['Relative_Flux']
-        relative_flux_err_values = row['Relative_Flux_err']
+    average_rms_values = []
+    times_binned = []
+    max_binning = 151
 
-        # Expand jd_mid, relative_flux, and relative_flux_err columns into individual columns
-        for i in range(len(jd_mid_values)):
-            expanded_row = list(row)
-            expanded_row[row.colnames.index('Time_JD')] = jd_mid_values[i]
-            expanded_row[row.colnames.index('Relative_Flux')] = relative_flux_values[i]
-            expanded_row[row.colnames.index('Relative_Flux_err')] = relative_flux_err_values[i]
-            expanded_rows.append(expanded_row)
+    num_stars_used = 0
+    num_stars_excluded = 0
 
-    # Create a new table with expanded columns
-    expanded_table = Table(rows=expanded_rows, names=phot_table.colnames)
+    for Tmag in unique_tmags:
+        # Get data for the current Tmag
+        Tmag_data = table[table['Tmag'] == Tmag]
+        # Extract relevant data
+        jd_mid = Tmag_data['jd_mid']
+        flux_5 = Tmag_data['flux_6']
+        fluxerr_5 = Tmag_data['fluxerr_6']
+        current_tic_id = Tmag_data['tic_id'][0]  # Assuming Tmag is the same for all jd_mid values of a star
 
-    return expanded_table
-
-
-def relative_phot(table, tic_id_to_plot, bin_size):
-    # Select stars for master reference star, excluding the target star
-    master_star_data = table[(table['Tmag'] >= 9) & (table['Tmag'] <= 12) &
-                             (table['tic_id'] != tic_id_to_plot)]
-    print(f"Found {len(np.unique(master_star_data['tic_id']))} "
-          f"comparison stars for the target star {tic_id_to_plot}")
-    rms_comp_list = []
-
-    jd_mid, tmag, fluxes, fluxerrs, sky = extract_phot_file(table, tic_id_to_plot, aper=APERTURE)
-    airmass_list = table[table['tic_id'] == tic_id_to_plot]['airmass']
-    zero_point_list = table[table['tic_id'] == tic_id_to_plot]['zp']
-
-    sky_median = np.median(sky)
-    time_clipped, fluxes_clipped, fluxerrs_clipped, airmass_clipped, zero_point_clipped = (
-        remove_outliers(jd_mid, fluxes, fluxerrs, air_mass=airmass_list, zero_point=zero_point_list))
-
-    avg_zero_point = np.mean(zero_point_clipped)
-    avg_airmass = np.mean(airmass_clipped)
-    avg_magnitude = -2.5 * np.log10(np.mean(fluxes_clipped) / EXPOSURE) + avg_zero_point
-    print(f"The target star has TIC ID = {tic_id_to_plot} and TESS magnitude = {tmag:.2f}, "
-          f"and magnitude = {avg_magnitude:.2f}")
-
-    for tic_id in np.unique(master_star_data['tic_id']):
-        fluxes = master_star_data[master_star_data['tic_id'] == tic_id]['flux_6']
-        fluxerrs = master_star_data[master_star_data['tic_id'] == tic_id]['fluxerr_6']
-        time = master_star_data[master_star_data['tic_id'] == tic_id]['jd_mid']
-        time, fluxes, fluxerrs, _, _ = remove_outliers(time, fluxes, fluxerrs)
-
-        trend, fluxes_dt_comp, fluxerrs_dt_comp = calculate_trend_and_flux(time, fluxes, fluxerrs)
-        rms = np.std(fluxes_dt_comp)
-        rms_comp_list.append(rms)
-
-    min_rms_index = np.argmin(rms_comp_list)
-    min_rms_tic_id = np.unique(master_star_data['tic_id'])[min_rms_index]
-    print(f"Comparison star with min rms is TIC ID = {min_rms_tic_id} "
-          f"and RMS = {np.min(rms_comp_list):.4f}")
-
-    rms_std = np.std(rms_comp_list)
-    threshold = SIGMA * rms_std
-    print(f"Threshold for 2 sigma clipping = {threshold:.4f}")
-
-    min_rms_index = np.argmin(rms_comp_list)
-    min_rms_value = rms_comp_list[min_rms_index]
-
-    filtered_tic_ids = []
-    for tic_id, rms_value in zip(np.unique(master_star_data['tic_id']), rms_comp_list):
-        if np.abs(rms_value - min_rms_value) <= threshold:
-            filtered_tic_ids.append(tic_id)
-
-    print("Comparison stars within two sigma clipping from the minimum rms star:")
-    for tic_id in filtered_tic_ids:
-        print(
-            f"TIC ID {tic_id} with RMS = "
-            f"{rms_comp_list[np.where(np.unique(master_star_data['tic_id']) == tic_id)[0][0]]:.4f}")
-    print(
-        f"Number of comp stars within a sigma = "
-        f"{len(filtered_tic_ids)} from total of {len(np.unique(master_star_data['tic_id']))}")
-
-    filtered_master_star_data = master_star_data[np.isin(master_star_data['tic_id'], filtered_tic_ids)]
-
-    reference_fluxes = np.sum(filtered_master_star_data['flux_6'], axis=0)
-    reference_flux_mean = np.mean(reference_fluxes)
-    print(f"Reference flux mean = {reference_flux_mean:.2f}")
-
-    flux_ratio = fluxes_clipped / reference_fluxes
-    flux_ratio_mean = np.mean(flux_ratio)
-
-    dt_flux = flux_ratio / flux_ratio_mean
-    dt_fluxerr = dt_flux * np.sqrt(
-        (fluxerrs_clipped / fluxes_clipped) ** 2 + (fluxerrs_clipped[0] / fluxes_clipped[0]) ** 2)
-
-    trend, dt_flux_poly, dt_fluxerr_poly = calculate_trend_and_flux(time_clipped, dt_flux, dt_fluxerr)
-
-    time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(time_clipped, dt_flux_poly,
-                                                                         dt_fluxerr_poly, bin_size)
-
-    return (tmag, time_binned, dt_flux_binned, dt_fluxerr_binned, sky_median,
-            avg_magnitude, avg_airmass, avg_zero_point)
-
-
-def main():
-    parser = argparse.ArgumentParser(description='Perform relative photometry for a given night')
-    parser.add_argument('--bin_size', type=int, default=1, help='Number of images to bin')
-    args = parser.parse_args()
-    bin_size = args.bin_size
-
-    plot_images()
-
-    current_night_directory = os.getcwd()
-    phot_files = get_phot_files(current_night_directory)
-    print(f"Photometry files: {phot_files}")
-
-    for phot_file in phot_files:
-        phot_table = read_phot_file(os.path.join(current_night_directory, phot_file))
-
-        print(f"Photometry file: {phot_file}")
-
-        base_filename = phot_file.split('.')[0]
-        fits_filename = f"rel_{base_filename}_{bin_size}.fits"
-        if os.path.exists(fits_filename):
-            print(f"Data for {phot_file} already saved to {fits_filename}. Skipping analysis.")
+        # Check if tic_id is specified and matches current_tic_id
+        if tic_id is not None and current_tic_id != tic_id:
             continue
 
-        data_list = []
+        time_clipped, flux_5_clipped, fluxerr_5_clipped, _, _ = remove_outliers(jd_mid, flux_5, fluxerr_5)
 
-        for tic_id in np.unique(phot_table['tic_id']):
-            if np.all(phot_table['Tmag'][phot_table['tic_id'] == tic_id] < 14):
-                print(f"Performing relative photometry for TIC ID = {tic_id} and with Tmag = "
-                      f"{phot_table['Tmag'][phot_table['tic_id'] == tic_id][0]}")
-                (tmag, time_binned, dt_flux_binned, dt_fluxerr_binned, sky_median,
-                 magnitude, airmass_list, zero_point_list) = relative_phot(phot_table, tic_id, args.bin_size)
+        trend, dt_flux, dt_fluxerr = calculate_trend_and_flux(time_clipped, flux_5_clipped, fluxerr_5_clipped)
 
-                rms = np.std(dt_flux_binned)
-                print(f"RMS for TIC ID {tic_id} = {rms:.4f}")
+        RMS_values = []
+        time_seconds = []
+        for i in range(1, max_binning):
+            time_binned, dt_flux_binned, dt_fluxerr_binned = bin_time_flux_error(time_clipped, dt_flux, dt_fluxerr, i)
+            exposure_time_seconds = i * 10  # 10 seconds per binning
+            RMS = np.std(dt_flux_binned)
+            RMS_values.append(RMS)
+            time_seconds.append(exposure_time_seconds)
 
-                data_list.append((tic_id, tmag, time_binned, dt_flux_binned, dt_fluxerr_binned,
-                                  rms, sky_median, airmass_list, zero_point_list, magnitude))
-                print()
-            else:
-                print(f"TIC ID {tic_id} is not included in the analysis because "
-                      f"the Tmag = {phot_table['Tmag'][phot_table['tic_id'] == tic_id][0]} and is greater than 14.")
-                print()
+        # Check if the first RMS value is greater than 0.0065
+        # if RMS_values[0] > 0.006:
+        #     print('Excluding tic_id = {} and Tmag = {:.2f} due to RMS > 6000 ppm'.format(current_tic_id, Tmag))
+        #     num_stars_excluded += 1
+        #     continue
+        else:
+            print('Using star with tic_id = {} and Tmag = {:.2f} and RMS = {:.4f}'.
+                  format(current_tic_id, Tmag, RMS_values[0]))
 
-        data_table = Table(rows=data_list, names=('TIC_ID', 'Tmag', 'Time_JD', 'Relative_Flux', 'Relative_Flux_err',
-                                                  'RMS', 'Sky', 'Airmass', 'ZP', 'Magnitude'))
+        num_stars_used += 1
+        average_rms_values.append(RMS_values)
+        times_binned.append(time_seconds)
 
-        expanded_data_table = expand_and_rename_table(data_table)
+        # Stop if the number of stars used reaches the specified number
+        if num_stars_used >= num_stars:
+            break
 
-        expanded_data_table.write(fits_filename, format='fits', overwrite=True)
+    if not average_rms_values:
+        print("No stars found. Skipping this photometry file.")
+        return
 
-        print(f"Data for {phot_file} saved to {fits_filename}.")
+    print('The bright stars are: {}, Stars used: {}, Stars excluded: {}'.format(
+        len(unique_tmags), num_stars_used, num_stars_excluded))
+
+    # Calculate the average RMS across all stars for each bin
+    average_rms_values = np.mean(average_rms_values, axis=0) * 1000000  # Convert to ppm
+
+    # Generate binning times
+    binning_times = [i for i in range(1, max_binning)]
+
+    # Calculate the expected decrease in RMS
+    RMS_model = average_rms_values[0] / np.sqrt(binning_times)
+
+    # Plot RMS as a function of exposure time along with the expected decrease in RMS
+    plt.figure(figsize=(6, 10))
+    plt.plot(times_binned[0], average_rms_values, 'o', color='blue', label='Actual RMS')
+    plt.plot(times_binned[0], RMS_model, '--', color='red', label='Model RMS')
+    plt.title('Time binned for field: ' + phot_file[-16:-5])
+    plt.xscale('log')
+    plt.yscale('log')
+    plt.xlabel('Exposure time (s)')
+    plt.ylabel('RMS (ppm)')
+
+    plt.gca().yaxis.set_major_formatter(ticker.ScalarFormatter(useMathText=False))
+    plt.gca().yaxis.set_minor_formatter(ticker.ScalarFormatter(useMathText=False))
+    plt.gca().tick_params(axis='y', which='minor', length=4)
+
+    plt.legend()
+    plt.tight_layout()
+    plt.show()
+
+
+def run_for_one(phot_file, tic_id=None):
+    plot_images()
+    current_night_directory = '.'
+    phot_table = read_phot_file(os.path.join(current_night_directory, phot_file))
+    plot_rms_time(phot_table, 5, tic_id)
+
+
+def run_for_more(phot_file, num_stars):
+    plot_images()
+    current_night_directory = '.'
+    phot_table = read_phot_file(os.path.join(current_night_directory, phot_file))
+    plot_rms_time(phot_table, num_stars)
 
 
 if __name__ == "__main__":
-    main()
+    # Get the current night directory
+    current_night_directory = '.'
+
+    # Get photometry files with the pattern 'phot_*.fits'
+    phot_files = get_phot_files(current_night_directory)
+    print(f"Photometry files: {phot_files}")
+
+    # Parse command-line arguments
+    parser = argparse.ArgumentParser(description='Plot light curve for a specific TIC ID')
+    parser.add_argument('--num_stars', type=int, default=0, help='Number of stars to plot')
+    parser.add_argument('--tic_id', type=int, help='plot the time vs. binned RMS for a particular star')
+    args = parser.parse_args()
+
+    # Run the main function for each photometry file
+    if args.tic_id is not None:
+        for phot_file in phot_files:
+            # main(phot_file, args.tic_id)
+            run_for_one(phot_file, args.tic_id)
+    else:
+        for phot_file in phot_files:
+            # main(phot_file, args.num_stars)
+            run_for_more(phot_file, args.num_stars)
