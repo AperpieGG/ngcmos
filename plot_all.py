d8d27007c4a55295b9fd7ca24b5a014ae6e10794
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-07-09 19:20:27 +0100

    remove argpar

--- plot_all.py
+++ plot_all.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python
 import argparse
-from matplotlib import pyplot as plt, ticker
+from matplotlib import pyplot as plt
 import json
 import numpy as np
 import os
 from utils import plot_images
-
-
-# dark background
-# plt.style.use('dark_background')
 
 
 def load_rms_mags_data(filename):
@@ -20,26 +16,14 @@ def load_rms_mags_data(filename):
     return data
 
 
-def plot_noise_model(data):
+def plot_noise_model(all_data):
     fig, ax = plt.subplots(figsize=(10, 8))
-    RMS_list = data['RMS_list']
-    mags_list = data['mags_list']
-    synthetic_mag = data['synthetic_mag']
-    RNS = data['RNS']
-    photon_shot_noise = data['photon_shot_noise']
-    read_noise = data['read_noise']
-    dc_noise = data['dc_noise']
-    sky_noise = data['sky_noise']
-    N = data['N']
 
-    ax.plot(mags_list, RMS_list, 'o', color='c', label='total data', alpha=0.5)
-    # ax.plot(synthetic_mag, RNS, color='c', label='total noise')
-    # ax.plot(synthetic_mag, photon_shot_noise, color='green', label='photon shot', linestyle='--')
-    # ax.plot(synthetic_mag, read_noise, color='red', label='read noise', linestyle='--')
-    # ax.plot(synthetic_mag, dc_noise, color='purple', label='dark noise', linestyle='--')
-    # ax.plot(synthetic_mag, sky_noise, color='blue', label='sky bkg', linestyle='--')
-    # ax.plot(synthetic_mag, np.ones(len(synthetic_mag)) * N, color='orange', label='scintillation noise',
-    #         linestyle='--')
+    for data in all_data:
+        RMS_list = data['RMS_list']
+        mags_list = data['mags_list']
+        ax.plot(mags_list, RMS_list, 'o', alpha=0.5)
+
     ax.set_xlabel('TESS Magnitude')
     ax.set_ylabel('RMS (ppm)')
     ax.set_yscale('log')
@@ -51,13 +35,14 @@ def plot_noise_model(data):
     plt.show()
 
 
-def plot_tmag_vs_mag(data):
+def plot_tmag_vs_mag(all_data):
     fig, ax = plt.subplots(figsize=(10, 8))
 
-    mags_list = data['mags_list']
-    tmag_list = data['Tmag_list']
+    for data in all_data:
+        mags_list = data['mags_list']
+        tmag_list = data['Tmag_list']
+        ax.plot(tmag_list, mags_list, 'o', alpha=0.5)
 
-    ax.plot(tmag_list, mags_list, 'o', color='red', alpha=0.5)
     ax.set_xlabel('Tmag')
     ax.set_ylabel('Apparent Magnitude')
     ax.set_xlim(7.5, 16)
@@ -72,6 +57,8 @@ def main(directory):
     # Set plot parameters
     plot_images()
 
+    all_data = []
+
     # Iterate over all JSON files in the specified directory
     for filename in os.listdir(directory):
         if filename.endswith('.json'):
@@ -80,11 +67,14 @@ def main(directory):
 
             # Load RMS and magnitude data from JSON file
             data = load_rms_mags_data(json_file)
-            for i in data:
-                plot_noise_model(data[i])
-                plot_tmag_vs_mag(data[i])
+            all_data.append(data)
+
+    # Plot combined results
+    plot_noise_model(all_data)
+    plot_tmag_vs_mag(all_data)
 
 
 if __name__ == "__main__":
     directory = '.'
+    # Run main function
     main(directory)
