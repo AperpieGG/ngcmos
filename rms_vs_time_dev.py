d53a2a0bd258e09f35f00af54914f634bf4f6dd2
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-07-12 07:04:03 +0100

    change magnitude selections range

--- rms_vs_time_dev.py
+++ rms_vs_time_dev.py
@@ -7,7 +7,7 @@ from utils import plot_images,  get_rel_phot_files, read_phot_file, bin_time_flu
 
 
 def plot_rms_time(table, num_stars, tic_id=None):
-    filtered_table = table[(table['Tmag'] >= 9.2) & (table['Tmag'] <= 9.6)]
+    filtered_table = table[(table['Tmag'] >= 9) & (table['Tmag'] <= 10)]
     unique_tmags = np.unique(filtered_table['Tmag'])
     print('The bright stars are: ', len(unique_tmags))
 
