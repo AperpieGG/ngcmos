6da70be1abb9ad9ca6fcf4daa631404654d5c7ad
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-08-04 12:58:36 +0100

    change fitting method to normal polynomial

--- plot_detrending.py
+++ plot_detrending.py
@@ -50,12 +50,12 @@ def plot_lc_with_detrend(table, tic_id_to_plot, bin_size):
     fluxes = tic_id_data['flux_6']
     fluxerrs = tic_id_data['fluxerr_6']
 
-    # # use polyfit to detrend the light curve
-    # trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), fluxes, 2),
-    #                    jd_mid - int(jd_mid[0]))
+    # use polyfit to detrend the light curve
+    trend = np.polyval(np.polyfit(jd_mid - int(jd_mid[0]), fluxes, 2),
+                       jd_mid - int(jd_mid[0]))
 
     # use wotan to detrend the light curve
-    flatten_flux, trend = flatten(jd_mid, fluxes, window_length=0.05, method='mean', return_trend=True)
+    # flatten_flux, trend = flatten(jd_mid, fluxes, window_length=0.05, method='mean', return_trend=True)
 
     dt_flux = fluxes / trend
     dt_fluxerr = fluxerrs / trend
