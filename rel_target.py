9e4e1c24c6db2d14d6d285b2603442e0ec5dacb0
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-09-14 10:57:59 +0100

    change fontisize and subplots size

--- rel_target.py
+++ rel_target.py
@@ -85,7 +85,8 @@ def plot_lightcurves_in_subplots(times, fluxes, fluxerrs, tic_ids):
     for j in range(i + 1, rows * cols):
         fig.delaxes(axes[j // cols, j % cols])
 
-    plt.tight_layout(rect=[0.02, 0.02, 0.98, 0.98])
+    # Adjust the space around and between subplots using subplots_adjust
+    plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.05, hspace=0.5, wspace=0.3)
     plt.show()
 
 
