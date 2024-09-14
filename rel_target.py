116a7dc0afeeffec576ccb6d605dc1c23a4ae80e
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-09-14 10:22:16 +0100

    fix mag limit targets

--- rel_target.py
+++ rel_target.py
@@ -118,10 +118,11 @@ def relative_phot(table, tic_id_to_plot, bin_size, APERTURE, EXPOSURE):
     magnitude_tolerance = 1
 
     within_color_limit = valid_color_data[np.abs(color_index - target_color_index) <= color_tolerance]
-    print(f'Comparison stars within color limit: {len(np.unique(within_color_limit["tic_id"]))}')
+    print(f'Comp stars within color limit: {len(np.unique(within_color_limit["tic_id"]))}')
 
-    within_magnitude_limit = within_color_limit[np.abs(magnitude - target_tmag) <= magnitude_tolerance]
-    print(f'Comparison stars within mag and color limit: {len(np.unique(within_magnitude_limit["tic_id"]))}')
+    within_magnitude_limit = within_color_limit[np.abs(within_color_limit['Tmag'] - target_tmag)
+                                                <= magnitude_tolerance]
+    logger.info(f"Comp stars within color and mag limit: {len(np.unique(within_magnitude_limit['tic_id']))}")
 
     master_star_data = within_magnitude_limit[within_magnitude_limit['tic_id'] != tic_id_to_plot]
     master_stars_data_tic_ids = np.unique(master_star_data['tic_id'])
