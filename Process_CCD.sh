566161b5a29e7552bd6f0da0510957b58b196c64
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-09-25 12:35:34 +0200

    add time recording

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Record the start time
+start_time=$(date +%s)
+
 echo "Starting processing..."
 
 # Run the Python scripts
@@ -10,6 +13,15 @@ python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/calibration_images_ccd.py
 python /home/ops/ngcmos/process_ccd.py
 python /home/ops/ngcmos/relative_phot.py
-python /home/ops/ngcmos/zip_fits.py # zipped the fits to bz2 and deleted .fits files
+#python /home/ops/ngcmos/zip_fits.py # zipped the fits to bz2 and deleted .fits files
 
 echo "Finishing processing!"
+
+# Record the end time
+end_time=$(date +%s)
+
+# Calculate the total time taken
+elapsed_time=$((end_time - start_time))
+
+# Print the total time taken
+echo "Total time taken: $elapsed_time seconds"
