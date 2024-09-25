0ab2e86ce56ba7c0b20855c00d00b92d3e78f0f0
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-09-25 11:10:16 +0200

    change to Tmag

--- plot_results.py
+++ plot_results.py
@@ -70,14 +70,14 @@ def plot_noise_model(data):
     print(f'The average scintillation noise is: {np.mean(N)}')
 
     # Filter data points based on magnitude and RMS
-    filtered_indices_bright, filtered_indices_dim = filter_data(mags_list, RMS_list)
+    filtered_indices_bright, filtered_indices_dim = filter_data(Tmag_list, RMS_list)
 
     # append the indices of the outliers
     filtered_indices = np.append(filtered_indices_bright, filtered_indices_dim)
     
     # # Exclude outliers from the total data
     total_RMS = [RMS_list[i] for i in range(len(RMS_list)) if i not in filtered_indices]
-    total_mags = [mags_list[i] for i in range(len(mags_list)) if i not in filtered_indices]
+    total_mags = [Tmag_list[i] for i in range(len(Tmag_list)) if i not in filtered_indices]
 
     ax.plot(total_mags, total_RMS, 'o', color='c', label='total data', alpha=0.5)
 
