65940b9869849797ecbca0e5e3c3033ad304d20c
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-09 18:29:19 +0100

    change color tolerance

--- rel_target.py
+++ rel_target.py
@@ -182,7 +182,7 @@ def relative_phot(table, tic_id_to_plot, bin_size, APERTURE, EXPOSURE, comp_star
     # Calculate the color index for all stars
     color_index = valid_color_data['gaiabp'] - valid_color_data['gaiarp']
 
-    color_tolerance = 0.2
+    color_tolerance = 0.4
     magnitude_tolerance = 1
 
     within_color_limit = valid_color_data[np.abs(color_index - target_color_index) <= color_tolerance]
