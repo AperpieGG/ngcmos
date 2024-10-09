67e5ddf07e1ad1635ed9ef0b7312607600ac13d8
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-09 17:55:13 +0100

    fix the plots

--- rel_target.py
+++ rel_target.py
@@ -258,10 +258,12 @@ def relative_phot(table, tic_id_to_plot, bin_size, APERTURE, EXPOSURE, comp_star
     plt.plot(time_clipped, reference_fluxes, 'o', label='Reference Star', color='blue')
     plt.show()
 
+    plt.plot(time_clipped, fluxes_clipped, 'o', label='Target Star', color='red')
+
     # Calculate the flux ratio for the target star with respect the summation of the reference stars fluxes
     flux_ratio = fluxes_clipped / reference_fluxes
     # Calculate the average flux ratio of the target star
-    flux_ratio_mean = np.median(flux_ratio)
+    flux_ratio_mean = np.mean(flux_ratio)
     # Normalize the flux ratio (result around unity)
     dt_flux = flux_ratio / flux_ratio_mean
     dt_fluxerr = dt_flux * np.sqrt(
