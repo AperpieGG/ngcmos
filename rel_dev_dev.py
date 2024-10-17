830a0da463e800321e254924f3e80d3f46794cc8
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-17 15:40:09 +0100

    add prints

--- rel_dev_dev.py
+++ rel_dev_dev.py
@@ -47,6 +47,7 @@ def find_bad_comp_stars(comp_fluxes, airmass, comp_mags0, sig_level=3., dmag=0.5
     comp_star_rms = find_comp_star_rms(comp_fluxes, airmass)
     print(f'Number of comparison stars RMS before filtering: {len(comp_star_rms)}')
     comp_star_mask = np.array([True for cs in comp_star_rms])
+    print(f'The comp star mask is: {comp_star_mask}')
     i = 0
     while True:
         i += 1
