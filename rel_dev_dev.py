8764b335d90d147c7a83ee464b00cadd3a0f996a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-21 11:53:37 +0100

    bin before plotting

--- rel_dev_dev.py
+++ rel_dev_dev.py
@@ -6,7 +6,7 @@ from scipy.interpolate import InterpolatedUnivariateSpline as Spline
 from utils import plot_images, read_phot_file, bin_time_flux_error  # Assuming read_phot_file is available in utils
 
 # Constants for filtering stars
-COLOR_TOLERANCE = 0.05
+COLOR_TOLERANCE = 0.2
 MAGNITUDE_TOLERANCE = 0.5
 APERTURE = 5
 
