642487794987b2608b5c2d40a9ecc5a736b6232b
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-21 15:14:14 +0100

    change x limits

--- overplot_lc.py
+++ overplot_lc.py
@@ -20,7 +20,6 @@ def plot_light_curves():
     plt.subplots_adjust(wspace=0)  # Remove space between plots
 
     plot_axes = [ax2, ax1]  # Swapped the axes (ax2 is now left, ax1 is right)
-    xlims = []
 
     for i, json_filename in enumerate(json_files[:2]):  # Limit to two plots (one per column)
         # Load the JSON data from each file
@@ -43,19 +42,10 @@ def plot_light_curves():
         plot_axes[i].plot(target_time_binned, target_fluxes_dt, 'o', color='black')
         plot_axes[i].set_title(f'{label}')
         plot_axes[i].set_xlabel('Binned Time (BJD)')
-        if i == 0:
+        if i == 1:
             plot_axes[i].set_ylabel('Normalized Flux')
         plot_axes[i].grid(True)
 
-        # Store the first and last x-values for aligning limits later
-        xlims.append((target_time_binned[0], target_time_binned[-1]))
-
-    # Set the x-limits to make the plots continuous
-    plot_axes[0].set_xlim(left=xlims[1][1], right=xlims[0][1])  # Second plot: right limit to match last data point
-    plot_axes[1].set_xlim(left=xlims[1][0], right=xlims[0][0])  # First plot: left limit to match first data point
-
-    # Customize the overall figure
-    plt.suptitle('Target Light Curves')
     plt.show()
 
 
