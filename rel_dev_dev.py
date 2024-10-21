ea9205b5c02c24e61e50957e56b78646464916ad
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-21 12:01:32 +0100

    change color tolerance and make mag very wide

--- rel_dev_dev.py
+++ rel_dev_dev.py
@@ -7,9 +7,11 @@ from scipy.interpolate import InterpolatedUnivariateSpline as Spline
 from utils import plot_images, read_phot_file, bin_time_flux_error  # Assuming read_phot_file is available in utils
 
 # Constants for filtering stars
-COLOR_TOLERANCE = 0.2
-MAGNITUDE_TOLERANCE = 5
+COLOR_TOLERANCE = 0.05
+MAGNITUDE_TOLERANCE = 10
 APERTURE = 5
+
+plot_images()
 
 
 def target_info(table, tic_id_to_plot):
