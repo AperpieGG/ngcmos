232a8a25f2df426617fadc13bcbe56d5ec4a3a4a
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-23 13:32:42 +0100

    add invert y-axis in the end

--- measure_fwhm.py
+++ measure_fwhm.py
@@ -109,7 +109,6 @@ for i, (x_star, y_star) in enumerate(zip(selected_sources['xcentroid'], selected
 plt.figure()
 vmin, vmax = np.percentile(image_data, [5, 95])
 plt.imshow(image_data, cmap='hot', vmin=vmin, vmax=vmax)
-plt.gca().invert_yaxis()
 positions = np.transpose((selected_sources['xcentroid'] + (center_x - crop_size),
                           selected_sources['ycentroid'] + (center_y - crop_size)))
 apertures = CircularAperture(positions, r=5.)
@@ -123,4 +122,5 @@ print(f'Average FWHM_y: {average_fwhm_y:.2f} pixels')
 FWHM = (average_fwhm_x + average_fwhm_y) / 2
 print(f'Final FWHM: {FWHM:.2f} pixels')
 plt.title(f'Measured FWHM: {FWHM:.2f} pixels')
+plt.gca().invert_yaxis()
 plt.show()
