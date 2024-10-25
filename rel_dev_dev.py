5d7ed8854eaaed0f50a6dd2f884dc82489d80b07
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-25 08:43:59 +0100

    change color to 0.5

--- rel_dev_dev.py
+++ rel_dev_dev.py
@@ -11,7 +11,7 @@ from utils import plot_images, read_phot_file, bin_time_flux_error, \
     remove_outliers
 
 # Constants for filtering stars
-COLOR_TOLERANCE = 1
+COLOR_TOLERANCE = 0.5
 
 plot_images()
 
