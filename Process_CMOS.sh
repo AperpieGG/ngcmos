5c3ec8a3a077f71d0d8479c46b302cde1077cca7
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-10-31 16:10:28 +0000

    add lines to create directories.json file

--- Process_CMOS.sh
+++ Process_CMOS.sh
@@ -5,6 +5,24 @@ start_time=$(date +%s)
 
 echo "Starting processing..."
 
+# Create directories.json with the specified paths
+cat <<EOF > directories.json
+{
+  "calibration_paths": [
+    "/Users/u5500483/Downloads/DATA_MAC/CMOS/20231212/",
+    "/home/ops/data/20231212/"
+  ],
+  "base_paths": [
+    "/Users/u5500483/Downloads/DATA_MAC/CMOS/",
+    "/home/ops/data/"
+  ],
+  "out_paths": [
+    "/Users/u5500483/Downloads/DATA_MAC/CMOS/calibration_images/",
+    "/home/ops/data/calibration_images/"
+  ]
+}
+EOF
+
 # Run the Python scripts
 python /home/ops/refcatpipe2/cmos/simple_wrapper.py
 python /home/ops/ngcmos/check_cmos.py
@@ -13,7 +31,7 @@ python /home/ops/ngcmos/create_flats.py
 python /home/ops/ngcmos/process_cmos.py
 python /home/ops/fwhm_stars/fhwm_batches.py --size 11 # make plot and save to fwhm_results.json
 python /home/ops/ngcmos/relative_phot_dev.py --aper 5
-python /home/ops/fhwm_stars/best_fwhm.py  --size 11 # save to fwhm_positions.json
+python /home/ops/fwhm_stars/best_fwhm.py --size 11 # save to fwhm_positions.json
 python /home/ops/ngcmos/remove_fits_files.py
 
 echo "Finishing processing!"
