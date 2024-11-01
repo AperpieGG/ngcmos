8da6dd6bea1ae1caa82251f0736c7d80d9924499
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-01 15:30:28 +0000

    add fwhm_ scripts to the script

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -12,7 +12,9 @@ python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
 python /home/ops/ngcmos/check_ccd.py
 python /home/ops/ngcmos/calibration_images_ccd.py
 python /home/ops/ngcmos/process_ccd.py
-python /home/ops/ngcmos/relative_phot.py
+python /home/ops/fwhm_stars/fwhm_batches.py --size 13.5 # make plot and save to fwhm_results.json
+python /home/ops/ngcmos/relative_phot_dev.py --aper 5
+python /home/ops/fwhm_stars/best_fwhm.py --size 13.5 # save to fwhm_positions.json
 python /home/ops/ngcmos/zip_fits.py # zipped the fits to bz2 and deleted .fits files
 
 echo "Finishing processing!"
