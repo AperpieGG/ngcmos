4707bee8ca475d0808035bbdd43d15a6b26ce914
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-05 16:47:22 +0000

    add gain to the function

--- utils.py
+++ utils.py
@@ -717,8 +717,8 @@ def noise_sources(sky_list, bin_size, airmass_list, zp, aper, rn, dc, exposure, 
 
     # set exposure time and and random flux
     exposure_time = exposure
-    synthetic_flux = np.arange(100, 1e7, 1000)
-    synthetic_mag = np.mean(zp*gain) - (2.5 * np.log10(synthetic_flux*gain/exposure_time))
+    synthetic_flux = np.arange(100, 1e7, 1000) * gain
+    synthetic_mag = np.mean(zp) * gain - (2.5 * np.log10(synthetic_flux/exposure_time))
 
     # set dark current rate from cmos characterisation
     dark_current = dc * exposure_time * npix
