fda60dca621be59a8c66b327b482f6e100ceaadc
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-06 09:17:11 +0000

    include color map

--- plot_results.py
+++ plot_results.py
@@ -64,7 +64,10 @@ def plot_noise_model(data):
     fig, ax = plt.subplots(figsize=(10, 8))
     RMS_list = data['RMS_list']
     tic_ids = data['TIC_IDs']
-    color_list = data['COLOR']
+    color_list = []
+    for tic_id in tic_ids:
+        tic_data = data[data['TIC_ID'] == tic_id]
+        color_list.extend(tic_data['COLOR'])
     Tmag_list = data['Tmag_list']
     synthetic_mag = data['synthetic_mag']
     RNS = data['RNS']
