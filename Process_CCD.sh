a4aebcb9b0230afcb50a820b705362be912d9622
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-07 15:51:21 +0000

    adjust the script to go to subdir and do fwhm/rel phot stuff, pass it to a loop for every subdir

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -4,7 +4,7 @@
 start_time=$(date +%s)
 
 echo "Starting processing..."
-#
+
 ## Run the initial Python scripts
 #python /home/ops/ngcmos/unzip_fits.py     # unzips the FITS files and deletes the bz2 extension
 #python /home/ops/ngcmos/trim_ccd.py
@@ -13,27 +13,24 @@ echo "Starting processing..."
 #python /home/ops/ngcmos/calibration_images_ccd.py
 #python /home/ops/ngcmos/process_ccd.py
 
-# Check for a subdirectory that matches "action*observeField"
-observe_dir=$(find . -maxdepth 1 -type d -name "action*_observeField" | head -n 1)
-
-if [[ -d "$observe_dir" ]]; then
+# Loop through each subdirectory that matches "action*_observeField"
+# shellcheck disable=SC2044
+for observe_dir in $(find . -maxdepth 1 -type d -name "action*_observeField"); do
     echo "Found observeField directory: $observe_dir"
-    # shellcheck disable=SC2164
-    cd "$observe_dir"
+
+    # Change to the directory
+    cd "$observe_dir" || continue
     echo "Changed to directory: $(pwd)"
 
-    # Run the additional Python scripts within the observeField subdirectory
+    # Run the additional Python scripts within this subdirectory
     python /home/ops/fwhm_stars/fwhm_batches.py --size 13.5 --cam CCD   # make plot and save to fwhm_results.json
     python /home/ops/ngcmos/relative_phot_dev.py --aper 4
     python /home/ops/fwhm_stars/best_fwhm.py --size 13.5                # save to fwhm_positions.json
     python /home/ops/ngcmos/zip_fits.py                                 # zip the FITS files to bz2 and delete .fits files
-else
-    echo "No matching observeField directory found."
-fi
 
-# Return to the original directory
-# shellcheck disable=SC2164
-cd -
+    # Return to the parent directory
+    cd - || exit
+done
 
 echo "Finishing processing!"
 
