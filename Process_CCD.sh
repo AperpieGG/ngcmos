f374ba58d467caa3b31f4007bdf8f93a6e8ba48c
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-07 15:49:07 +0000

    adjust the script to go to subdir and do fwhm/rel phot stuff

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -4,18 +4,36 @@
 start_time=$(date +%s)
 
 echo "Starting processing..."
+#
+## Run the initial Python scripts
+#python /home/ops/ngcmos/unzip_fits.py     # unzips the FITS files and deletes the bz2 extension
+#python /home/ops/ngcmos/trim_ccd.py
+#python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
+#python /home/ops/ngcmos/check_ccd.py
+#python /home/ops/ngcmos/calibration_images_ccd.py
+#python /home/ops/ngcmos/process_ccd.py
 
-# Run the Python scripts
-python /home/ops/ngcmos/unzip_fits.py # unzipped the fits files and delete the bz2 extension
-python /home/ops/ngcmos/trim_ccd.py
-python /home/ops/refcatpipe2/cmos/simple_wrapper_ccd.py
-python /home/ops/ngcmos/check_ccd.py
-python /home/ops/ngcmos/calibration_images_ccd.py
-python /home/ops/ngcmos/process_ccd.py
-python /home/ops/fwhm_stars/fwhm_batches.py --size 13.5 --cam CCD # make plot and save to fwhm_results.json
-python /home/ops/ngcmos/relative_phot_dev.py --aper 4
-python /home/ops/fwhm_stars/best_fwhm.py --size 13.5 # save to fwhm_positions.json
-python /home/ops/ngcmos/zip_fits.py # zipped the fits to bz2 and deleted .fits files
+# Check for a subdirectory that matches "action*observeField"
+observe_dir=$(find . -maxdepth 1 -type d -name "action*_observeField" | head -n 1)
+
+if [[ -d "$observe_dir" ]]; then
+    echo "Found observeField directory: $observe_dir"
+    # shellcheck disable=SC2164
+    cd "$observe_dir"
+    echo "Changed to directory: $(pwd)"
+
+    # Run the additional Python scripts within the observeField subdirectory
+    python /home/ops/fwhm_stars/fwhm_batches.py --size 13.5 --cam CCD   # make plot and save to fwhm_results.json
+    python /home/ops/ngcmos/relative_phot_dev.py --aper 4
+    python /home/ops/fwhm_stars/best_fwhm.py --size 13.5                # save to fwhm_positions.json
+    python /home/ops/ngcmos/zip_fits.py                                 # zip the FITS files to bz2 and delete .fits files
+else
+    echo "No matching observeField directory found."
+fi
+
+# Return to the original directory
+# shellcheck disable=SC2164
+cd -
 
 echo "Finishing processing!"
 
