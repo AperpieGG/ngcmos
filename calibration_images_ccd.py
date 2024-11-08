c873c24ad6cd914e8b746e317e546b58bb194eda
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-08 16:26:14 +0000

    adjust the code to go one directory back find and make master bias

--- calibration_images_ccd.py
+++ calibration_images_ccd.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 """
-Find and read bias images, trim them and median combine them
-
+Find and read bias images, trim them, and median combine them.
 """
 
 import os
 from astropy.io import fits
 import numpy as np
-
 
 def filter_filenames(directory):
     """
@@ -30,7 +28,7 @@ def filter_filenames(directory):
             with fits.open(fits_path) as hdul:
                 header = hdul[0].header
                 if 'IMGTYPE' in header and header['IMGTYPE'] == 'BIAS':
-                    filtered_filenames.append(filename)  # Append only the filename without the directory path
+                    filtered_filenames.append(filename)
     return sorted(filtered_filenames)
 
 
@@ -40,31 +38,25 @@ def trim_bias(directory):
 
     Parameters
     ----------
-    directory
-    containing the bias files.
+    directory : str
+        Directory containing the bias files.
 
     Returns
     -------
     numpy.ndarray
         Master bias.
     """
-
-    # Find and read the bias for hdr mode
     files = filter_filenames(directory)
-
-    # join the directory path to the filenames
     files_filtered = [os.path.join(directory, f) for f in files]
     print(f'Found {len(files_filtered)} bias files in {directory}')
 
     if fits.open(files_filtered[0])[0].data.shape == (2048, 2048):
         print('The files are already trimmed')
     else:
-        # check if we have an overscan to remove
         for filename in files_filtered:
-            frame = fits.open(filename)
-            frame = frame[0]
+            frame = fits.open(filename)[0]
             print('Initial frame shape:', frame.data.shape)
-            if frame.data.shape == (2048, 2088):  # overscan present: x,y
+            if frame.data.shape == (2048, 2088):
                 frame_data = frame.data[:, 20:2068].astype(float)
                 print('Final frame shape:', frame_data.shape)
                 frame = fits.PrimaryHDU(frame_data.astype(np.uint16), header=frame.header)
@@ -74,7 +66,6 @@ def trim_bias(directory):
 
 
 def bias(directory):
-
     master_bias_path = os.path.join(directory, 'master_bias.fits')
 
     if os.path.exists(master_bias_path):
@@ -82,25 +73,17 @@ def bias(directory):
         return fits.getdata(master_bias_path)
     else:
         print('Did not find master bias, creating....')
-
-        # Find and read the bias for hdr mode
         files = filter_filenames(directory)
-
-        # Limit the number of files to the first 21
         files_filtered = files[:21]
-
-        # join the directory path to the filenames
         files_filtered = [os.path.join(directory, f) for f in files_filtered]
         print(f'Found {len(files_filtered)} bias files in {directory}')
 
         try:
-            # create a 3D cube of the bias files
             cube = np.zeros((2048, 2048, len(files_filtered)))
             for i, f in enumerate(files_filtered):
                 cube[:, :, i] = fits.getdata(f)
 
             master_bias = np.median(cube, axis=2)
-
             header = fits.getheader(files_filtered[0])
             fits.PrimaryHDU(master_bias, header=header).writeto(master_bias_path, overwrite=True)
 
@@ -108,7 +91,6 @@ def bias(directory):
             print(f'Failed to create master bias. Exception: {str(e)}')
             return None
 
-        # zip the files apart from master_bias
         for filename in files:
             if filename != 'master_bias.fits':
                 os.system(f"bzip2 {os.path.join(directory, filename)}")
@@ -119,39 +101,39 @@ def bias(directory):
 
 
 if __name__ == '__main__':
-    # Set the directory containing the bias files
-    parent_directory = os.getcwd()
+    # Store the initial directory path
+    initial_directory = os.getcwd()
+    print(f"Initial directory: {initial_directory}")
 
-    # get a list of subdirectories inside the parent directory
+    # Navigate to the parent directory
+    parent_directory = os.path.dirname(initial_directory)
+    os.chdir(parent_directory)
+    print(f"Changed to parent directory: {parent_directory}")
+
+    # Search for subdirectories in the parent directory
     subdirectories = [name for name in os.listdir(parent_directory) if
                       os.path.isdir(os.path.join(parent_directory, name))]
 
-    print('Action found:', subdirectories)
-
     for subdirectory in subdirectories:
         if subdirectory.startswith("action") and subdirectory.endswith("_biasFrames"):
-            # form the full path to the subdirectory
             subdirectory_path = os.path.join(parent_directory, subdirectory)
-
-            # set directory for the current subdirectory
             directory = subdirectory_path
-            print(f"Directory: {directory}")
+            print(f"Processing directory: {directory}")
 
-            # unzip the files
+            # Unzip the files
             os.system(f"bzip2 -d {directory}/*.bz2")
             print(f"Unzipped files in {directory}")
 
-            # trim the bias images (2048, 2088) to (2048, 2048)
+            # Trim the bias images
             trim_bias(directory)
 
-            # Reduce the image
+            # Generate the master bias
             bias(directory)
 
-            # move master_bias to parent directory
+            # Move master_bias to the parent directory
             os.system(f"mv {directory}/master_bias.fits {parent_directory}")
             print(f"Moved master_bias to {parent_directory}")
 
-
-
-
-
+    # Return to the initial directory
+    os.chdir(initial_directory)
+    print(f"Returned to the initial directory: {initial_directory}")
