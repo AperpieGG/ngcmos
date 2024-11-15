5ca4cfaa32fa8ed6f1b1e9ee80867e9bd1e5d1a7
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-15 02:58:21 +0000

    add adding_headers script

--- Process_CCD.sh
+++ Process_CCD.sh
@@ -20,6 +20,7 @@ for observe_dir in $(find . -maxdepth 1 -type d -name "action*_observeField"); d
     # add this point you have to sent the catalog input file for phot.
     python /home/ops/refcatpipe2/cmos/simple_wrapper.py --camera ccd
     python /home/ops/ngcmos/check_ccd.py
+    python /home/ops/ngcmos/adding_headers.py
     python /home/ops/ngcmos/calibration_images_ccd.py
     python /home/ops/ngcmos/process_ccd.py
 
