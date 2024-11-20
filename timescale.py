56d46e486f785f9b0305a6d7e0a4e15f88d3fed9
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-11-20 16:59:55 +0000

    add prints

--- timescale.py
+++ timescale.py
@@ -63,9 +63,6 @@ def compute_rms_values(phot_table):
     # Adjust the red noise model
     RMS_model_red = np.sqrt((RMS[0] ** 2) + red_noise)
 
-    # Scale models to the first data point
-    scaling_factor_red = RMS[0] / RMS_model_red[0]
-    RMS_model_red *= scaling_factor_red
 
     # Combine white and red noise
     RMS_model_combined = np.sqrt((RMS[0] ** 2 / binning_values) + red_noise)
