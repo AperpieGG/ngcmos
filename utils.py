d5cd09530552544b0a456f696f1b0f1f01dc021d
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-12-04 11:07:58 +0000

    divide sky by 2

--- utils.py
+++ utils.py
@@ -731,7 +731,7 @@ def noise_sources(sky_list, bin_size, airmass_list, zp, aper, rn, dc, exposure, 
     read_signal = npix * (read_noise_pix ** 2)
 
     # sky_flux = np.median(sky_list)
-    sky_flux = np.mean(sky_list)
+    sky_flux = np.mean(sky_list) / 2
     sky_noise = np.sqrt(sky_flux) / synthetic_flux / np.sqrt(bin_size) * 1000000  # Convert to ppm
     print('Average sky flux: ', sky_flux)
 
