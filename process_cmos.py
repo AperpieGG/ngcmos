16a6b0f00a696e28c018304d87ae22fa57237a89
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-12-05 04:39:21 +0000

    for relative phot, run pipeline for stars brigther than 12, for process cmos change from a path to the archive

--- process_cmos.py
+++ process_cmos.py
@@ -66,7 +66,7 @@ def load_config(filename):
 
 
 # Load paths from the configuration file
-config = load_config('directories.json')
+config = load_config('Archive/directories.json')
 calibration_paths = config["calibration_paths"]
 base_paths = config["base_paths"]
 out_paths = config["out_paths"]
