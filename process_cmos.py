ae23f82b17547cb8b136d9470a6a95f01b424074
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2024-12-09 20:27:09 +0000

    fix the loop same as in process_ccd for saving the phot table

--- process_cmos.py
+++ process_cmos.py
@@ -153,91 +153,91 @@ def main():
         for filename in prefix_filenames:
             logging.info(f"Processing filename {filename}......")
             # Calibrate image and get FITS file
-            try:
-                # logging.info(
-                #     f"The average pixel value for {filename} is {fits.getdata(os.path.join(directory, filename)).mean()}")
-                reduced_data, reduced_header, _ = reduce_images(base_path, out_path, [filename])
-                # logging.info(f"The average pixel value for {filename} is {reduced_data[0].mean()}")
-                # Convert reduced_data to a dictionary with filenames as keys
-                reduced_data_dict = {filename: (data, header) for data, header in zip(reduced_data, reduced_header)}
+            # logging.info(
+            #     f"The average pixel value for {filename} is {fits.getdata(os.path.join(directory, filename)).mean()}")
+            reduced_data, reduced_header, _ = reduce_images(base_path, out_path, [filename])
+            # logging.info(f"The average pixel value for {filename} is {reduced_data[0].mean()}")
+            # Convert reduced_data to a dictionary with filenames as keys
+            reduced_data_dict = {filename: (data, header) for data, header in zip(reduced_data, reduced_header)}
 
-                # Access the reduced data and header corresponding to the filename
-                frame_data, frame_hdr = reduced_data_dict[filename]
-                # logging.info(f"Extracting photometry for {filename}")
+            # Access the reduced data and header corresponding to the filename
+            frame_data, frame_hdr = reduced_data_dict[filename]
+            # logging.info(f"Extracting photometry for {filename}")
 
-                # Extract airmass and zero point from the header
-                airmass, zp = extract_airmass_and_zp(frame_hdr)
+            # Extract airmass and zero point from the header
+            airmass, zp = extract_airmass_and_zp(frame_hdr)
 
-                wcs_ignore_cards = ['SIMPLE', 'BITPIX', 'NAXIS', 'EXTEND', 'DATE', 'IMAGEW', 'IMAGEH']
-                wcs_header = {}
-                for line in [frame_hdr[i:i + 80] for i in range(0, len(frame_hdr), 80)]:
-                    key = line[0:8].strip()
-                    if '=' in line and key not in wcs_ignore_cards:
-                        card = fits.Card.fromstring(line)
-                        wcs_header[card.keyword] = card.value
+            wcs_ignore_cards = ['SIMPLE', 'BITPIX', 'NAXIS', 'EXTEND', 'DATE', 'IMAGEW', 'IMAGEH']
+            wcs_header = {}
+            for line in [frame_hdr[i:i + 80] for i in range(0, len(frame_hdr), 80)]:
+                key = line[0:8].strip()
+                if '=' in line and key not in wcs_ignore_cards:
+                    card = fits.Card.fromstring(line)
+                    wcs_header[card.keyword] = card.value
 
-                frame_bg = sep.Background(frame_data)
-                frame_data_corr_no_bg = frame_data - frame_bg
-                estimate_coord = SkyCoord(ra=frame_hdr['TELRA'],
-                                          dec=frame_hdr['TELDEC'],
-                                          unit=(u.deg, u.deg))
-                estimate_coord_radius = 3 * u.deg
+            frame_bg = sep.Background(frame_data)
+            frame_data_corr_no_bg = frame_data - frame_bg
+            estimate_coord = SkyCoord(ra=frame_hdr['TELRA'],
+                                      dec=frame_hdr['TELDEC'],
+                                      unit=(u.deg, u.deg))
+            estimate_coord_radius = 3 * u.deg
 
-                frame_objects = _detect_objects_sep(frame_data_corr_no_bg, frame_bg.globalrms,
-                                                    AREA_MIN, AREA_MAX, DETECTION_SIGMA, DEFOCUS)
-                if len(frame_objects) < N_OBJECTS_LIMIT:
-                    logging.info(f"Fewer than {N_OBJECTS_LIMIT} objects found in {filename}, skipping photometry!")
-                    continue
+            frame_objects = _detect_objects_sep(frame_data_corr_no_bg, frame_bg.globalrms,
+                                                AREA_MIN, AREA_MAX, DETECTION_SIGMA, DEFOCUS)
+            if len(frame_objects) < N_OBJECTS_LIMIT:
+                logging.info(f"Fewer than {N_OBJECTS_LIMIT} objects found in {filename}, skipping photometry!")
+                continue
 
-                # Load the photometry catalog
-                phot_cat, _ = get_catalog(f"{directory}/{prefix}_catalog_input.fits", ext=1)
-                # logging.info(f"Found catalog with name {prefix}_catalog_input.fits")
-                # Convert RA and DEC to pixel coordinates using the WCS information from the header
-                phot_x, phot_y = WCS(frame_hdr).all_world2pix(phot_cat['ra_deg_corr'], phot_cat['dec_deg_corr'], 1)
+            # Load the photometry catalog
+            phot_cat, _ = get_catalog(f"{directory}/{prefix}_catalog_input.fits", ext=1)
+            # logging.info(f"Found catalog with name {prefix}_catalog_input.fits")
+            # Convert RA and DEC to pixel coordinates using the WCS information from the header
+            phot_x, phot_y = WCS(frame_hdr).all_world2pix(phot_cat['ra_deg_corr'], phot_cat['dec_deg_corr'], 1)
 
-                # Do time conversions - one time value per format per target
-                half_exptime = frame_hdr['EXPTIME'] / 2.
-                time_isot = Time([frame_hdr['DATE-OBS'] for i in range(len(phot_x))],
-                                 format='isot', scale='utc', location=get_location())
-                time_jd = Time(time_isot.jd, format='jd', scale='utc', location=get_location())
-                # Correct to mid-exposure time
-                time_jd = time_jd + half_exptime * u.second
-                ra = phot_cat['ra_deg_corr']
-                dec = phot_cat['dec_deg_corr']
-                ltt_bary, ltt_helio = get_light_travel_times(ra, dec, time_jd)
-                time_bary = time_jd.tdb + ltt_bary
-                time_helio = time_jd.utc + ltt_helio
+            # Do time conversions - one time value per format per target
+            half_exptime = frame_hdr['EXPTIME'] / 2.
+            time_isot = Time([frame_hdr['DATE-OBS'] for i in range(len(phot_x))],
+                             format='isot', scale='utc', location=get_location())
+            time_jd = Time(time_isot.jd, format='jd', scale='utc', location=get_location())
+            # Correct to mid-exposure time
+            time_jd = time_jd + half_exptime * u.second
+            ra = phot_cat['ra_deg_corr']
+            dec = phot_cat['dec_deg_corr']
+            ltt_bary, ltt_helio = get_light_travel_times(ra, dec, time_jd)
+            time_bary = time_jd.tdb + ltt_bary
+            time_helio = time_jd.utc + ltt_helio
 
-                frame_ids = [filename for i in range(len(phot_x))]
-                logging.info(f"Found {len(frame_ids)} sources")
+            frame_ids = [filename for i in range(len(phot_x))]
+            logging.info(f"Found {len(frame_ids)} sources")
 
-                frame_preamble = Table([frame_ids, phot_cat['gaia_id'], phot_cat['Tmag'], phot_cat['tic_id'],
-                                        phot_cat['gaiabp'], phot_cat['gaiarp'], time_jd.value, time_bary.value,
-                                        time_helio.value, phot_x, phot_y,
-                                        [airmass] * len(phot_x), [zp] * len(phot_x)],
-                                       names=("frame_id", "gaia_id", "Tmag", "tic_id", "gaiabp", "gaiarp", "jd_mid",
-                                              "jd_bary", "jd_helio", "x", "y", "airmass", "zp"))
+            frame_preamble = Table([frame_ids, phot_cat['gaia_id'], phot_cat['Tmag'], phot_cat['tic_id'],
+                                    phot_cat['gaiabp'], phot_cat['gaiarp'], time_jd.value, time_bary.value,
+                                    time_helio.value, phot_x, phot_y,
+                                    [airmass] * len(phot_x), [zp] * len(phot_x)],
+                                   names=("frame_id", "gaia_id", "Tmag", "tic_id", "gaiabp", "gaiarp", "jd_mid",
+                                          "jd_bary", "jd_helio", "x", "y", "airmass", "zp"))
 
-                # Extract photometry at locations
-                frame_phot = wcs_phot(frame_data, phot_x, phot_y, RSI, RSO, APERTURE_RADII, gain=GAIN)
+            # Extract photometry at locations
+            frame_phot = wcs_phot(frame_data, phot_x, phot_y, RSI, RSO, APERTURE_RADII, gain=GAIN)
 
-                # Stack the photometry and preamble
-                frame_output = hstack([frame_preamble, frame_phot])
+            frame_output = hstack([frame_preamble, frame_phot])
 
-                # Save or append to the photometry file
-                if os.path.exists(phot_output_filename):
-                    existing_table = Table.read(phot_output_filename)
-                    updated_table = vstack([existing_table, frame_output])
-                    updated_table.write(phot_output_filename, overwrite=True)
-                else:
-                    frame_output.write(phot_output_filename, overwrite=True)
+            # Stack the photometry results
+            if phot_table is None:
+                phot_table = frame_output
+            else:
+                phot_table = vstack([phot_table, frame_output])
 
-                logging.info(f"Finished photometry for {filename}\n")
+            logging.info(f"Finished photometry for {filename}\n")
 
-            except Exception as e:
-                logging.error(f"Error processing {filename}: {e}")
-            continue
-    logging.info("Done!\n")
+        # Save the accumulated photometry
+        if phot_table is not None:
+            phot_table.write(phot_output_filename, overwrite=True)
+            logging.info(f"Saved photometry for prefix {prefix} to {phot_output_filename}\n")
+        else:
+            logging.info(f"No photometry data for prefix {prefix}.\n")
+
+        logging.info("Done!\n")
 
 
 if __name__ == "__main__":
