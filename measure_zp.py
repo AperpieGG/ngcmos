9073d99ac5d0e16e508b0f4666d0ae3f132a3d39
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-01-06 16:15:40 +0200

    remove legends

--- measure_zp.py
+++ measure_zp.py
@@ -60,13 +60,13 @@ def main():
         print(f"Zero point average: {np.nanmean(zp_list)}")
 
         # plot zp_list on a histogram
-        plt.hist(zp_list, bins=100)
+        plt.hist(zp_list, bins=100, label=f"Avg: {np.nanmean(zp_list):.2f}")
         plt.axvline(np.nanmean(zp_list), color='black', linestyle='--', label=f"Mean: {np.nanmean(zp_list):.2f}")
         # plt.axvline(np.nanmedian(zp_list), color='g', linestyle='--', label=f"Median: {np.nanmedian(zp_list):.2f}")
         plt.xlabel('Zero Point')
         plt.ylabel('Frequency')
         plt.yscale('log')
-        plt.legend(loc='upper right')
+        # plt.legend(loc='upper right')
         plt.show()
         # save the histogram to a png file
         plt.savefig(f'zp{APERTURE}.pdf', dpi=300)
