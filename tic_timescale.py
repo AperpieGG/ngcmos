1c64a7a8646e6cc1412beeed009b008ec9d6105f
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-05-23 12:35:48 +0100

    add color

--- tic_timescale.py
+++ tic_timescale.py
@@ -67,10 +67,10 @@ def compute_rms_values(data, exp, max_binning):
     return time_seconds, average_rms_values, RMS_model
 
 
-def plot_timescale(times, avg_rms, RMS_model, label):
+def plot_timescale(times, avg_rms, RMS_model, label, label_color):
     fig, ax = plt.subplots(figsize=(6, 8))
-    ax.plot(times, avg_rms, 'o', label=f"{label} Data", color='blue')
-    ax.plot(times, RMS_model, '--', label=f"{label} Model", color='blue')
+    ax.plot(times, avg_rms, 'o', label=f"{label} Data", color=label_color)
+    ax.plot(times, RMS_model, '--', label=f"{label} Model", color=label_color)
     ax.axvline(x=900, color='black', linestyle='-', label='Reference Line (x=900)')
     ax.set_xscale('log')
     ax.set_yscale('log')
@@ -88,10 +88,17 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run and plot RMS from JSON photometry file.')
     parser.add_argument('json_file', type=str, help='Path to JSON photometry file')
     parser.add_argument('--bin', type=int, default=180, help='Maximum binning steps')
+    parser.add_argument('--cam', type=str, default='CMOS', help='Maximum binning steps')
+
     args = parser.parse_args()
 
     phot_data = load_json_data(args.json_file)
     print("Trimming data by time...")
     phot_data = trim_target_data_by_time(phot_data)
     times, avg_rms, RMS_model = compute_rms_values(phot_data, exp=10, max_binning=args.bin)
-    plot_timescale(times, avg_rms, RMS_model, label=str(phot_data['TIC_ID']))
+    if args.cam == 'CMOS':
+        label_color = 'blue'
+    else:
+        label_color = 'red'
+
+    plot_timescale(times, avg_rms, RMS_model, label=str(phot_data['TIC_ID']), label_color=label_color)
