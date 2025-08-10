e3c9e382dc307e6402938e7551160c32277f4a37
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2025-08-10 02:47:17 +0100

    add printints

--- utils.py
+++ utils.py
@@ -721,6 +721,7 @@ def noise_sources(sky_list, bin_size, airmass_list, zp, aper, rn, dc, exposure, 
     # set exposure time and random flux
     exposure_time = exposure
     synthetic_flux = np.arange(100, 1e7, 1000)
+    print(f'Using Gain: {gain}, Exposure time: {exposure_time}, Aperture radius: {aper}')
     synthetic_mag = np.mean(zp) + 2.5 * np.log10(gain) - 2.5 * np.log10(synthetic_flux / exposure_time)
     # set dark current rate from cmos characterisation
     dark_current = dc * exposure_time * npix
