d5c7b3bb4ffec71e63aee6e5d6c3b9ced8a11600
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2026-01-28 17:00:09 +0200

    change the mask ellipse to 3 radius

--- utils.py
+++ utils.py
@@ -355,7 +355,7 @@ def wcs_phot(data, x, y, rsi, rso, aperture_radii, gain):
                          obj['x'], obj['y'],
                          obj['a'], obj['b'],
                          obj['theta'],
-                         r=3.0)
+                         r=2.0)
 
     # Column labels
     col_labels = ["flux", "fluxerr", "flux_w_sky", "fluxerr_w_sky", "max_pixel_value"]
