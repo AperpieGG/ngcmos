affd1dbd9c0f75031d9b00ea854ae72d79e0ccd0
Ioannis Apergis <ioannis.apergis@warwick.ac.uk>
2026-02-11 15:06:46 +0200

    remove legends

--- plot_results.py
+++ plot_results.py
@@ -88,7 +88,7 @@ def plot_noise_model(data):
     ax.set_xlim(7.5, 14)
     ax.set_ylim(1000, 100000)
     ax.invert_xaxis()
-    plt.legend(loc='best')
+    # plt.legend(loc='best')
     plt.tight_layout()
     plt.show()
 
